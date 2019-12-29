431d7607df08076e5160a6c456a151d21c0462be
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2019-12-30 03:29:22 +0800

    Update

--- MIDI Editor/zaibuyidao_Insert Sustain Pedal.lua
+++ MIDI Editor/zaibuyidao_Insert Sustain Pedal.lua
@@ -1,7 +1,7 @@
 --[[
  * ReaScript Name: Insert Sustain Pedal
  * Instructions: Open a MIDI take in MIDI Editor. Position Edit Cursor, Run.
- * Version: 1.2
+ * Version: 1.3
  * Author: zaibuyidao
  * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
  * Repository: GitHub > zaibuyidao > ReaScripts
@@ -12,47 +12,38 @@
 
 --[[
  * Changelog:
+ * v1.3 (2019-12-30)
+  + Updated algorithm to accurately generate CC event in measuers
+  + 更新算法以在小节中准确生成CC事件
  * v1.0 (2019-12-12)
   + Initial release
 --]]
 
-interval = 1920 -- 重复间隔. 480tick下每小节1920, 960tick下每小节3840.
-selected = false
-
-local retval, userInputsCSV = reaper.GetUserInputs("Insert Sustain Pedal", 6, "CC Number,On:0-63,Off:64-127,Repetition,First Offset,Second Offset", "64,127,0,10,110,-10")
+local retval, userInputsCSV = reaper.GetUserInputs("Insert Sustain Pedal", 1, "Repetition", "99")
 if not retval then return reaper.SN_FocusMIDIEditor() end
-local cc_num, cc_begin, cc_end, cishu, first_offset, second_offset = userInputsCSV:match("(.*),(.*),(.*),(.*),(.*),(.*)")
-cc_num, cc_begin, cc_end, cishu, first_offset, second_offset = tonumber(cc_num), tonumber(cc_begin), tonumber(cc_end), tonumber(cishu), tonumber(first_offset), tonumber(second_offset)
+local cishu = userInputsCSV:match("(.*)")
+cishu = tonumber(cishu)
 
 function HoldPedalOn()
   local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
   if take == nil then return end
-  local pos = reaper.GetCursorPositionEx(0)
-  local ppq = reaper.MIDI_GetPPQPosFromProjTime(take, pos)
-  ppq = ppq - interval + first_offset -- 踩下偏移
   for i = 1, cishu do
-    ppq = ppq + interval
-    reaper.MIDI_InsertCC(take, selected, false, ppq, 0xB0, 0, cc_num, cc_begin)
-    i=i+1
-  end
-end
-
-function HoldPedalOff()
-  local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
-  if take == nil then return end
-  local pos = reaper.GetCursorPositionEx(0)
-  local ppq = reaper.MIDI_GetPPQPosFromProjTime(take, pos)
-  ppq = ppq + second_offset -- 释放偏移
-  for i = 1, cishu do
-    ppq = ppq + interval
-    reaper.MIDI_InsertCC(take, selected, false, ppq, 0xB0, 0, cc_num, cc_end)
+    local pos = reaper.GetCursorPositionEx(0) -- 获得光标位置
+    local ppq = reaper.MIDI_GetPPQPosFromProjTime(take, pos) -- 获得PPQ值
+    local newstart =  reaper.MIDI_GetProjQNFromPPQPos(take, ppq) -- 获得QN值
+    local Meas, Bar_Start_QN, Bar_End_QN = reaper.TimeMap_QNToMeasures(0, newstart)
+    local new_Start_QN = reaper.MIDI_GetPPQPosFromProjQN( take, Bar_Start_QN )
+	local new_End_QN = reaper.MIDI_GetPPQPosFromProjQN( take, Bar_End_QN )
+    reaper.MIDI_InsertCC(take, selected, false, new_Start_QN+110, 0xB0, 0, 64, 127) -- 踩下
+    reaper.MIDI_InsertCC(take, selected, false, new_End_QN-10, 0xB0, 0, 64, 0) -- 释放
+	reaper.MIDIEditor_OnCommand(reaper.MIDIEditor_GetActive(), 40451)
     i=i+1
   end
 end
 
 reaper.Undo_BeginBlock()
+selected = true
 HoldPedalOn()
-HoldPedalOff()
 reaper.UpdateArrange()
 reaper.Undo_EndBlock("Insert Sustain Pedal", -1)
 reaper.SN_FocusMIDIEditor()
