10357c6e9e331a285a53705d0fdf4d719c5c4c59
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2020-02-22 00:40:02 +0800

    Update

--- MIDI Editor/zaibuyidao_Copy Selected Rhythm.lua
+++ MIDI Editor/zaibuyidao_Copy Selected Rhythm.lua
@@ -1,7 +1,7 @@
 --[[
  * ReaScript Name: Copy Selected Rhythm
  * Instructions: Open a MIDI take in MIDI Editor. Select Notes. Run.
- * Version: 1.0
+ * Version: 1.1
  * Author: zaibuyidao
  * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
  * Repository: GitHub > zaibuyidao > ReaScripts
@@ -25,11 +25,11 @@ function CopySelectedRhythm()
   local t = {}
   local str = ""
   for i = 1, notecnt do
-    _, sel, _, s, e, _, _, _ = reaper.MIDI_GetNote(take, i - 1)
+    _, sel, _, s, e, _, _, v = reaper.MIDI_GetNote(take, i - 1)
     local meas = reaper.MIDI_GetPPQPos_StartOfMeasure(take, s)
     local sppq = s - meas
     local eppq = e - meas
-   if sel == true then str = str..'\n '..math.floor(sppq)..' '..math.floor(eppq) end
+    if sel == true then str = str..'\n '..math.floor(sppq)..' '..math.floor(eppq)..' '..math.floor(v) end
   end
   reaper.SetExtState('CopySelectedRhythm', 'buf', str, false)
 end
--- MIDI Editor/zaibuyidao_Paste Selected Rhythm.lua
+++ MIDI Editor/zaibuyidao_Paste Selected Rhythm.lua
@@ -1,7 +1,7 @@
 --[[
  * ReaScript Name: Paste Selected Rhythm
  * Instructions: Open a MIDI take in MIDI Editor. Select Notes. Run.
- * Version: 1.0
+ * Version: 1.1
  * Author: zaibuyidao
  * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
  * Repository: GitHub > zaibuyidao > ReaScripts
@@ -17,28 +17,30 @@
 --]]
 
 function PasteSelectedRhythm()
-    local midieditor = reaper.MIDIEditor_GetActive()
-    if not midieditor then return end
-    local take = reaper.MIDIEditor_GetTake(midieditor)
-    if not take or not reaper.TakeIsMIDI(take) then return end
-    local _, notecnt = reaper.MIDI_CountEvts(take)
-    str = reaper.GetExtState('CopySelectedRhythm', 'buf')
-    if str == '' then return end
-    t = {}
-    for num in str:gmatch('[%d]+') do t[#t + 1] = tonumber(num)  end
-    local t2 = {}
-    for i = 1, #t-1, 2 do t2[#t2+1] = {sppq = t[i], eppq = t[i+1]} end
-    for i = 1, notecnt do
-      retval, sel, m, s, e, c, p, v = reaper.MIDI_GetNote(take, i - 1)
-      if sel == true then
-        local meas = reaper.MIDI_GetPPQPos_StartOfMeasure(take, s)
-        for i = 1, #t2 do
-          reaper.MIDI_InsertNote(take, false, m, meas + t2[i].sppq, meas + t2[i].eppq, c, p, v, true)
-        end
+  local midieditor = reaper.MIDIEditor_GetActive()
+  if not midieditor then return end
+  local take = reaper.MIDIEditor_GetTake(midieditor)
+  if not take or not reaper.TakeIsMIDI(take) then return end
+  local _, notecnt = reaper.MIDI_CountEvts(take)
+  str = reaper.GetExtState('CopySelectedRhythm', 'buf')
+  if str == '' then return end
+  t = {}
+  for num in str:gmatch('[%d]+') do t[#t + 1] = tonumber(num)  end
+  local t2 = {}
+  for i = 1, #t-1, 3 do
+    t2[#t2+1] = {sppq = t[i], eppq = t[i+1], vel = t[i+2]}
+  end
+  for i = 1, notecnt do
+    retval, sel, m, s, e, c, p, v = reaper.MIDI_GetNote(take, i - 1)
+    if sel == true then
+      local meas = reaper.MIDI_GetPPQPos_StartOfMeasure(take, s)
+      for i = 1, #t2 do
+        reaper.MIDI_InsertNote(take, false, m, meas + t2[i].sppq, meas + t2[i].eppq, c, p, t2[i].vel, true)
       end
     end
-    reaper.MIDIEditor_OnCommand(midieditor, 40002)
-    reaper.MIDI_Sort(take)
+  end
+  reaper.MIDIEditor_OnCommand(midieditor, 40002)
+  reaper.MIDI_Sort(take)
 end
 
 title = "Copy Selected Rhythm"
