ac1a8103c47f892992bac6bbe9a087fd48edaead
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2020-05-20 02:56:20 +0800

    Update

--- MIDI Editor/zaibuyidao_End Time.lua
+++ MIDI Editor/zaibuyidao_End Time.lua
@@ -1,7 +1,7 @@
 --[[
  * ReaScript Name: End Time
  * Instructions: Open a MIDI take in MIDI Editor. Select Notes. Run.
- * Version: 1.0
+ * Version: 1.1
  * Author: zaibuyidao
  * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
  * Repository: GitHub > zaibuyidao > ReaScripts
