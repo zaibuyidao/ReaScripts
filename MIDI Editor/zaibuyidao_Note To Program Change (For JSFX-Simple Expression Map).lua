5c3689bcc10d6842a17f0dc4b247fa18610c907b
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2020-08-12 01:13:38 +0800

    Update

--- MIDI Editor/zaibuyidao_Note To Program Change (For JSFX-Simple Expression Map).lua
+++ MIDI Editor/zaibuyidao_Note To Program Change (For JSFX-Simple Expression Map).lua
@@ -1,7 +1,7 @@
 --[[
  * ReaScript Name: Note To Program Change (For JSFX-Simple Expression Map)
  * Instructions: Part of [JSFX: Simple Expression Map]. Open a MIDI take in MIDI Editor. Select Notes. Run.
- * Version: 1.3
+ * Version: 1.4
  * Author: zaibuyidao
  * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
  * Repository: GitHub > zaibuyidao > ReaScripts
@@ -23,17 +23,24 @@ end
 function main()
   local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
   if take == nil then return end
+  local cnt, index = 0, {}
+  local val = reaper.MIDI_EnumSelNotes(take, -1)
+  while val ~= - 1 do
+    cnt = cnt + 1
+    index[cnt] = val
+    val = reaper.MIDI_EnumSelNotes(take, val)
+  end
+  if cnt == 0 then return reaper.SN_FocusMIDIEditor() end
   local MSB = reaper.GetExtState("NoteToProgramChange", "MSB")
   if (MSB == "") then MSB = "0" end
   local LSB
-  local user_ok, MSB = reaper.GetUserInputs('Confirm Group Number', 1, 'Group Number (Defaul=0)', MSB)
-  if not user_ok then return reaper.SN_FocusMIDIEditor() end
+  local user_ok, MSB = reaper.GetUserInputs('Confirm instrument group', 1, 'Group number', MSB)
+  if not user_ok or not tonumber(MSB) then return reaper.SN_FocusMIDIEditor() end
   reaper.SetExtState("NoteToProgramChange", "MSB", MSB, false)
-  _, notecnt, _, _ = reaper.MIDI_CountEvts(take)
   reaper.MIDI_DisableSort(take)
 
-  for i = 1, notecnt do
-    retval, selected, muted, startppqpos, endppqpos, chan, pitch, vel = reaper.MIDI_GetNote(take, i - 1)
+  for i = 1, #index do
+    retval, selected, muted, startppqpos, endppqpos, chan, pitch, vel = reaper.MIDI_GetNote(take, index[i])
     if selected == true then
       if vel == 127 then
         LSB = 127
