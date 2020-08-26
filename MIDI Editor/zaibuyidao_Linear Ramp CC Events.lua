4edf0690bddc72bd58b80cee02706fb66ea1f0ee
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2020-08-27 03:30:13 +0800

    Add Scripts

--- MIDI Editor/zaibuyidao_Linear Ramp CC Events.lua
+++ MIDI Editor/zaibuyidao_Linear Ramp CC Events.lua
@@ -1,7 +1,7 @@
 --[[
  * ReaScript Name: Linear Ramp CC Events
  * Instructions: Open a MIDI take in MIDI Editor. Select Notes. Run.
- * Version: 1.5
+ * Version: 1.6
  * Author: zaibuyidao
  * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
  * Repository: GitHub > zaibuyidao > ReaScripts
@@ -12,8 +12,6 @@
 
 --[[
  * Changelog:
- * v1.3 (2020-2-15)
-  + Perform actions based on note length
  * v1.0 (2019-12-12)
   + Initial release
 --]]
@@ -22,7 +20,7 @@ function main()
   local take=reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
   local tick = reaper.SNM_GetIntConfigVar("MidiTicksPerBeat", 480)
   if take == nil then return end
-  reaper.MIDI_DisableSort(take)
+
   local cnt, index = 0, {}
   local val = reaper.MIDI_EnumSelNotes(take, -1)
   while val ~= - 1 do
@@ -30,59 +28,51 @@ function main()
     index[cnt] = val
     val = reaper.MIDI_EnumSelNotes(take, val)
   end
+  
   if #index > 0 then
     cc_num = reaper.GetExtState("LinearRampCCEvents", "Number")
-    cc_begin = reaper.GetExtState("LinearRampCCEvents", "Begin")
-    cc_end = reaper.GetExtState("LinearRampCCEvents", "End")
-    step = reaper.GetExtState("LinearRampCCEvents", "Step")
+    min_val = reaper.GetExtState("LinearRampCCEvents", "Begin")
+    max_val = reaper.GetExtState("LinearRampCCEvents", "End")
     if (cc_num == "") then cc_num = "11" end
-    if (cc_begin == "") then cc_begin = "90" end
-    if (cc_end == "") then cc_end = "127" end
-    if (step == "") then step = "1" end
-    local user_ok, user_input_csv = reaper.GetUserInputs("Linear Ramp CC Events", 4, "CC Number,Min Volume,Max Volume,Step", cc_num..','..cc_begin..','.. cc_end..','..step)
-    cc_num, cc_begin, cc_end, step = user_input_csv:match("(.*),(.*),(.*),(.*)")
-    if not user_ok or not tonumber(cc_num) or not tonumber(cc_begin) or not tonumber(cc_end) or not tonumber(step) then return reaper.SN_FocusMIDIEditor() end
-    cc_num, cc_begin, cc_end, step = tonumber(cc_num), tonumber(cc_begin), tonumber(cc_end), tonumber(step)
+    if (min_val == "") then min_val = "90" end
+    if (max_val == "") then max_val = "127" end
+    local user_ok, user_input_csv = reaper.GetUserInputs("Linear Ramp CC Events", 3, "CC number,Min value,Max value", cc_num..','..min_val..','.. max_val)
+    cc_num, min_val, max_val = user_input_csv:match("(.*),(.*),(.*)")
+    if not user_ok or not tonumber(cc_num) or not tonumber(min_val) or not tonumber(max_val)then return reaper.SN_FocusMIDIEditor() end
+    cc_num, min_val, max_val = tonumber(cc_num), tonumber(min_val), tonumber(max_val)
+    if min_val < 1 or max_val > 127 or min_val >= max_val then return reaper.SN_FocusMIDIEditor() end
     reaper.SetExtState("LinearRampCCEvents", "Number", cc_num, false)
-    reaper.SetExtState("LinearRampCCEvents", "Begin", cc_begin, false)
-    reaper.SetExtState("LinearRampCCEvents", "End", cc_end, false)
-    reaper.SetExtState("LinearRampCCEvents", "Step", step, false)
+    reaper.SetExtState("LinearRampCCEvents", "Begin", min_val, false)
+    reaper.SetExtState("LinearRampCCEvents", "End", max_val, false)
 
-    if cc_begin >= cc_end then return reaper.SN_FocusMIDIEditor() end
-    local ppq = {} -- 音符开头位置
-    local ppq_end = {} -- 音符尾巴位置
-    local tbl = {} -- 存储CC值
-  
-    for j = cc_begin - 1, cc_end, step do
-      j = j + 1
-      if j > 127 then j = 127 end
-      if j > cc_end then j = cc_end end
-      table.insert(tbl, j) -- 将计算得到的CC值存入tbl表
-    end
-  
-    for i = 1, #index do
-      retval, selected, muted, ppq[i], ppq_end[i], chan, pitch, vel = reaper.MIDI_GetNote(take, index[i])
-      if selected == true then
-        ppq_len = ppq_end[i] - ppq[i]
-        if ppq_len >= tick / 2 and ppq_len < tick then
-          for k,v in pairs(tbl) do
-            reaper.MIDI_InsertCC(take, selected, muted, ppq[i]+(k-1)*5, 0xB0, chan, cc_num, v)
-          end
-        end
-        if ppq_len >= tick and ppq_len < tick * 2 then
-            for k,v in pairs(tbl) do
-              reaper.MIDI_InsertCC(take, selected, muted, ppq[i]+(k-1)*8, 0xB0, chan, cc_num, v)
+    reaper.MIDI_DisableSort(take)
+    i = reaper.MIDI_EnumSelNotes(take, -1)
+    while i ~= -1 do
+      note_ok, selected, muted, startppqpos, endppqpos, chan, pitch, vel = reaper.MIDI_GetNote(take, i)
+      diff = max_val - min_val
+        if note_ok and selected then
+          for k = 0, diff do
+            local linear = min_val + k
+            diff_01 = (tick+tick/4) / diff -- 将 1.25 拍的音符长度除于差值
+            note_len = endppqpos - startppqpos
+            if note_len >= tick/2 and note_len < tick then -- 如果音符大于等于 240 并且 小于 480
+              reaper.MIDI_InsertCC(take, selected, muted, startppqpos+k*diff_01*0.3, 0xB0, chan, cc_num, linear)
             end
-        end
-        if ppq_len >= tick * 2 then
-          for k,v in pairs(tbl) do
-            reaper.MIDI_InsertCC(take, selected, muted, ppq[i]+(k-1)*10, 0xB0, chan, cc_num, v)
+            if note_len >= tick and note_len < tick*2 then -- 如果音符大于等于 480 并且 小于 960
+              reaper.MIDI_InsertCC(take, selected, muted, startppqpos+k*diff_01*0.55, 0xB0, chan, cc_num, linear)
+            end
+            if note_len == tick*2 then -- 如果音符等于 960
+              reaper.MIDI_InsertCC(take, selected, muted, startppqpos+k*diff_01*0.8, 0xB0, chan, cc_num, linear)
+            end
+            if note_len > tick*2 then -- 如果音符大于 960
+              reaper.MIDI_InsertCC(take, selected, muted, startppqpos+k*diff_01, 0xB0, chan, cc_num, linear)
+            end
           end
         end
-      end
+        i = reaper.MIDI_EnumSelNotes(take, i)
     end
+    reaper.MIDI_Sort(take)
   end
-  reaper.MIDI_Sort(take)
 end
 
 script_title = "Linear Ramp CC Events"
--- Utility/zaibuyidao_Auto Expression (Multitrack).lua
+++ Utility/zaibuyidao_Auto Expression (Multitrack).lua
@@ -1,22 +1,32 @@
 --[[
- * ReaScript Name: Linear Ramp CC Events (Multitrack)
+ * ReaScript Name: Auto Expression (Multitrack)
  * Instructions: Open a MIDI take in MIDI Editor. Select Notes. Run.
- * Version: 1.1
- * Author: zaibuyidao
+ * Version: 1.0
+ * Author: zaibuyidao, dangguidan
  * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
  * Repository: GitHub > zaibuyidao > ReaScripts
  * Repository URI: https://github.com/zaibuyidao/ReaScripts
  * REAPER: 6.0
- * Donation: http://www.paypal.me/zaibuyidao
 --]]
 
 --[[
  * Changelog:
- * v1.0 (2020-8-26)
+ * v1.0 (2020-8-27)
   + Initial release
 --]]
 
-function LRCE()
+-- USER AREA
+-- Settings that the user can customize.
+
+cc_number = 11
+
+-- End of USER AREA
+
+function Msg(param)
+  reaper.ShowConsoleMsg(tostring(param) .. "\n")
+end
+
+function autoExp()
   tick = reaper.SNM_GetIntConfigVar("MidiTicksPerBeat", 480)
   i = reaper.MIDI_EnumSelNotes(take, -1)
   while i ~= -1 do
@@ -25,19 +35,23 @@ function LRCE()
       if note_ok and selected then
         for k = 0, diff do
           local linear = min_val + k
+          local fast_end = min_val + k - 1
           diff_01 = (tick+tick/4) / diff -- 将 1.25 拍的音符长度除于差值
+          diff_02 = k*k / diff -- 减弱CC差值
           note_len = endppqpos - startppqpos
           if note_len >= tick/2 and note_len < tick then -- 如果音符大于等于 240 并且 小于 480
-            reaper.MIDI_InsertCC(take, selected, muted, startppqpos+k*diff_01*0.3, 0xB0, chan, cc_num, linear)
+            reaper.MIDI_InsertCC(take, selected, muted, startppqpos+k*diff_01*0.3, 0xB0, chan, cc_number, linear)
           end
           if note_len >= tick and note_len < tick*2 then -- 如果音符大于等于 480 并且 小于 960
-            reaper.MIDI_InsertCC(take, selected, muted, startppqpos+k*diff_01*0.55, 0xB0, chan, cc_num, linear)
+            reaper.MIDI_InsertCC(take, selected, muted, startppqpos+k*diff_01*0.55, 0xB0, chan, cc_number, linear)
           end
           if note_len == tick*2 then -- 如果音符等于 960
-            reaper.MIDI_InsertCC(take, selected, muted, startppqpos+k*diff_01*0.8, 0xB0, chan, cc_num, linear)
+            reaper.MIDI_InsertCC(take, selected, muted, startppqpos+k*diff_01*0.8, 0xB0, chan, cc_number, linear)
+            reaper.MIDI_InsertCC(take, selected, muted, endppqpos-tick/24-diff_01*diff_02*0.3, 0xB0, chan, cc_number, fast_end)
           end
           if note_len > tick*2 then -- 如果音符大于 960
-            reaper.MIDI_InsertCC(take, selected, muted, startppqpos+k*diff_01, 0xB0, chan, cc_num, linear)
+            reaper.MIDI_InsertCC(take, selected, muted, startppqpos+k*diff_01, 0xB0, chan, cc_number, linear)
+            reaper.MIDI_InsertCC(take, selected, muted, endppqpos-tick/24-diff_01*diff_02*0.4, 0xB0, chan, cc_number, fast_end)
           end
         end
       end
@@ -46,20 +60,17 @@ function LRCE()
 end
 
 function main()
-  cc_num = reaper.GetExtState("LinearRampCCEventsMulti", "Number")
-  min_val = reaper.GetExtState("LinearRampCCEventsMulti", "Begin")
-  max_val = reaper.GetExtState("LinearRampCCEventsMulti", "End")
-  if (cc_num == "") then cc_num = "11" end
+  min_val = reaper.GetExtState("AutoExpression", "Begin")
+  max_val = reaper.GetExtState("AutoExpression", "End")
   if (min_val == "") then min_val = "90" end
   if (max_val == "") then max_val = "127" end
-  local user_ok, user_input_csv = reaper.GetUserInputs("Linear Ramp CC Events", 3, "CC number,Min value,Max value", cc_num..','..min_val..','.. max_val)
-  cc_num, min_val, max_val = user_input_csv:match("(.*),(.*),(.*)")
-  if not user_ok or not tonumber(cc_num) or not tonumber(min_val) or not tonumber(max_val)then return reaper.SN_FocusMIDIEditor() end
-  cc_num, min_val, max_val = tonumber(cc_num), tonumber(min_val), tonumber(max_val)
+  local user_ok, user_input_csv = reaper.GetUserInputs("自动表情(多轨)", 2, "最小值,最大值", min_val..','.. max_val)
+  min_val, max_val = user_input_csv:match("(.*),(.*),(.*)")
+  if not user_ok or not tonumber(min_val) or not tonumber(max_val) then return reaper.SN_FocusMIDIEditor() end
+  min_val, max_val = tonumber(min_val), tonumber(max_val)
   if min_val < 1 or max_val > 127 or min_val >= max_val then return reaper.SN_FocusMIDIEditor() end
-  reaper.SetExtState("LinearRampCCEventsMulti", "Number", cc_num, false)
-  reaper.SetExtState("LinearRampCCEventsMulti", "Begin", min_val, false)
-  reaper.SetExtState("LinearRampCCEventsMulti", "End", max_val, false)
+  reaper.SetExtState("AutoExpression", "Begin", min_val, false)
+  reaper.SetExtState("AutoExpression", "End", max_val, false)
 
   count_sel_items = reaper.CountSelectedMediaItems(0)
 
@@ -69,19 +80,19 @@ function main()
       take = reaper.GetTake(item, 0)
       if not take or not reaper.TakeIsMIDI(take) then return end
       reaper.MIDI_DisableSort(take)
-      LRCE()
+      autoExp()
       reaper.MIDI_Sort(take)
     end
   else
     take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
     if not take or not reaper.TakeIsMIDI(take) then return end
     reaper.MIDI_DisableSort(take)
-    LRCE()
+    autoExp()
     reaper.MIDI_Sort(take)
   end
 end
 
-script_title = "Linear Ramp CC Events (Multitrack)"
+script_title = "Auto Expression (Multitrack)"
 reaper.PreventUIRefresh(1) -- 防止UI刷新
 reaper.Undo_BeginBlock() -- 撤销块开始
 main() -- 执行函数
