9c9f3ce20eea90befec1b60f3c9f8932d56def00
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2020-09-04 02:20:11 +0800

    Update zaibuyidao_Duplicate Events To Edit Cursor (Multitrack).lua

--- MIDI Editor/zaibuyidao_Duplicate Events To Edit Cursor (Multitrack).lua
+++ MIDI Editor/zaibuyidao_Duplicate Events To Edit Cursor (Multitrack).lua
@@ -1,7 +1,7 @@
 --[[
  * ReaScript Name: Duplicate Events To Edit Cursor (Multitrack)
  * Instructions: Open a MIDI take in MIDI Editor. Select Notes or CC Events. Run.
- * Version: 1.1
+ * Version: 1.2
  * Author: zaibuyidao
  * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
  * Repository: GitHub > zaibuyidao > ReaScripts
@@ -38,7 +38,7 @@ function table_min(t)
     return mn
 end
 
-function CountNote()
+function CountAllSelEvents() -- 对所有选中的音符和CC进行遍历
     note_cnt, note_idx = 0, {}
     note_val = reaper.MIDI_EnumSelNotes(take, -1)
     while note_val ~= -1 do
@@ -46,15 +46,13 @@ function CountNote()
         note_idx[note_cnt] = note_val
         note_val = reaper.MIDI_EnumSelNotes(take, note_val)
     end
-    
+    if #note_idx > 0 then isDuplicateNotes = true end -- 未选中轨道并且只有MIDI编辑器被激活。只要音符选择数大于0，就需要复制音符
     start_ppq = {}
     end_ppq = {}
     for i = 1, #note_idx do
         _, sel, _, start_ppq[i], end_ppq[i], _, _, _ = reaper.MIDI_GetNote(take, note_idx[i])
     end
-end
 
-function CountCC()
     ccs_cnt, ccs_idx = 0, {}
     ccs_val = reaper.MIDI_EnumSelCC(take, -1)
     while ccs_val ~= -1 do
@@ -62,18 +60,18 @@ function CountCC()
         ccs_idx[ccs_cnt] = ccs_val
         ccs_val = reaper.MIDI_EnumSelCC(take, ccs_val)
     end
-    
+    if #ccs_idx > 0 then isDuplicateCCs = true end -- 未选中轨道并且只有MIDI编辑器被激活。只要cc选择数大于0，就需要复制CC
     ppqpos = {}
     for i = 1, #ccs_idx do
         _, sel, _, ppqpos[i], _, _, _, _ = reaper.MIDI_GetCC(take, ccs_idx[i])
     end
 end
 
-function DuplicateNotes()
-    local note_dur = math.floor(0.5 + cur_pos - table_min(start_ppq))
+function DuplicateNotes(m) -- 最小位置通过参数m传入
+    local note_dur = math.floor(0.5 + cur_pos - m) -- 长度补偿，将光标位置减去所有选中音符起始位置的最小值
     for i = 1, #note_idx do
         local retval, selected, muted, startppqpos, endppqpos, chan, pitch, vel = reaper.MIDI_GetNote(take, note_idx[i])
-        local start_meas = table_min(start_ppq)
+        local start_meas = m
         local start_tick = startppqpos - start_meas
         local tick_01 = start_tick % table_max(end_ppq)
 
@@ -84,12 +82,12 @@ function DuplicateNotes()
     end
 end
 
-function DuplicateCCs()
-    local cc_dur = math.floor(0.5 + cur_pos - table_min(ppqpos))
+function DuplicateCCs(m) -- 最小位置通过参数m传入
+    local cc_dur = math.floor(0.5 + cur_pos - m) -- 长度补偿，将光标位置减去所有选中CC起始位置的最小值
     for i = 1, #ccs_idx do
         local retval, selected, muted, cc_pos, chanmsg, chan, msg2, msg3 = reaper.MIDI_GetCC(take, ccs_idx[i])
         local _, shape, beztension = reaper.MIDI_GetCCShape(take, ccs_idx[i])
-        local cc_meas = table_min(ppqpos)
+        local cc_meas = m
         local cc_tick = cc_pos - cc_meas
         local tick_02 = cc_tick % table_max(ppqpos)
 
@@ -102,80 +100,93 @@ function DuplicateCCs()
     end
 end
 
-function DuplicateMix()
-    local mix_start
-    local mix_end
-    if table_min(start_ppq) > table_min(ppqpos) then mix_start = table_min(ppqpos) elseif table_min(start_ppq) < table_min(ppqpos) then mix_start = table_min(start_ppq) elseif table_min(start_ppq) == table_min(ppqpos) then mix_start = table_min(start_ppq) end
-    if table_max(end_ppq) > table_max(ppqpos) then mix_end = table_max(end_ppq) elseif table_max(end_ppq) < table_max(ppqpos) then mix_end = table_max(ppqpos) elseif table_max(end_ppq) == table_max(ppqpos) then mix_end = table_max(end_ppq) end
-    local mix_dur = math.floor(0.5 + (cur_pos - mix_start))
+isDuplicateNotesMulti = false -- 是否需要复制音符(仅针对选中的轨道)
+isDuplicateCCsMulti = false -- 是否需要复制CC(仅针对选中的轨道)
+isDuplicateNotes = false -- 是否需要复制音符(仅针对激活的MIDI编辑器)
+isDuplicateCCs = false -- 是否需要复制CC(仅针对激活的MIDI编辑器)
+
+script_title = "Duplicate Events To Edit Cursor (Multitrack)"
+count_sel_items = reaper.CountSelectedMediaItems(0)
+reaper.Undo_BeginBlock()
+
+min_start_ppq = math.huge -- 包含多轨 start_ppq 最小值
+min_ppqpos = math.huge -- 包含多轨 ppqpos 最小值
+
+for i = 1, count_sel_items do -- 对全部选中轨道的音符和CC进行遍历
+    item = reaper.GetSelectedMediaItem(0, i - 1)
+    take = reaper.GetTake(item, 0)
+    note_cnt, note_idx = 0, {}
+    note_val = reaper.MIDI_EnumSelNotes(take, -1)
+    while note_val ~= -1 do
+        note_cnt = note_cnt + 1
+        note_idx[note_cnt] = note_val
+        note_val = reaper.MIDI_EnumSelNotes(take, note_val)
+    end
+    if #note_idx > 0 then isDuplicateNotesMulti = true end -- 只要有一轨音符选择数大于0，就需要复制音符
+    start_ppq = {} -- 获得音符开头位置
+    end_ppq = {} -- 获得音符结尾位置
     for i = 1, #note_idx do
-        local retval, selected, muted, startppqpos, endppqpos, chan, pitch, vel = reaper.MIDI_GetNote(take, note_idx[i])
-        local start_meas = table_min(start_ppq)
-        local start_tick = startppqpos - start_meas
-        local tick_01 = start_tick % table_max(end_ppq)
-
-        reaper.MIDI_InsertNote(take, true, muted, startppqpos + mix_dur, endppqpos + mix_dur, chan, pitch, vel, false)
-        if not (tick_01 > table_max(end_ppq)) then
-            reaper.MIDI_SetNote(take, note_idx[i], false, nil, nil, nil, nil, nil, nil, false)
-        end
+        _, sel, _, start_ppq[i], end_ppq[i], _, _, _ = reaper.MIDI_GetNote(take, note_idx[i])
     end
+    min_start_ppq = table_min({min_start_ppq, table_min(start_ppq)})
+
+    ccs_cnt, ccs_idx = 0, {}
+    ccs_val = reaper.MIDI_EnumSelCC(take, -1)
+    while ccs_val ~= -1 do
+        ccs_cnt = ccs_cnt + 1
+        ccs_idx[ccs_cnt] = ccs_val
+        ccs_val = reaper.MIDI_EnumSelCC(take, ccs_val)
+    end
+    if #ccs_idx > 0 then isDuplicateCCsMulti = true end -- 只要有一轨CC选择数大于0，就需要复制cc
+    ppqpos = {} -- 获得CC位置
     for i = 1, #ccs_idx do
-        local retval, selected, muted, cc_pos, chanmsg, chan, msg2, msg3 = reaper.MIDI_GetCC(take, ccs_idx[i])
-        local _, shape, beztension = reaper.MIDI_GetCCShape(take, ccs_idx[i])
-        local cc_meas = table_min(ppqpos)
-        local cc_tick = cc_pos - cc_meas
-        local tick_02 = cc_tick % table_max(ppqpos)
-
-        reaper.MIDI_InsertCC(take, true, muted, cc_pos + mix_dur, chanmsg, chan, msg2, msg3)
-        if not (tick_02 > table_max(ppqpos)) then
-            reaper.MIDI_SetCC(take, ccs_idx[i], false, nil, nil, nil, nil, nil, nil, false)
-        end
-        ccevtcnt = ccevtcnt + 1
-        reaper.MIDI_SetCCShape(take, ccevtcnt, shape, beztension, false)
+        _, sel, _, ppqpos[i], _, _, _, _ = reaper.MIDI_GetCC(take, ccs_idx[i])
     end
+    min_ppqpos = table_min({min_ppqpos, table_min(ppqpos)})
 end
 
-title = "Duplicate Events To Edit Cursor (Multitrack)"
-count_sel_items = reaper.CountSelectedMediaItems(0)
-
-reaper.Undo_BeginBlock()
 if count_sel_items > 0 then
+    -- Msg("count_sel_items > 0")
     for i = 1, count_sel_items do
         item = reaper.GetSelectedMediaItem(0, i - 1)
         take = reaper.GetTake(item, 0)
         if not take or not reaper.TakeIsMIDI(take) then return end
-        CountNote()
-        CountCC()
+        CountAllSelEvents()
         cur_pos = reaper.MIDI_GetPPQPosFromProjTime(take, reaper.GetCursorPositionEx(0))
         _, notecnt, ccevtcnt, textsyxevtcnt = reaper.MIDI_CountEvts(take)
         ccevtcnt = ccevtcnt - 1
         reaper.MIDI_DisableSort(take)
-        if #note_idx > 0 and #ccs_idx == 0 then 
-            DuplicateNotes()
-        elseif #ccs_idx > 0 and #note_idx == 0 then
-            DuplicateCCs()
-        elseif #ccs_idx > 0 and #note_idx > 0 then
-            DuplicateMix()
+        if isDuplicateNotesMulti and not isDuplicateCCsMulti then -- 只复制音符
+            DuplicateNotes(min_start_ppq)
+        elseif isDuplicateCCsMulti and not isDuplicateNotesMulti then -- 只复制CC
+            DuplicateCCs(min_ppqpos)
+        elseif isDuplicateCCsMulti and isDuplicateNotesMulti then -- 同时复制
+            local m = table_min{min_ppqpos, min_start_ppq}
+            DuplicateNotes(m)
+            DuplicateCCs(m)
         end
         reaper.MIDI_Sort(take)
     end
 else
+    -- Msg("count_sel_items == 0")
     take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
     if not take or not reaper.TakeIsMIDI(take) then return end
-    CountNote()
-    CountCC()
+    CountAllSelEvents()
     cur_pos = reaper.MIDI_GetPPQPosFromProjTime(take, reaper.GetCursorPositionEx(0))
     _, notecnt, ccevtcnt, textsyxevtcnt = reaper.MIDI_CountEvts(take)
     ccevtcnt = ccevtcnt - 1
     reaper.MIDI_DisableSort(take)
-    if #note_idx > 0 and #ccs_idx == 0 then 
-        DuplicateNotes()
-    elseif #ccs_idx > 0 and #note_idx == 0 then
-        DuplicateCCs()
-    elseif #ccs_idx > 0 and #note_idx > 0 then
-        DuplicateMix()
+    if isDuplicateNotes and not isDuplicateCCs then -- 只复制音符
+        DuplicateNotes(table_min(start_ppq))
+    elseif isDuplicateCCs and not isDuplicateNotes then -- 只复制CC
+        DuplicateCCs(table_min(ppqpos))
+    elseif isDuplicateCCs and isDuplicateNotes then -- 同时复制
+        local m = table_min{table_min(ppqpos), table_min(start_ppq)}
+        DuplicateNotes(m)
+        DuplicateCCs(m)
     end
     reaper.MIDI_Sort(take)
 end
-reaper.Undo_EndBlock(title, 0)
+
+reaper.Undo_EndBlock(script_title, 0)
 reaper.UpdateArrange()
