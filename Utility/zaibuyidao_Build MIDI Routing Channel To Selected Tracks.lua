552f7edda3e58d546afd5274653977aefc3628d0
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2020-11-19 03:25:55 +0800

    Add Scripts

---
+++ Utility/zaibuyidao_Build MIDI Routing Channel To Selected Tracks.lua
@@ -0,0 +1,68 @@
+--[[
+ * ReaScript Name: Build MIDI Routing Channel To Selected Tracks
+ * Version: 1.0
+ * Author: zaibuyidao
+ * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+ * Repository: GitHub > zaibuyidao > ReaScripts
+ * Repository URI: https://github.com/zaibuyidao/ReaScripts
+ * REAPER: 6.0
+--]]
+
+--[[
+ * Changelog:
+ * v1.0 (2020-11-19)
+  + Initial release
+--]]
+
+function Msg(param) reaper.ShowConsoleMsg(tostring(param) .. "\n") end
+
+function main()
+    count_sel_track = reaper.CountSelectedTracks(0)
+    if count_sel_track == 0 then return end
+
+    for i = 0, count_sel_track-1 do
+        select_track = reaper.GetSelectedTrack(0, i)
+        select_track_num = reaper.GetMediaTrackInfo_Value(select_track,'IP_TRACKNUMBER')
+        isVSTi = reaper.TrackFX_GetInstrument(select_track) -- 判斷是否為VSTi
+        if isVSTi == -1 then goto continue end
+
+        local channel_total = reaper.GetExtState("BuildMIDIRoutingChannel", "Total")
+        if (channel_total == "") then channel_total = "16" end
+        local channel_ordinal = reaper.GetExtState("BuildMIDIRoutingChannel", "Ordinal")
+        if (channel_ordinal == "") then channel_ordinal = "1" end
+        
+        _, get_track_name = reaper.GetTrackName(select_track, "")
+        user_ok, input_cav = reaper.GetUserInputs("Build MIDI Routing To " .. get_track_name, 2, "Total number of channels,Channel ordinal", channel_total ..','.. channel_ordinal)
+        channel_total, channel_ordinal = input_cav:match("(.*),(.*)")
+        if not user_ok or not tonumber(channel_total) or not tonumber(channel_ordinal) then return end
+        channel_total, channel_ordinal = tonumber(channel_total), tonumber(channel_ordinal)
+
+        reaper.SetExtState("BuildMIDIRoutingChannel", "Total", channel_total, false)
+        reaper.SetExtState("BuildMIDIRoutingChannel", "Ordinal", channel_ordinal, false)
+
+        reaper.Undo_BeginBlock()
+        for j = 1, channel_total do
+            reaper.InsertTrackAtIndex((select_track_num-1)+j, false) -- 插入軌道
+            track_to_send = reaper.GetTrack(0,(select_track_num-1)+j) -- 建立MIDI路由軌道
+            name_ok, track_name = reaper.GetSetMediaTrackInfo_String(select_track, 'P_NAME', '', 0) -- 獲取軌道名稱
+            if track_name ~= '' then track_name = track_name .. ' ' end
+            
+            local channel = (channel_ordinal-1)+j
+            reaper.GetSetMediaTrackInfo_String(track_to_send, 'P_NAME', track_name ..'MIDI '.. channel, true) -- 設置軌道名稱
+            reaper.SetMediaTrackInfo_Value(track_to_send, "B_MAINSEND", 0) -- 禁用主/父發送
+            reaper.CreateTrackSend(track_to_send, select_track)
+
+            reaper.SetTrackSendInfo_Value(track_to_send, 0, 0, 'I_SRCCHAN', -1)
+
+            if channel < 1 or channel > 16 then channel = 0 end
+            if channel == '0' then channel = 'All' end
+            reaper.SetTrackSendInfo_Value(track_to_send, 0, 0, 'I_MIDIFLAGS', channel << 5)
+        end
+        reaper.Undo_EndBlock("Build MIDI Routing Channel To Selected Tracks", 0)
+        ::continue::
+    end
+end
+reaper.PreventUIRefresh(1)
+main()
+reaper.PreventUIRefresh(-1)
+reaper.UpdateArrange()
