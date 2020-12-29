d297406bcb2bf79c813915a6d321e6668bb7ffcb
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2020-12-30 01:25:47 +0800

    Update

--- MIDI Editor/zaibuyidao_MIDI Item List (Dynamic Menu).lua
+++ MIDI Editor/zaibuyidao_MIDI Item List (Dynamic Menu).lua
@@ -1,5 +1,5 @@
 --[[
- * ReaScript Name: MIDI Item List
+ * ReaScript Name: MIDI Item List (Dynamic Menu)
  * Version: 1.0
  * Author: zaibuyidao
  * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
