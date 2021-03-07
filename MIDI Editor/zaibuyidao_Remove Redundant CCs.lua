7c1707659ae33717ab6d702b482394748aa7a79b
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2021-03-07 18:12:42 +0800

    Update zaibuyidao_Remove Redundant CCs.lua

--- MIDI Editor/zaibuyidao_Remove Redundant CCs.lua
+++ MIDI Editor/zaibuyidao_Remove Redundant CCs.lua
@@ -1,6 +1,6 @@
 --[[
  * ReaScript Name: Remove Redundant CCs
- * Version: 1.0
+ * Version: 1.1
  * Author: zaibuyidao
  * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
  * Repository: GitHub > zaibuyidao > ReaScripts
@@ -15,7 +15,8 @@
 --]]
 
 function Msg(param) reaper.ShowConsoleMsg(tostring(param) .. "\n") end
-local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
+local editor = reaper.MIDIEditor_GetActive()
+local take = reaper.MIDIEditor_GetTake(editor)
 local cnt, index = 0, {}
 local val = reaper.MIDI_EnumSelCC(take, -1)
 while val ~= - 1 do
@@ -23,24 +24,17 @@ while val ~= - 1 do
     index[cnt] = val
     val = reaper.MIDI_EnumSelCC(take, val)
 end
-local threshold, idx, tc, tp = 0, -1, 0, 0
+local idx, tc, tp = -1, "", ""
 reaper.Undo_BeginBlock()
 reaper.PreventUIRefresh(1)
 reaper.MIDI_DisableSort(take)
 for i = 1, #index do
     index[i] = reaper.MIDI_EnumSelCC(take, idx)
     retval, selected, muted, ppqpos, chanmsg, chan, msg2, msg3 = reaper.MIDI_GetCC(take, index[i])
-    pitchbend = 128*msg3+msg2
-    diffc = msg3-tc
-    diffp = pitchbend-tp
-    if diffc < 0 then diffc = math.abs(diffc) end
-    if diffp < 0 then diffp = math.abs(diffp) end
-    --Msg(index[i] ..' A: '.. diffc ..' B: '..msg3.." C: "..tc)
-    --Msg(index[i] ..' A: '.. diffp ..' B: '..pitchbend.." C: "..tp)
-    if chanmsg == 176 and diffc <= threshold then
-        --Msg(index[i].." ----------------")
+    pitchbend = 128 * msg3 + msg2
+    if chanmsg == 176 and tc == msg3 then
         reaper.MIDI_DeleteCC(take, index[i])
-    elseif chanmsg == 224 and diffp <= threshold then
+    elseif chanmsg == 224 and tp == pitchbend then
         reaper.MIDI_DeleteCC(take, index[i])
     else
         tc = msg3
