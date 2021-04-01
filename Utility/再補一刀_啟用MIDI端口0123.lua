566ce4c782d3bac075dcc563bac276d7d2bd989c
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2021-04-01 17:52:34 +0800

    Update 再補一刀_啟用MIDI端口0123.lua

--- Utility/再補一刀_啟用MIDI端口0123.lua
+++ Utility/再補一刀_啟用MIDI端口0123.lua
@@ -1,6 +1,6 @@
 --[[
  * ReaScript Name: 啟用MIDI端口0123
- * Version: 1.0
+ * Version: 1.0.1
  * Author: zaibuyidao
  * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
  * Repository: GitHub > zaibuyidao > ReaScripts
@@ -11,12 +11,14 @@
 
 --[[
  * Changelog:
+ * v1.0.1 (2021-4-1)
+  + 文字描述修正
  * v1.0 (2021-4-1)
   + Initial release
 --]]
 
 function Msg(param) reaper.ShowConsoleMsg(tostring(param) .. "\n") end
-reaper.MB("ID 0 = SOUND Canvas VA [A]\nID 1 = SOUND Canvas VA [B]\nID 2 = PhoenixVSTi [A]\nID 3 = PhoenixVSTi [B]", "启用MIDI端口0123", 0)
+reaper.MB("ID 0 = SOUND Canvas VA [A]\nID 1 = SOUND Canvas VA [B]\nID 2 = PhoenixVSTi [A]\nID 3 = PhoenixVSTi [B]", "啟用MIDI端口0123", 0)
 local file = reaper.GetResourcePath() .. "\\" .. "reaper.ini"
 local f = io.open(file, "r")
 local content = f:read('*all')
