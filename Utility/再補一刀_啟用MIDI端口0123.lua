98aa3b474e065cbeab04e4caaab3b8bc7c3f9ba2
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2021-07-02 11:59:10 +0800

    Update 再補一刀_啟用MIDI端口0123.lua

--- Utility/再補一刀_啟用MIDI端口0123.lua
+++ Utility/再補一刀_啟用MIDI端口0123.lua
@@ -1,6 +1,6 @@
 --[[
  * ReaScript Name: 啟用MIDI端口0123
- * Version: 1.1
+ * Version: 1.2
  * Author: zaibuyidao
  * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
  * Repository: GitHub > zaibuyidao > ReaScripts
@@ -11,6 +11,8 @@
 
 --[[
  * Changelog:
+ * v1.2 (2021-7-2)
+  + 優化彈窗
  * v1.1 (2021-5-9)
   + 設置完成將自動退出REAPER
  * v1.0.1 (2021-4-1)
@@ -20,7 +22,11 @@
 --]]
 
 function Msg(param) reaper.ShowConsoleMsg(tostring(param) .. "\n") end
-reaper.MB("本操作將啟用0123端口，設置完畢後需重啟REAPER才會生效！\n", "注意", 0)
+script_name = "啟用MIDI端口0123"
+text = "本操作將啟用0123端口並退出REAPER，請確保項目已保存。\n設置完畢後請手動重啟REAPER！\n"
+text = text.."\n要繼續嗎？"
+local box_ok = reaper.ShowMessageBox("注意：\n\n"..text, script_name, 4)
+if box_ok == 7 then return end
 local file = reaper.GetResourcePath() .. "\\" .. "reaper.ini"
 local f = io.open(file, "r")
 local content = f:read('*all')
