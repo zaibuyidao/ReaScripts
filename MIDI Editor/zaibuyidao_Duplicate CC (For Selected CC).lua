902d620614b7ea5774a0d243346b139f47cdac1f
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2022-03-26 21:47:50 +0800

    Update

--- MIDI Editor/zaibuyidao_Duplicate CC (For Selected CC).lua
+++ MIDI Editor/zaibuyidao_Duplicate CC (For Selected CC).lua
@@ -1,6 +1,6 @@
 --[[
  * ReaScript Name: Duplicate CC (For Selected CC)
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: zaibuyidao
  * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
  * Repository: GitHub > zaibuyidao > ReaScripts
@@ -19,34 +19,50 @@ function Msg(param)
   reaper.ShowConsoleMsg(tostring(param) .. "\n")
 end
 
-function Main()
-  local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
-  if take == nil then return end
-  local _, notecnt, ccevtcnt, textsyxevtcnt = reaper.MIDI_CountEvts(take)
-  local cc_new = reaper.GetExtState("DuplicateCCForSelectedCC", "CC_New")
-  if (cc_new == "") then cc_new = "11" end
-  local user_ok, user_input_CSV = reaper.GetUserInputs('Duplicate CC (For Selected CC)', 1, 'Target CC', cc_new)
-  cc_new = user_input_CSV:match("(.*)")
-  if not user_ok or not tonumber(cc_new) then return reaper.SN_FocusMIDIEditor() end
-  cc_new = tonumber(cc_new)
-  if cc_new > 127 or cc_new < 0 then
-    return
-    reaper.MB("Please enter a value from 0 through 127", "Error", 0),
-    reaper.SN_FocusMIDIEditor()
+if not reaper.SN_FocusMIDIEditor then
+  local retval = reaper.ShowMessageBox("This script requires the SWS extension, would you like to download it now?\n這個脚本需要SWS擴展，你想現在就下載它嗎？", "Warning", 1)
+  if retval == 1 then
+    Open_URL("http://www.sws-extension.org/download/pre-release/")
   end
-  reaper.SetExtState("DuplicateCCForSelectedCC", "CC_New", cc_new, false)
-  reaper.MIDI_DisableSort(take)
-  for i = 1, ccevtcnt do
-    local _, selected, muted, ppqpos, chanmsg, chan, msg2, msg3 = reaper.MIDI_GetCC(take, i - 1)
-    if selected == true then
-      reaper.MIDI_InsertCC(take, selected, muted, ppqpos, chanmsg, chan, cc_new, msg3)
-    end
-  end
-  reaper.UpdateArrange()
-  reaper.MIDI_Sort(take)
 end
 
-reaper.Undo_BeginBlock()
+function Main()
+  local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
+  if not take or not reaper.TakeIsMIDI(take) then return end
+  local _, _, ccevtcnt, _ = reaper.MIDI_CountEvts(take)
+
+  local target = reaper.GetExtState("DuplicateCCForSelectedCC", "Target")
+  if (target == "") then target = "11" end
+
+  local retval, retvals_csv = reaper.GetUserInputs('Duplicate CC (For Selected CC)', 1, 'Target CC', target)
+  target = retvals_csv:match("(.*)")
+  if not retval or not tonumber(target) then return reaper.SN_FocusMIDIEditor() end
+  target = tonumber(target)
+
+  if target > 127 or target < 0 then
+    return
+    reaper.MB("請輸入從0到127的數值", "錯誤", 0),
+    reaper.SN_FocusMIDIEditor()
+  end
+
+  reaper.SetExtState("DuplicateCCForSelectedCC", "Target", target, false)
+  
+  reaper.Undo_BeginBlock()
+  reaper.MIDI_DisableSort(take)
+  for i = 0, ccevtcnt - 1 do
+    local _, selected, muted, ppqpos, chanmsg, chan, msg2, msg3 = reaper.MIDI_GetCC(take, i)
+    local _, shape, beztension = reaper.MIDI_GetCCShape(take, i)
+
+    if selected == true then
+      ccevtcnt = ccevtcnt + 1
+      reaper.MIDI_InsertCC(take, selected, muted, ppqpos, chanmsg, chan, target, msg3)
+      reaper.MIDI_SetCCShape(take, ccevtcnt - 1, shape, beztension, false)
+    end
+  end
+  reaper.MIDI_Sort(take)
+  reaper.Undo_EndBlock("Duplicate CC (For Selected CC)", -1)
+  reaper.UpdateArrange()
+end
+
 Main()
-reaper.Undo_EndBlock("Duplicate CC (For Selected CC)", -1)
 reaper.SN_FocusMIDIEditor()
--- MIDI Editor/zaibuyidao_Duplicate CC.lua
+++ MIDI Editor/zaibuyidao_Duplicate CC.lua
@@ -1,6 +1,6 @@
 --[[
  * ReaScript Name: Duplicate CC
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: zaibuyidao
  * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
  * Repository: GitHub > zaibuyidao > ReaScripts
@@ -19,41 +19,53 @@ function Msg(param)
   reaper.ShowConsoleMsg(tostring(param) .. "\n")
 end
 
-function Main()
+if not reaper.SN_FocusMIDIEditor then
+  local retval = reaper.ShowMessageBox("This script requires the SWS extension, would you like to download it now?\n這個脚本需要SWS擴展，你想現在就下載它嗎？", "Warning", 1)
+  if retval == 1 then
+    Open_URL("http://www.sws-extension.org/download/pre-release/")
+  end
+end
+
+function main()
   local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
-  if take == nil then return end
-  local _, notecnt, ccevtcnt, textsyxevtcnt = reaper.MIDI_CountEvts(take)
-  local cc_num = reaper.GetExtState("DuplicateCC", "CC_Number")
-  if (cc_num == "") then cc_num = "1" end
-  local cc_new = reaper.GetExtState("DuplicateCC", "CC_New")
-  if (cc_new == "") then cc_new = "11" end
-  local user_ok, user_input = reaper.GetUserInputs('Duplicate CC', 2, 'Source CC,Target CC', cc_num ..','.. cc_new)
-  cc_num, cc_new = user_input:match("(.*),(.*)")
-  if not user_ok or not tonumber(cc_num) or not tonumber(cc_new) then return reaper.SN_FocusMIDIEditor() end
+  if not take or not reaper.TakeIsMIDI(take) then return end
+  local _, _, ccevtcnt, _ = reaper.MIDI_CountEvts(take)
 
-  cc_num, cc_new = tonumber(cc_num), tonumber(cc_new)
+  local source = reaper.GetExtState("DuplicateCC", "Source")
+  if (source == "") then source = "1" end
+  local target = reaper.GetExtState("DuplicateCC", "Target")
+  if (target == "") then target = "11" end
 
-  if cc_num > 127 or cc_num < 0 or cc_new > 127 or cc_new < 0 then
+  local retval, retvals_csv = reaper.GetUserInputs('Duplicate CC', 2, 'Source CC,Target CC', source ..','.. target)
+  source, target = retvals_csv:match("(.*),(.*)")
+  if not retval or not tonumber(source) or not tonumber(target) then return reaper.SN_FocusMIDIEditor() end
+  source, target = tonumber(source), tonumber(target)
+
+  if source > 127 or source < 0 or target > 127 or target < 0 then
     return
-    reaper.MB("Please enter a value from 0 through 127", "Error", 0),
+    reaper.MB("請輸入從0到127的數值", "錯誤", 0),
     reaper.SN_FocusMIDIEditor()
   end
 
-  reaper.SetExtState("DuplicateCC", "CC_Number", cc_num, false)
-  reaper.SetExtState("DuplicateCC", "CC_New", cc_new, false)
-
+  reaper.SetExtState("DuplicateCC", "Source", source, false)
+  reaper.SetExtState("DuplicateCC", "Target", target, false)
+  
+  reaper.Undo_BeginBlock()
   reaper.MIDI_DisableSort(take)
-  for i = 1, ccevtcnt do
-    _, selected, muted, ppqpos, chanmsg, chan, msg2, msg3 = reaper.MIDI_GetCC(take, i - 1)
-    if msg2 == cc_num then
-      reaper.MIDI_InsertCC(take, selected, muted, ppqpos, chanmsg, chan, cc_new, msg3)
+  for i = 0, ccevtcnt - 1 do
+    local _, selected, muted, ppqpos, chanmsg, chan, msg2, msg3 = reaper.MIDI_GetCC(take, i)
+    local _, shape, beztension = reaper.MIDI_GetCCShape(take, i)
+    
+    if msg2 == source then
+      ccevtcnt = ccevtcnt + 1
+      reaper.MIDI_InsertCC(take, selected, muted, ppqpos, chanmsg, chan, target, msg3)
+      reaper.MIDI_SetCCShape(take, ccevtcnt - 1, shape, beztension, false)
     end
   end
-  reaper.UpdateArrange()
   reaper.MIDI_Sort(take)
+  reaper.Undo_EndBlock("Duplicate CC", -1)
+  reaper.UpdateArrange()
 end
 
-reaper.Undo_BeginBlock()
-Main()
-reaper.Undo_EndBlock("Duplicate CC", -1)
+main()
 reaper.SN_FocusMIDIEditor()
