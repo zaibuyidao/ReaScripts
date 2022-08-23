12934f35140cfed386f1a181f0395910c1e894a1
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2022-08-23 16:55:09 +0800

    Update

--- MIDI Editor/再補一刀_插入彎音(半音).lua
+++ MIDI Editor/再補一刀_插入彎音(半音).lua
@@ -1,69 +1,143 @@
---[[
- * ReaScript Name: 插入彎音(半音)
- * Version: 1.4
- * Author: 再補一刀
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository: GitHub > zaibuyidao > ReaScripts
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * REAPER: 6.0
- * Donation: http://www.paypal.me/zaibuyidao
---]]
+-- @description 插入彎音(半音)
+-- @version 1.4.1
+-- @author 再補一刀
+-- @changelog 優化彎音
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires SWS Extensions
 
---[[
- * Changelog:
- * v1.0 (2019-12-12)
-  + Initial release
---]]
+function print(...)
+  local params = {...}
+  for i = 1, #params do
+    if i ~= 1 then reaper.ShowConsoleMsg(" ") end
+    reaper.ShowConsoleMsg(tostring(params[i]))
+  end
+  reaper.ShowConsoleMsg("\n")
+end
+
+function table.print(t)
+  local print_r_cache = {}
+  local function sub_print_r(t, indent)
+    if (print_r_cache[tostring(t)]) then
+      print(indent .. "*" .. tostring(t))
+    else
+      print_r_cache[tostring(t)] = true
+      if (type(t) == "table") then
+        for pos, val in pairs(t) do
+          if (type(val) == "table") then
+            print(indent .. "[" .. tostring(pos) .. "] => " .. tostring(t) .. " {")
+            sub_print_r(val, indent .. string.rep(" ", string.len(tostring(pos)) + 8))
+            print(indent .. string.rep(" ", string.len(tostring(pos)) + 6) .. "}")
+          elseif (type(val) == "string") then
+            print(indent .. "[" .. tostring(pos) .. '] => "' .. val .. '"')
+          else
+            print(indent .. "[" .. tostring(pos) .. "] => " .. tostring(val))
+          end
+        end
+      else
+        print(indent .. tostring(t))
+      end
+    end
+  end
+  if (type(t) == "table") then
+    print(tostring(t) .. " {")
+    sub_print_r(t, "  ")
+    print("}")
+  else
+    sub_print_r(t, "  ")
+  end
+end
+
+function open_url(url)
+  if not OS then local OS = reaper.GetOS() end
+  if OS=="OSX32" or OS=="OSX64" then
+    os.execute("open ".. url)
+  else
+    os.execute("start ".. url)
+  end
+end
+
+if not reaper.SN_FocusMIDIEditor then
+  local retval = reaper.ShowMessageBox("這個脚本需要SWS擴展，你想現在就下載它嗎？", "Warning", 1)
+  if retval == 1 then
+    open_url("http://www.sws-extension.org/download/pre-release/")
+  end
+end
 
 local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
 local pos = reaper.GetCursorPositionEx(0)
 local ppq = reaper.MIDI_GetPPQPosFromProjTime(take, pos)
+
 local pitch = reaper.GetExtState("InsertPitchBendSemitone", "Pitch")
 if (pitch == "") then pitch = "0" end
-local user_ok, user_input_csv = reaper.GetUserInputs('插入彎音', 1, '半音', pitch)
-if not user_ok then return reaper.SN_FocusMIDIEditor() end
-pitch = user_input_csv:match("(.*)")
-if not tonumber(pitch) then return reaper.SN_FocusMIDIEditor() end
-reaper.SetExtState("InsertPitchBendSemitone", "Pitch", pitch, false)
+local range = reaper.GetExtState("InsertPitchBendSemitone", "Range")
+if (range == "") then range = "12" end
+local uok, uinput = reaper.GetUserInputs('插入彎音', 2, '半音(0=復位),音高範圍', pitch ..','.. range)
+if not uok then return reaper.SN_FocusMIDIEditor() end
+pitch, range = uinput:match("(.*),(.*)")
+if not tonumber(pitch) or not tonumber(range) then return reaper.SN_FocusMIDIEditor() end
+pitch, range = tonumber(pitch), tonumber(range)
 
-pitch = tonumber(pitch)
 if pitch > 12 or pitch < -12 then
-    return reaper.MB("請輸入一個介於-12到12之間的值", "錯誤", 0),
-           reaper.SN_FocusMIDIEditor()
+  return reaper.MB("請輸入一個介於 -12 到 12 之間的值", "錯誤", 0), reaper.SN_FocusMIDIEditor()
 end
 
-tbl = {}
-tbl["12"] = "8191"
-tbl["11"] = "7513"
-tbl["10"] = "6830"
-tbl["9"] = "6147"
-tbl["8"] = "5464"
-tbl["7"] = "4781"
-tbl["6"] = "4098"
-tbl["5"] = "3415"
-tbl["4"] = "2732"
-tbl["3"] = "2049"
-tbl["2"] = "1366"
-tbl["1"] = "683"
-tbl["0"] = "0"
-tbl["-1"] = "-683"
-tbl["-2"] = "-1366"
-tbl["-3"] = "-2049"
-tbl["-4"] = "-2732"
-tbl["-5"] = "-3415"
-tbl["-6"] = "-4098"
-tbl["-7"] = "-4781"
-tbl["-8"] = "-5464"
-tbl["-9"] = "-6147"
-tbl["-10"] = "-6830"
-tbl["-11"] = "-7513"
-tbl["-12"] = "-8192"
+if pitch > range then
+  return reaper.MB("彎音間隔不能大於彎音範圍", "錯誤", 0), reaper.SN_FocusMIDIEditor()
+end
+
+reaper.SetExtState("InsertPitchBendSemitone", "Pitch", pitch, false)
+reaper.SetExtState("InsertPitchBendSemitone", "Range", range, false)
+
+function getSegments(n)
+  local x = 8192
+  local p = math.floor((x / n) + 0.5) -- 四舍五入
+  local arr = {}
+  local cur = 0
+  for i = 1, n do
+    cur = cur + p
+    table.insert(arr, math.min(cur, x))
+  end
+  local res = {}
+  for i = #arr, 1, -1 do
+    table.insert(res, -arr[i])
+  end
+  table.insert(res, 0)
+  for i = 1, #arr do
+    table.insert(res, arr[i])
+  end
+  res[#res] = 8191 -- 将最后一个点强制设为8191，否则8192会被reaper处理为-8192
+  return res
+end
+
+function pitchUp(o, targets)
+  if #targets == 0 then error() end
+  for i = 1, #targets do
+    return targets[o + (range + 1)]
+  end
+end
+
+function pitchDown(p, targets)
+  if #targets == 0 then error() end
+  for i = #targets, 1, -1 do
+    return targets[p + (range + 1)]
+  end
+end
 
 reaper.Undo_BeginBlock()
-local value = tonumber(tbl[tostring(pitch)])
-value = value + 8192
-local LSB = value & 0x7f
-local MSB = value >> 7 & 0x7f
+local seg = getSegments(range)
+
+if pitch > 0 then
+  pitchbend = pitchUp(pitch, seg)
+else
+  pitchbend = pitchDown(pitch, seg)
+end
+
+LSB = pitchbend & 0x7F
+MSB = (pitchbend >> 7) + 64
+
 reaper.MIDI_InsertCC(take, false, false, ppq, 224, 0, LSB, MSB)
 reaper.Undo_EndBlock("插入彎音(半音)", -1)
 reaper.UpdateArrange()
