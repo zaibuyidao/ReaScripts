6213c04016af0b673563a7c54fc518e739a34102
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2022-08-30 23:42:42 +0800

    Update

--- MIDI Editor/zaibuyidao_Move Events Left 10 Ticks (Fast).lua
+++ MIDI Editor/zaibuyidao_Move Events Left 10 Ticks (Fast).lua
@@ -1,26 +1,34 @@
---[[
- * ReaScript Name: Move Events Left 10 Ticks (Fast)
- * Version: 1.0
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * Donation: http://www.paypal.me/zaibuyidao
---]]
+-- @description Move Events Left 10 Ticks (Fast)
+-- @version 1.0.1
+-- @author zaibuyidao
+-- @changelog Optimised articulation
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about
+--   # Move Events Left 10 Ticks (Fast)
 
---[[
- * Changelog:
- * v1.0 (2022-5-29)
-  + Initial release
---]]
+EVENT_NOTE_START = 9 -- Note-on 音符开
+EVENT_NOTE_END = 8 -- Note-off 音符关
+EVENT_AFTER_TOUCH = 10 -- After Touch 触后
+EVENT_CC = 11 -- Continuous Controller CC控制器
+EVENT_PROGRAM = 12 -- Program 程序选择
+EVENT_CHPRESS = 13 -- Channel Pressure 通道压力
+EVENT_PITCH = 14 -- Pitch Wheel 弯音事件
+EVENT_ARTICULATION = 15 -- Articulation 符号
 
-local ticks = -10
+--EVENT_BANKPROG = 11 and (msg:byte(2) == 0 or msg:byte(2) == 32) -- 音色库事件
+--EVENT_TEXT = msg:byte(1) == 0xFF and not (msg:byte(2) == 0x0F) -- 文本事件，排除 notation text 事件
+--EVENT_SYSEX = 0xF and not (msg:byte(1) == 0xFF) -- 系统码事件
 
-function print(param)
-  if type(param) == "table" then
-    table.print(param)
-    return
+function print(...)
+  local params = {...}
+  for i = 1, #params do
+    if i ~= 1 then reaper.ShowConsoleMsg(" ") end
+    reaper.ShowConsoleMsg(tostring(params[i]))
   end
-  reaper.ShowConsoleMsg(tostring(param) .. "\n")
+  reaper.ShowConsoleMsg("\n")
 end
 
 function table.print(t)
@@ -56,63 +64,79 @@ function table.print(t)
   end
 end
 
-function open_url(url)
-  if not OS then local OS = reaper.GetOS() end
-  if OS=="OSX32" or OS=="OSX64" then
-    os.execute("open ".. url)
-  else
-    os.execute("start ".. url)
-  end
-end
+function setAllEvents(take, events)
+  local headPos = math.huge -- 开头位置，用于扩展边界
+  local tailPos = 0 -- 末尾位置，用于扩展边界
+  local lastPos = 0 -- 上一次遍历到的位置
+  
+  -- local minPos = math.huge
+  -- for _, event in pairs(events) do minPos = math.min(minPos, event.pos) end
+  -- local lastPos = math.min(minPos, 0) -- 上一次遍历到的位置
 
-if not reaper.BR_GetMidiSourceLenPPQ then
-  local retval = reaper.ShowMessageBox("This script requires the SWS extension, would you like to download it now?\n\n這個脚本需要SWS擴展，你想現在就下載它嗎？", "Warning", 1)
-  if retval == 1 then
-    open_url("http://www.sws-extension.org/download/pre-release/")
+  for _, event in pairs(events) do
+    headPos = math.min(headPos, event.pos)
+    tailPos = math.max(tailPos, event.pos)
+    event.offset = event.pos - lastPos
+    lastPos = event.pos
   end
-end
+  
+  -- item边界扩展处理
+  events[#events].pos = math.max(events[#events].pos, tailPos)
+  local item = reaper.GetMediaItemTake_Item(take)
+  reaper.MIDI_SetItemExtents(
+    item, 
+    reaper.MIDI_GetProjQNFromPPQPos(take, math.min(0, headPos)), -- reaper.TimeMap2_timeToQN(0, reaper.GetMediaItemInfo_Value(item, "D_POSITION"))
+    reaper.MIDI_GetProjQNFromPPQPos(take, events[#events].pos)
+  )
 
-function getAllTakes()
-  tTake = {}
-  if reaper.MIDIEditor_EnumTakes then
-    local editor = reaper.MIDIEditor_GetActive()
-    for i = 0, math.huge do
-      take = reaper.MIDIEditor_EnumTakes(editor, i, false)
-      if take and reaper.ValidatePtr2(0, take, "MediaItem_Take*") then 
-        tTake[take] = true
-        tTake[take] = {item = reaper.GetMediaItemTake_Item(take)}
-      else
-        break
-      end
-    end
-  else
-    for i = 0, reaper.CountMediaItems(0)-1 do
-      local item = reaper.GetMediaItem(0, i)
-      local take = reaper.GetActiveTake(item)
-      if reaper.ValidatePtr2(0, take, "MediaItem_Take*") and reaper.TakeIsMIDI(take) and reaper.MIDI_EnumSelNotes(take, -1) == 0 then -- Get potential takes that contain notes. NB == 0 
-        tTake[take] = true
-      end
-    end
-    
-    for take in next, tTake do
-      if reaper.MIDI_EnumSelNotes(take, -1) ~= -1 then tTake[take] = nil end -- Remove takes that were not affected by deselection
-    end
+  local tab = {}
+  for _, event in pairs(events) do
+    table.insert(tab, string.pack("i4Bs4", event.offset, event.flags, event.msg))
   end
-  if not next(tTake) then return end
-  return tTake
+  reaper.MIDI_SetAllEvts(take, table.concat(tab))
 end
 
 function getAllEvents(take, onEach)
   local getters = {
-    selected = function (event) return event.flags & 1 == 1 end,
-    pitch = function (event) return event.msg:byte(2) end,
-    velocity = function (event) return event.msg:byte(3) end,
-    type = function (event) return event.msg:byte(1) >> 4 end,
-    articulation = function (event) return event.msg:byte(1) >> 4 end
+    selected = function (event) return event.flags & 1 == 1 end, -- 选中
+    pitch = function (event)  -- 音高
+      if event.isArticulation then  -- 音符事件的通道和音高需要特殊处理
+        local _, pitch = event.msg:match("NOTE (%d+) (%d+) ")
+        return tonumber(pitch)
+      end
+      return event.msg:byte(2) 
+    end,
+    velocity = function (event) return event.msg:byte(3) end, -- 力度
+    channel = function (event)  -- 通道
+      if event.isArticulation then  -- 音符事件的通道和音高需要特殊处理
+        local channel, _ = event.msg:match("NOTE (%d+) (%d+) ")
+        return tonumber(channel)
+      end
+      return event.msg:byte(1)&0x0F 
+    end,
+    type = function (event) return event.msg:byte(1) >> 4 end, -- 类型
+    articulation = function (event) return event.msg:byte(1) >> 4 end,
+    isArticulation = function (event) if event.type == EVENT_ARTICULATION and event.msg:find("articulation") then return true end return false end,
+    isCCBZ = function (event) if event.type == EVENT_ARTICULATION and event.msg:find("CCBZ") then return true end return false end,
   }
   local setters = {
     pitch = function (event, value)
+      if event.isArticulation then  -- 音符事件的通道和音高需要特殊处理
+        event.msg = event.msg:gsub("NOTE (%d+) (%d+) ", function (channel, pitch)
+          return string.format("NOTE %s %d ", channel, value)
+        end)
+        return
+      end
       event.msg = string.pack("BBB", event.msg:byte(1), value or event.msg:byte(2), event.msg:byte(3))
+    end,
+    channel = function (event, value)
+      if event.isArticulation then  -- 音符事件的通道和音高需要特殊处理
+        event.msg = event.msg:gsub("NOTE (%d+) (%d+) ", function (channel, pitch)
+          return string.format("NOTE %d %s ", value, pitch)
+        end)
+        return
+      end
+      event.msg = string.pack("BBB", ((event.msg:byte(1)&0xF0) | (value&0x0F)) , event.msg:byte(2), event.msg:byte(3))
     end,
     velocity = function (event, value)
       event.msg = string.pack("BBB", event.msg:byte(1), event.msg:byte(2), value or event.msg:byte(3))
@@ -150,114 +174,143 @@ function getAllEvents(take, onEach)
   return events
 end
 
-local function setAllEvents(take, events)
-  local lastPos = 0
-  for _, event in pairs(events) do -- 把事件的位置转换成偏移量
-    event.offset = event.pos - lastPos
-    lastPos = event.pos
+function getAllTakes()
+  tTake = {}
+  if reaper.MIDIEditor_EnumTakes then
+    local editor = reaper.MIDIEditor_GetActive()
+    for i = 0, math.huge do
+      take = reaper.MIDIEditor_EnumTakes(editor, i, false)
+      if take and reaper.ValidatePtr2(0, take, "MediaItem_Take*") then 
+        tTake[take] = true
+        tTake[take] = {item = reaper.GetMediaItemTake_Item(take), editor = editor}
+      else
+        break
+      end
+    end
+  else
+    for i = 0, reaper.CountMediaItems(0)-1 do
+      local item = reaper.GetMediaItem(0, i)
+      local take = reaper.GetActiveTake(item)
+      if reaper.ValidatePtr2(0, take, "MediaItem_Take*") and reaper.TakeIsMIDI(take) and reaper.MIDI_EnumSelNotes(take, -1) == 0 then -- Get potential takes that contain notes. NB == 0 
+        tTake[take] = true
+      end
+    end
+  
+    for take in next, tTake do
+      if reaper.MIDI_EnumSelNotes(take, -1) ~= -1 then tTake[take] = nil end -- Remove takes that were not affected by deselection
+    end
   end
-
-  local tab = {}
-  for _, event in pairs(events) do
-    table.insert(tab, string.pack("i4Bs4", event.offset, event.flags, event.msg))
-  end
-  reaper.MIDI_SetAllEvts(take, table.concat(tab)) -- 将编辑好的MIDI上传到take
+  if not next(tTake) then return end
+  return tTake
 end
 
-function setItemExtent(take)
-  local range = {head = math.huge, tail = -math.huge}
-  local events = getAllEvents(take, function(event)
-    if event.selected then
-      local pos = event.pos
-      range.head = math.min(range.head, pos)
-      range.tail = math.max(range.tail, pos)
+for take, _ in pairs(getAllTakes()) do
+  local sourceLengthTicks = reaper.BR_GetMidiSourceLenPPQ(take)
+  local _, originEvent = reaper.MIDI_GetAllEvts(take)
+  local skipCheck = true -- 是否跳过检查
+
+  -- 打印所有参数
+  if false then
+    local events = getAllEvents(take, function (event)
+      print(
+        "type:" .. event.type 
+        .. " flags:" .. event.flags 
+        .. " offset:" .. event.offset 
+        .. " isArticulation:" .. tostring(event.isArticulation)
+        .. " isCCBZ:" .. tostring(event.isCCBZ)
+        -- .. " msg:" .. table.concat(table.pack(string.byte(event.msg, 1, #event.msg)), " ")
+        .. " msg:" .. event.msg
+        
+      )
+    end)
+  end
+
+  reaper.PreventUIRefresh(1)
+  reaper.Undo_BeginBlock()
+
+  local moveDistance = 10 -- 移动距离
+  local diff = 120
+
+  -- 符号事件记录表
+  local articulationMap = {}
+
+  -- 贝塞尔曲线事件记录表
+  local ccbzMap = {}
+
+  -- 在某个音高上的音符事件，用于判断移动过程中的阻挡音符
+  local pitchNoteEvents = {}
+  
+  -- 其他被选中事件
+  local otherSelectedEvents = {}
+
+  -- 遇到阻挡音符或超出0边界
+  local overflow = true
+
+  -- 收集事件
+  local events = getAllEvents(take, function (event)
+    if event.isArticulation then
+      articulationMap[event.pos] = articulationMap[event.pos] or {}
+      articulationMap[event.pos][event.pitch] = event
+    elseif event.isCCBZ then
+      ccbzMap[event.pos] = event
+    end
+
+    if event.type == EVENT_NOTE_START or event.type == EVENT_NOTE_END then
+      pitchNoteEvents[event.pitch] = pitchNoteEvents[event.pitch] or {}
+      table.insert(pitchNoteEvents[event.pitch], event) 
+    elseif event.selected then
+      table.insert(otherSelectedEvents, event) 
     end
   end)
-  local range_qn = {
-    head = reaper.MIDI_GetProjQNFromPPQPos(take, range.head),
-    tail = reaper.MIDI_GetProjQNFromPPQPos(take, range.tail)
-  }
-  range_qn.len = range_qn.tail - range_qn.head
 
-  local item = reaper.GetMediaItemTake_Item(take)
-  local item_len = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
-  local item_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION") -- + reaper.GetMediaItemInfo_Value(item, "D_SNAPOFFSET")
-  local qn_item_pos = reaper.TimeMap2_timeToQN(0, item_pos)
-  local qn_item_len = reaper.TimeMap2_timeToQN(0, item_len)
-  local qn_item_end = qn_item_pos + qn_item_len
-  local event_start_pos_qn = reaper.MIDI_GetProjQNFromPPQPos(take, 0)
-  
-  local result_tail_qn = range_qn.head + (range_qn.tail - range_qn.head)
-  if result_tail_qn > qn_item_len + qn_item_pos then
-    reaper.MIDI_SetItemExtents(item, qn_item_pos, result_tail_qn)
-  end
-end
+  -- 移动音符事件
+  for pitch, pitchNoteEvent in pairs(pitchNoteEvents) do
+    for i=1, #pitchNoteEvent do
+      if not pitchNoteEvent[i].selected then goto continue end
+      local prePos = reaper.MIDI_GetPPQPosFromProjQN(take, 0)
+      if i > 1 then prePos = pitchNoteEvent[i-1].pos end
 
-reaper.Undo_BeginBlock()
-for take, _ in pairs(getAllTakes()) do
-  -- local sourceLengthTicks = reaper.BR_GetMidiSourceLenPPQ(take)
-  local lastPos = 0
-  local events = {}
-  
-  local _, MIDI = reaper.MIDI_GetAllEvts(take, "")
-  local selectedStartEvents = {}
-  local pitchLastStart = {}
-  
-  local stringPos = 1
-  while stringPos < MIDI:len() do
-    local offset, flags, msg
-    offset, flags, msg, stringPos = string.unpack("i4Bs4", MIDI, stringPos)
-  
-    local selected = flags&1 == 1
-    local pitch = msg:byte(2)
-    local status = msg:byte(1)>>4
-    local event = {
-      ["pos"] = lastPos + offset,
-      ["flags"] = flags,
-      ["msg"] = msg,
-      ["selected"] = selected,
-      ["pitch"] = pitch,
-      ["status"] = status,
-    }
-    table.insert(events, event)
+      local originPos = pitchNoteEvent[i].pos
+      local originPitch = pitchNoteEvent[i].pitch
 
-    if event.selected then
-      if selectedStartEvents[event.pos] == nil then selectedStartEvents[event.pos] = {} end
-      table.insert(selectedStartEvents[event.pos], event)
-    end
+      -- 移动音符
+      if pitchNoteEvent[i].type == EVENT_NOTE_END then
+        if originPos - moveDistance < prePos + moveDistance then overflow = true end
+        pitchNoteEvent[i].pos = math.max(prePos + moveDistance + (diff - moveDistance), originPos - moveDistance)
+      else
+        if originPos - moveDistance < prePos then overflow = true end
+        pitchNoteEvent[i].pos = math.max(prePos, originPos - moveDistance)
+      end
 
-    lastPos = lastPos + offset
-  end
-  
-  for _, es in pairs(selectedStartEvents) do
-    for i=1, #es do
-      es[i].pos = es[i].pos + ticks
+      -- 移动符号
+      if pitchNoteEvent[i].type == EVENT_NOTE_START and articulationMap[originPos] and articulationMap[originPos][originPitch] then
+        articulationMap[originPos][originPitch].pos = pitchNoteEvent[i].pos
+      end
+
+      ::continue::
     end
   end
-  
---   local last = events[#events]
---   table.remove(events, #events)
---   table.sort(events,function(a,b) -- 事件重新排序
---     -- if a.status == 11 then return false end
---     if a.pos == b.pos then
---       if a.status == b.status then
---         return a.pitch < b.pitch
---       end
---       return a.status < b.status
---     end
---     return a.pos < b.pos
---   end)
---   table.insert(events, last)
-  
-  setAllEvents(take, events)
-  setItemExtent(take)
+
+  -- 移动其他事件
+  for _, event in pairs(otherSelectedEvents) do
+    event.pos = math.max(0, event.pos - moveDistance)
+    if event.type == EVENT_CC and ccbzMap[event.pos] then
+      ccbzMap[event.pos].pos = event.pos
+    end
+  end
+
+  -- 提交事件
+  if overflow then
+    setAllEvents(take, events)
+  end
   reaper.MIDI_Sort(take)
-  
---   if not (sourceLengthTicks == reaper.BR_GetMidiSourceLenPPQ(take)) then
---     reaper.MIDI_SetAllEvts(take, MIDI)
---     reaper.ShowMessageBox("腳本造成事件位置位移，原始MIDI數據已恢復", "錯誤", 0)
---   end
+
+  if not skipCheck and not (sourceLengthTicks == reaper.BR_GetMidiSourceLenPPQ(take)) then
+    reaper.MIDI_SetAllEvts(take, originEvent)
+    reaper.ShowMessageBox("腳本造成事件位置位移，原始MIDI數據已恢復", "錯誤", 0)
+  end
 end
 
 reaper.Undo_EndBlock("Move Events Left 10 Ticks (Fast)", -1)
+reaper.PreventUIRefresh(-1)
 reaper.UpdateArrange()
---
+++ MIDI Editor/zaibuyidao_Move Notes -01 Semitone (Fast).lua
@@ -0,0 +1,271 @@
+-- @description Move Notes -01 Semitone (Fast)
+-- @version 1.0
+-- @author zaibuyidao
+-- @changelog Initial release
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about
+--   # Move Notes -01 Semitone (Fast)
+
+EVENT_NOTE_START = 9 -- Note-on 音符开
+EVENT_NOTE_END = 8 -- Note-off 音符关
+EVENT_AFTER_TOUCH = 10 -- After Touch 触后
+EVENT_CC = 11 -- Continuous Controller CC控制器
+EVENT_PROGRAM = 12 -- Program 程序选择
+EVENT_CHPRESS = 13 -- Channel Pressure 通道压力
+EVENT_PITCH = 14 -- Pitch Wheel 弯音事件
+EVENT_ARTICULATION = 15 -- Articulation 符号
+
+--EVENT_BANKPROG = 11 and (msg:byte(2) == 0 or msg:byte(2) == 32) -- 音色库事件
+--EVENT_TEXT = msg:byte(1) == 0xFF and not (msg:byte(2) == 0x0F) -- 文本事件，排除 notation text 事件
+--EVENT_SYSEX = 0xF and not (msg:byte(1) == 0xFF) -- 系统码事件
+
+function print(...)
+  local params = {...}
+  for i = 1, #params do
+    if i ~= 1 then reaper.ShowConsoleMsg(" ") end
+    reaper.ShowConsoleMsg(tostring(params[i]))
+  end
+  reaper.ShowConsoleMsg("\n")
+end
+
+function table.print(t)
+  local print_r_cache = {}
+  local function sub_print_r(t, indent)
+    if (print_r_cache[tostring(t)]) then
+      print(indent .. "*" .. tostring(t))
+    else
+      print_r_cache[tostring(t)] = true
+      if (type(t) == "table") then
+        for pos, val in pairs(t) do
+          if (type(val) == "table") then
+            print(indent .. "[" .. tostring(pos) .. "] => " .. tostring(t) .. " {")
+            sub_print_r(val, indent .. string.rep(" ", string.len(tostring(pos)) + 8))
+            print(indent .. string.rep(" ", string.len(tostring(pos)) + 6) .. "}")
+          elseif (type(val) == "string") then
+            print(indent .. "[" .. tostring(pos) .. '] => "' .. val .. '"')
+          else
+            print(indent .. "[" .. tostring(pos) .. "] => " .. tostring(val))
+          end
+        end
+      else
+        print(indent .. tostring(t))
+      end
+    end
+  end
+  if (type(t) == "table") then
+    print(tostring(t) .. " {")
+    sub_print_r(t, "  ")
+    print("}")
+  else
+    sub_print_r(t, "  ")
+  end
+end
+
+function setAllEvents(take, events)
+  local headPos = math.huge -- 开头位置，用于扩展边界
+  local tailPos = 0 -- 末尾位置，用于扩展边界
+  local lastPos = 0 -- 上一次遍历到的位置
+  
+  -- local minPos = math.huge
+  -- for _, event in pairs(events) do minPos = math.min(minPos, event.pos) end
+  -- local lastPos = math.min(minPos, 0) -- 上一次遍历到的位置
+
+  for _, event in pairs(events) do
+    headPos = math.min(headPos, event.pos)
+    tailPos = math.max(tailPos, event.pos)
+    event.offset = event.pos - lastPos
+    lastPos = event.pos
+  end
+  
+  -- item边界扩展处理
+  events[#events].pos = math.max(events[#events].pos, tailPos)
+  local item = reaper.GetMediaItemTake_Item(take)
+  reaper.MIDI_SetItemExtents(
+    item, 
+    reaper.MIDI_GetProjQNFromPPQPos(take, math.min(0, headPos)), -- reaper.TimeMap2_timeToQN(0, reaper.GetMediaItemInfo_Value(item, "D_POSITION"))
+    reaper.MIDI_GetProjQNFromPPQPos(take, events[#events].pos)
+  )
+
+  local tab = {}
+  for _, event in pairs(events) do
+    table.insert(tab, string.pack("i4Bs4", event.offset, event.flags, event.msg))
+  end
+  reaper.MIDI_SetAllEvts(take, table.concat(tab))
+end
+
+function getAllEvents(take, onEach)
+  local getters = {
+    selected = function (event) return event.flags & 1 == 1 end, -- 选中
+    pitch = function (event)  -- 音高
+      if event.isArticulation then  -- 音符事件的通道和音高需要特殊处理
+        local _, pitch = event.msg:match("NOTE (%d+) (%d+) ")
+        return tonumber(pitch)
+      end
+      return event.msg:byte(2) 
+    end,
+    velocity = function (event) return event.msg:byte(3) end, -- 力度
+    channel = function (event)  -- 通道
+      if event.isArticulation then  -- 音符事件的通道和音高需要特殊处理
+        local channel, _ = event.msg:match("NOTE (%d+) (%d+) ")
+        return tonumber(channel)
+      end
+      return event.msg:byte(1)&0x0F 
+    end,
+    type = function (event) return event.msg:byte(1) >> 4 end, -- 类型
+    articulation = function (event) return event.msg:byte(1) >> 4 end,
+    isArticulation = function (event) if event.type == EVENT_ARTICULATION and event.msg:find("articulation") then return true end return false end,
+    isCCBZ = function (event) if event.type == EVENT_ARTICULATION and event.msg:find("CCBZ") then return true end return false end,
+  }
+  local setters = {
+    pitch = function (event, value)
+      if event.isArticulation then  -- 音符事件的通道和音高需要特殊处理
+        event.msg = event.msg:gsub("NOTE (%d+) (%d+) ", function (channel, pitch)
+          return string.format("NOTE %s %d ", channel, value)
+        end)
+        return
+      end
+      event.msg = string.pack("BBB", event.msg:byte(1), value or event.msg:byte(2), event.msg:byte(3))
+    end,
+    channel = function (event, value)
+      if event.isArticulation then  -- 音符事件的通道和音高需要特殊处理
+        event.msg = event.msg:gsub("NOTE (%d+) (%d+) ", function (channel, pitch)
+          return string.format("NOTE %d %s ", value, pitch)
+        end)
+        return
+      end
+      event.msg = string.pack("BBB", ((event.msg:byte(1)&0xF0) | (value&0x0F)) , event.msg:byte(2), event.msg:byte(3))
+    end,
+    velocity = function (event, value)
+      event.msg = string.pack("BBB", event.msg:byte(1), event.msg:byte(2), value or event.msg:byte(3))
+    end,
+    selected = function (event, value)
+      if value then
+        event.flags = event.flags | 1
+      else
+        event.flags = event.flags & 0xFFFFFFFE
+      end
+    end
+  }
+  local eventMetaTable = {
+    __index = function (event, key) return getters[key](event) end,
+    __newindex = function (event, key, value) return setters[key](event, value) end
+  }
+
+  local events = {}
+  local _, MIDIstring = reaper.MIDI_GetAllEvts(take, "")
+  local stringPos = 1
+  local lastPos = 0
+  while stringPos <= MIDIstring:len() do
+    local offset, flags, msg
+    offset, flags, msg, stringPos = string.unpack("i4Bs4", MIDIstring, stringPos)
+    local event = setmetatable({
+      offset = offset,
+      pos = lastPos + offset,
+      flags = flags,
+      msg = msg
+    }, eventMetaTable)
+    table.insert(events, event)
+    onEach(event)
+    lastPos = lastPos + offset
+  end
+  return events
+end
+
+function getAllTakes()
+  tTake = {}
+  if reaper.MIDIEditor_EnumTakes then
+    local editor = reaper.MIDIEditor_GetActive()
+    for i = 0, math.huge do
+      take = reaper.MIDIEditor_EnumTakes(editor, i, false)
+      if take and reaper.ValidatePtr2(0, take, "MediaItem_Take*") then 
+        tTake[take] = true
+        tTake[take] = {item = reaper.GetMediaItemTake_Item(take), editor = editor}
+      else
+        break
+      end
+    end
+  else
+    for i = 0, reaper.CountMediaItems(0)-1 do
+      local item = reaper.GetMediaItem(0, i)
+      local take = reaper.GetActiveTake(item)
+      if reaper.ValidatePtr2(0, take, "MediaItem_Take*") and reaper.TakeIsMIDI(take) and reaper.MIDI_EnumSelNotes(take, -1) == 0 then -- Get potential takes that contain notes. NB == 0 
+        tTake[take] = true
+      end
+    end
+  
+    for take in next, tTake do
+      if reaper.MIDI_EnumSelNotes(take, -1) ~= -1 then tTake[take] = nil end -- Remove takes that were not affected by deselection
+    end
+  end
+  if not next(tTake) then return end
+  return tTake
+end
+
+for take, _ in pairs(getAllTakes()) do
+  local sourceLengthTicks = reaper.BR_GetMidiSourceLenPPQ(take)
+  local _, originEvent = reaper.MIDI_GetAllEvts(take)
+  local skipCheck = true -- 是否跳过检查
+
+  -- 打印所有参数
+  if false then
+    local events = getAllEvents(take, function (event)
+      print(
+        "type:" .. event.type 
+        .. " flags:" .. event.flags 
+        .. " offset:" .. event.offset 
+        .. " isArticulation:" .. tostring(event.isArticulation)
+        .. " isCCBZ:" .. tostring(event.isCCBZ)
+        -- .. " msg:" .. table.concat(table.pack(string.byte(event.msg, 1, #event.msg)), " ")
+        .. " msg:" .. event.msg
+        
+      )
+    end)
+  end
+
+  reaper.PreventUIRefresh(1)
+  reaper.Undo_BeginBlock()
+
+  local semitone = 1
+
+  -- 符号事件记录表
+  local articulationMap = {}
+
+  local noteEvents = {}
+
+  local events = getAllEvents(take, function (event)
+    if event.isArticulation then
+      articulationMap[event.pos] = articulationMap[event.pos] or {}
+      articulationMap[event.pos][event.pitch] = event
+    end
+
+    if (event.type == EVENT_NOTE_START or event.type == EVENT_NOTE_END) and event.selected then
+      table.insert(noteEvents, event)
+    end
+  end)
+
+  for _, event in pairs(noteEvents) do
+    local originPos = event.pos
+    local originPitch = event.pitch
+    -- 移动音符
+    event.pitch = math.max(event.pitch - semitone, 0)
+    -- 移动符号
+    if event.type == EVENT_NOTE_START and articulationMap[originPos] and articulationMap[originPos][originPitch] then
+      articulationMap[originPos][originPitch].pitch = event.pitch
+    end
+  end
+
+  setAllEvents(take, events)
+  reaper.MIDI_Sort(take)
+ 
+  if not skipCheck and not (sourceLengthTicks == reaper.BR_GetMidiSourceLenPPQ(take)) then
+    reaper.MIDI_SetAllEvts(take, originEvent)
+    reaper.ShowMessageBox("腳本造成事件位置位移，原始MIDI數據已恢復", "錯誤", 0)
+  end
+end
+
+reaper.Undo_EndBlock("Move Notes -01 Semitone (Fast)", -1)
+reaper.PreventUIRefresh(-1)
+reaper.UpdateArrange()
