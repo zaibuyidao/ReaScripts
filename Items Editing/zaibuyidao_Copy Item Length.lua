bbd1779cf860cdc48a0b171144634f95cef81b3a
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2022-11-15 23:45:44 +0800

    Update

--- Items Editing/zaibuyidao_Copy Item Length.lua
+++ Items Editing/zaibuyidao_Copy Item Length.lua
@@ -1,31 +1,210 @@
---[[
- * ReaScript Name: Copy Item Length
- * Version: 1.0
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository: GitHub > zaibuyidao > ReaScripts
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * REAPER: 6.0
- * Donation: http://www.paypal.me/zaibuyidao
---]]
+-- @description Copy Item Length
+-- @version 1.0.1
+-- @author zaibuyidao
+-- @changelog Fix data saving errors
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires SWS Extensions
 
---[[
- * Changelog:
- * v1.0 (2021-6-5)
-  + Initial release
---]]
-
-function Msg(param)
-  reaper.ShowConsoleMsg(tostring(param) .. "\n")
+function print(...)
+  local params = {...}
+  for i = 1, #params do
+    if i ~= 1 then reaper.ShowConsoleMsg(" ") end
+    reaper.ShowConsoleMsg(tostring(params[i]))
+  end
+  reaper.ShowConsoleMsg("\n")
 end
 
+function table.print(t)
+  local print_r_cache = {}
+  local function sub_print_r(t, indent)
+    if (print_r_cache[tostring(t)]) then
+      print(indent .. "*" .. tostring(t))
+    else
+      print_r_cache[tostring(t)] = true
+      if (type(t) == "table") then
+        for pos, val in pairs(t) do
+          if (type(val) == "table") then
+            print(indent .. "[" .. tostring(pos) .. "] => " .. tostring(t) .. " {")
+            sub_print_r(val, indent .. string.rep(" ", string.len(tostring(pos)) + 8))
+            print(indent .. string.rep(" ", string.len(tostring(pos)) + 6) .. "}")
+          elseif (type(val) == "string") then
+            print(indent .. "[" .. tostring(pos) .. '] => "' .. val .. '"')
+          else
+            print(indent .. "[" .. tostring(pos) .. "] => " .. tostring(val))
+          end
+        end
+      else
+        print(indent .. tostring(t))
+      end
+    end
+  end
+  if (type(t) == "table") then
+    print(tostring(t) .. " {")
+    sub_print_r(t, "  ")
+    print("}")
+  else
+    sub_print_r(t, "  ")
+  end
+end
+
+function table.serialize(obj)
+  local lua = ""
+  local t = type(obj)
+  if t == "number" then
+    lua = lua .. obj
+  elseif t == "boolean" then
+    lua = lua .. tostring(obj)
+  elseif t == "string" then
+    lua = lua .. string.format("%q", obj)
+  elseif t == "table" then
+    lua = lua .. "{\n"
+  for k, v in pairs(obj) do
+    lua = lua .. "[" .. table.serialize(k) .. "]=" .. table.serialize(v) .. ",\n"
+  end
+  local metatable = getmetatable(obj)
+  if metatable ~= nil and type(metatable.__index) == "table" then
+    for k, v in pairs(metatable.__index) do
+      lua = lua .. "[" .. table.serialize(k) .. "]=" .. table.serialize(v) .. ",\n"
+    end
+  end
+  lua = lua .. "}"
+  elseif t == "nil" then
+    return nil
+  else
+    error("can not serialize a " .. t .. " type.")
+  end
+  return lua
+end
+
+function table.unserialize(lua)
+  local t = type(lua)
+  if t == "nil" or lua == "" then
+    return nil
+  elseif t == "number" or t == "string" or t == "boolean" then
+    lua = tostring(lua)
+  else
+    error("can not unserialize a " .. t .. " type.")
+  end
+  lua = "return " .. lua
+  local func = load(lua)
+  if func == nil then return nil end
+  return func()
+end
+
+function to_string_ex(value)
+  if type(value)=='table' then
+    return table_to_str(value)
+  elseif type(value)=='string' then
+    return value
+  else
+    return tostring(value)
+  end
+end
+
+function table_to_str(t)
+  if t == nil then return "" end
+  local retstr= ""
+
+  local i = 1
+  for key,value in pairs(t) do
+    local signal = "" .. ','
+    if i == 1 then
+      signal = ""
+    end
+
+    if key == i then
+      retstr = retstr .. signal .. to_string_ex(value)
+    else
+      if type(key) == 'number' or type(key) == 'string' then
+        retstr = retstr .. signal .. to_string_ex(value)
+      else
+        if type(key) == 'userdata' then
+            retstr = retstr .. signal .. "*s" .. table_to_str(getmetatable(key)) .. "*e" .. "=" .. to_string_ex(value)
+        else
+            retstr = retstr .. signal .. key .. "=" .. to_string_ex(value)
+        end
+      end
+    end
+    i = i + 1
+  end
+
+  retstr = retstr .. ""
+  return retstr
+end
+
+function string.split(input, delimiter)
+  input = tostring(input)
+  delimiter = tostring(delimiter)
+  if (delimiter == "") then return false end
+  local pos, arr = 0, {}
+  for st, sp in function() return string.find(input, delimiter, pos, true) end do
+      table.insert(arr, string.sub(input, pos, st - 1))
+      pos = sp + 1
+  end
+  table.insert(arr, string.sub(input, pos))
+  return arr
+end
+
+function saveData(key1, key2, data, boolean)
+  reaper.SetExtState(key1, key2, data, boolean)
+end
+
+function getSavedData(key1, key2)
+  return table.unserialize(reaper.GetExtState(key1, key2))
+end
+
+function saveDataList(key1, key2, data, boolean)
+  reaper.SetExtState(key1, key2, table_to_str(data), boolean)
+end
+
+function getSavedDataList(key1, key2)
+  local check_state = reaper.GetExtState(key1, key2)
+  if check_state == nil or check_state == "" then
+    return nil
+  end
+  return string.split(reaper.GetExtState(key1, key2), ",")
+end
+
+function getMutiInput(title,num,lables,defaults)
+  title = title or "Title"
+  lables = lables or "Lable:"
+  local uok, uinput = reaper.GetUserInputs(title, num, lables, defaults)
+  if uok then return string.split(uinput,",") end
+end
+
+reaper.PreventUIRefresh(1)
 reaper.Undo_BeginBlock()
-local len_t = {}
-count_sel_items = reaper.CountSelectedMediaItems(0)
-if count_sel_items > 1 then return end
-for i = 0, count_sel_items - 1 do
+
+local count_sel_items = reaper.CountSelectedMediaItems(0)
+if count_sel_items <= 0 then return end
+local track_items = {}
+local item_lens = {}
+
+for i = 0, count_sel_items - 1  do
   local item = reaper.GetSelectedMediaItem(0, i)
-  item_len = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+  local track = reaper.GetMediaItem_Track(item)
+  local track_num = reaper.GetMediaTrackInfo_Value(track, "IP_TRACKNUMBER")
+  if i == 0 then tn = track_num end
+  if track_num == tn then
+    if not track_items[track] then track_items[track] = {} end
+    table.insert(track_items[track], item)
+  end
 end
-reaper.SetExtState("CopyItemLength", "Length", item_len, false)
+
+for _, items in pairs(track_items) do
+  for i, item in ipairs(items) do
+    take = reaper.GetActiveTake(item)
+    item_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+    item_len = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+    item_lens[#item_lens + 1] = item_len
+  end
+end
+
+saveDataList("Copy Item Length", "Parameters", item_lens, false)
+
 reaper.Undo_EndBlock("Copy Item Length", -1)
+reaper.PreventUIRefresh(-1)
+reaper.UpdateArrange()
