8641bd665a96a6d2216535af0918d75b8cdaf0a4
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2023-03-07 10:18:06 +0800

    Update

--- Various/UCS Tag Search Rename Bundle.lua
+++ Various/UCS Tag Search Rename Bundle.lua
@@ -1,6 +1,6 @@
 -- @description UCS Tag Search Rename Bundle
 -- @author zaibuyidao
--- @version 1.3.7
+-- @version 1.3.8
 -- @changelog Update: UCS v8.2
 -- @links
 --   https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
