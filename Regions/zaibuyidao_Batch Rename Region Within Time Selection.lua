23306bd286051ffa6c69d6b0e742aebf85991240
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2023-03-14 13:53:15 +0800

    Update zaibuyidao_Batch Rename Region Within Time Selection.lua

--- Regions/zaibuyidao_Batch Rename Region Within Time Selection.lua
+++ Regions/zaibuyidao_Batch Rename Region Within Time Selection.lua
@@ -1,22 +1,88 @@
---[[
- * ReaScript Name: Batch Rename Region Within Time Selection
- * Version: 1.1.5
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * Donation: http://www.paypal.me/zaibuyidao
---]]
-
---[[
- * Changelog:
- * v1.0 (2021-7-18)
-  + Initial release
---]]
+-- @description Batch Rename Region Within Time Selection
+-- @version 1.1.6
+-- @author zaibuyidao
+-- @changelog Initial release
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
 local bias = 0.002 -- 补偿偏差值
 
-function Msg(param) 
-  reaper.ShowConsoleMsg(tostring(param) .. "\n") 
+function print(...)
+  local args = {...}
+  local str = ""
+  for i = 1, #args do
+    str = str .. tostring(args[i]) .. "\t"
+  end
+  reaper.ShowConsoleMsg(str .. "\n")
+end
+
+if not reaper.SNM_GetIntConfigVar then
+  local retval = reaper.ShowMessageBox("This script requires the SWS Extension.\n該脚本需要 SWS 擴展。\n\nDo you want to download it now? \n你想現在就下載它嗎？", "Warning 警告", 1)
+  if retval == 1 then
+    if not OS then local OS = reaper.GetOS() end
+    if OS=="OSX32" or OS=="OSX64" then
+      os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+    else
+      os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+    end
+  end
+  return
+end
+
+if not reaper.APIExists("JS_Localize") then
+  reaper.MB("Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\n請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n\nThen restart REAPER and run the script again, thank you!\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n", "You must install JS_ReaScriptAPI 你必須安裝JS_ReaScriptAPI", 0)
+  local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+  if ok then
+    reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+  else
+    reaper.MB(err, "錯誤", 0)
+  end
+  return reaper.defer(function() end)
+end
+
+function getSystemLanguage()
+  local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+  local os = reaper.GetOS()
+  local lang
+
+  if os == "Win32" or os == "Win64" then -- Windows
+    if locale == 936 then -- Simplified Chinese
+      lang = "简体中文"
+    elseif locale == 950 then -- Traditional Chinese
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "OSX32" or os == "OSX64" then -- macOS
+    local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+    if lang == "zh-CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh-TW" then -- 繁体中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "Linux" then -- Linux
+    local handle = io.popen("echo $LANG")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+    if lang == "zh_CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh_TW" then -- 繁體中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  end
+
+  return lang
 end
 
 function chsize(char)
@@ -162,18 +228,34 @@ end
 reaper.PreventUIRefresh(1)
 reaper.Undo_BeginBlock()
 
-local show_msg = reaper.GetExtState("BatchRenameRegionWithinTimeSelection", "ShowMsg")
+local language = getSystemLanguage()
+
+local show_msg = reaper.GetExtState("BATCH_RENAME_REGION_HITHIN_TIME_SELECTION", "ShowMsg")
 if (show_msg == "") then show_msg = "true" end
 
 if show_msg == "true" then
-    script_name = "在時間選擇内批量重命名區域"
-    text = "$regionname: Region name 區域名稱\nv=01: Region count 區域計數\nv=01-05 or v=05-01: Loop region count 循環區域計數\na=a: Letter count 字母計數\na=a-e or a=e-a: Loop letter count 循環字母計數\nr=10: Random string length 隨機字符串長度\n\nScript function description:\n脚本功能説明：\n\n1.Rename only\nRename 重命名\n\n2.String interception\nFrom beginning 截取開頭\nFrom end 截取結尾\n\n3.Specify position, insert or remove\nAt position 指定位置\nTo insert 插入\nRemove 移除\n\n4.Find and Replace\nFind what 查找\nReplace with 替換\n\nFind supports two pattern modifiers: * and ?\n查找支持两個模式修飾符：* 和 ?\n\n5.Loop count\nLimit or reverse cycle count. Enter 1 to enable, 0 to disable\n限制或反轉循環計數。輸入1為啓用，0為不啓用\n"
-    text = text.."\nWill this list be displayed next time?\n下次還顯示此列表嗎？"
-    local box_ok = reaper.ShowMessageBox("Wildcards 通配符:\n\n"..text, script_name, 4)
+    if language == "简体中文" then
+      script_name = "在时间选区内批量重命名区域"
+      text = "$regionname: 区域名称\nv=01: 区域计数\nv=01-05 or v=05-01: 循环区域计数\na=a: 字母计数\na=a-e or a=e-a: 循环字母计数\nr=10: 随机字符串长度\n\n脚本功能说明：\n\n1.仅重命名\n重命名\n\n2.截取字符串\n截取开头\n截取结尾\n\n3.指定位置插入或删除\n指定位置\n插入\n移除\n\n4.查找和替换\n查找\n替换\n\n查找支持两个模式修饰符：* 和 ?\n\n5.循环计数\n限制或反转循环计数。输入1为启用，0为不启用\n"
+      text = text.."\n下次还显示此页面吗？"
+      heading = "通配符 :\n\n"
+    elseif language == "繁体中文" then
+      script_name = "在時間選區内批量重命名區域"
+      text = "$regionname: 區域名稱\nv=01: 區域計數\nv=01-05 or v=05-01: 循環區域計數\na=a: 字母計數\na=a-e or a=e-a: 循環字母計數\nr=10: 隨機字符串長度\n\n脚本功能説明：\n\n1.僅重命名\n重命名\n\n2.截取字符串\n截取開頭\n截取結尾\n\n3.指定位置插入或刪除\n指定位置\n插入\n移除\n\n4.查找和替換\n查找\n替換\n\n查找支持两個模式修飾符：* 和 ?\n\n5.循環計數\n限制或反轉循環計數。輸入1為啓用，0為不啓用\n"
+      text = text.."\n下次還顯示此頁面嗎？"
+      heading = "通配符 :\n\n"
+    else
+      script_name = "Batch Rename Region Within Time Selection"
+      text = "$regionname: Region name\nv=01: Region count\nv=01-05 or v=05-01: Loop region count\na=a: Letter count\na=a-e or a=e-a: Loop letter count\nr=10: Random string length\n\nScript function description:\n\n1.Rename only\nRename\n\n2.String interception\nFrom beginning\nFrom end\n\n3.Specify position, insert or remove\nAt position\nTo insert\nRemove\n\n4.Find and Replace\nFind what\nReplace with\n\nFind supports two pattern modifiers: * and ?\n\n5.Loop count\nLimit or reverse cycle count. Enter 1 to enable, 0 to disable\n"
+      text = text.."\nWill this list be displayed next time?"
+      heading = "Wildcards :\n\n"
+    end
+  
+    local box_ok = reaper.ShowMessageBox(heading .. text, script_name, 4)
 
     if box_ok == 7 then
-        show_msg = "false"
-        reaper.SetExtState("BatchRenameRegionWithinTimeSelection", "ShowMsg", show_msg, true)
+      show_msg = "false"
+      reaper.SetExtState("BATCH_RENAME_REGION_HITHIN_TIME_SELECTION", "ShowMsg", show_msg, true)
     end
 end
 
@@ -182,10 +264,20 @@ if time_sel_start == time_sel_end then return end
 
 local pattern, begin_str, end_str, position, insert, delete, find, replace, reverse = '', '0', '0', '0', '', '0', '', '', '1'
 
-local retval, retvals_csv = reaper.GetUserInputs("Batch Rename Region Within Time Selection", 9, "Rename 重命名,From beginning 截取開頭,From end 截取結尾,At position 指定位置,To insert 插入,Remove 移除,Find what 查找,Replace with 替換,Loop count 循環計數,extrawidth=200", pattern ..','.. begin_str .. ','.. end_str ..','.. position ..','.. insert ..','.. delete ..','.. find ..','.. replace ..','.. reverse)
-if not retval then return end
+if language == "简体中文" then
+  title = "在时间选区内批量重命名区域"
+  uok, uinput = reaper.GetUserInputs(title, 9, "1.重命名,2.截取开头,   截取结尾,3.指定位置,   插入,   移除,4.查找,   替换,5.循环计数,extrawidth=200", pattern ..','.. begin_str .. ','.. end_str ..','.. position ..','.. insert ..','.. delete ..','.. find ..','.. replace ..','.. reverse)
+elseif language == "繁体中文" then
+  title = "在時間選區内批量重命名區域"
+  uok, uinput = reaper.GetUserInputs(title, 9, "1.重命名,2.截取開頭,   截取結尾,3.指定位置,   插入,   移除,4.查找,   替換,5.循環計數,extrawidth=200", pattern ..','.. begin_str .. ','.. end_str ..','.. position ..','.. insert ..','.. delete ..','.. find ..','.. replace ..','.. reverse)
+else
+  title = "Batch Rename Region Within Time Selection"
+  uok, uinput = reaper.GetUserInputs(title, 9, "1.Rename,2.From beginning,   From end,3.At position,   To insert,   Remove,4.Find what,   Replace with,5.Loop count,extrawidth=200", pattern ..','.. begin_str .. ','.. end_str ..','.. position ..','.. insert ..','.. delete ..','.. find ..','.. replace ..','.. reverse)
+end
 
-pattern, begin_str, end_str, position, insert, delete, find, replace, reverse = retvals_csv:match("(.*),(.*),(.*),(.*),(.*),(.*),(.*),(.*),(.*)")
+if not uok then return end
+
+pattern, begin_str, end_str, position, insert, delete, find, replace, reverse = uinput:match("(.*),(.*),(.*),(.*),(.*),(.*),(.*),(.*),(.*)")
 find = find:gsub('-', '%%-')
 find = find:gsub('+', '%%+')
 find = find:gsub('*', '.*')
@@ -288,6 +380,6 @@ for i, region in ipairs(sel_regions) do
   end
 end
 
-reaper.Undo_EndBlock('Batch Rename Region Within Time Selection', -1)
+reaper.Undo_EndBlock(title, -1)
 reaper.PreventUIRefresh(-1)
 reaper.UpdateArrange()
