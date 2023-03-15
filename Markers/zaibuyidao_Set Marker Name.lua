b2d57e0664a2f02caa848dacd656ebc776908be3
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2023-03-15 13:42:07 +0800

    Update

--- Markers/zaibuyidao_Set Marker Name.lua
+++ Markers/zaibuyidao_Set Marker Name.lua
@@ -1,20 +1,86 @@
---[[
- * ReaScript Name: Set Marker Name
- * Version: 1.0
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * Donation: http://www.paypal.me/zaibuyidao
---]]
+-- @description Set Marker Name
+-- @version 1.0.1
+-- @author zaibuyidao
+-- @changelog Initial release
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
---[[
- * Changelog:
- * v1.0 (2022-7-14)
-  + Initial release
---]]
+function print(...)
+  local args = {...}
+  local str = ""
+  for i = 1, #args do
+    str = str .. tostring(args[i]) .. "\t"
+  end
+  reaper.ShowConsoleMsg(str .. "\n")
+end
 
-function Msg(param) 
-  reaper.ShowConsoleMsg(tostring(param) .. "\n") 
+if not reaper.SNM_GetIntConfigVar then
+  local retval = reaper.ShowMessageBox("This script requires the SWS Extension.\n該脚本需要 SWS 擴展。\n\nDo you want to download it now? \n你想現在就下載它嗎？", "Warning 警告", 1)
+  if retval == 1 then
+    if not OS then local OS = reaper.GetOS() end
+    if OS=="OSX32" or OS=="OSX64" then
+      os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+    else
+      os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+    end
+  end
+  return
+end
+
+if not reaper.APIExists("JS_Localize") then
+  reaper.MB("Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\n請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n\nThen restart REAPER and run the script again, thank you!\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n", "You must install JS_ReaScriptAPI 你必須安裝JS_ReaScriptAPI", 0)
+  local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+  if ok then
+    reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+  else
+    reaper.MB(err, "錯誤", 0)
+  end
+  return reaper.defer(function() end)
+end
+
+function getSystemLanguage()
+  local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+  local os = reaper.GetOS()
+  local lang
+
+  if os == "Win32" or os == "Win64" then -- Windows
+    if locale == 936 then -- Simplified Chinese
+      lang = "简体中文"
+    elseif locale == 950 then -- Traditional Chinese
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "OSX32" or os == "OSX64" then -- macOS
+    local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+    if lang == "zh-CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh-TW" then -- 繁体中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "Linux" then -- Linux
+    local handle = io.popen("echo $LANG")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+    if lang == "zh_CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh_TW" then -- 繁體中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  end
+
+  return lang
 end
 
 function utf8_len(str)
@@ -127,32 +193,58 @@ end
 reaper.PreventUIRefresh(1)
 reaper.Undo_BeginBlock()
 
-local show_msg = reaper.GetExtState("SetMarkerName", "ShowMsg")
+local language = getSystemLanguage()
+
+local show_msg = reaper.GetExtState("SET_MARKER_NAME", "ShowMsg")
 if (show_msg == "") then show_msg = "true" end
 
 if show_msg == "true" then
+  if language == "简体中文" then
+    script_name = "设置标记名称"
+    text = "v=01: 标记计数\nv=01-05 or v=05-01: 循环标记计数\na=a: 字母计数\na=a-e or a=e-a: 循环字母计数\nr=10: 随机字符串长度\n\n脚本功能说明：\n\n1.仅设置名称\n标记名称\n\n2.循环计数\n限制或反转循环计数。输入1为启用，0为不启用\n"
+    text = text.."\n下次还显示此页面吗？"
+    heading = "通配符 :\n\n"
+  elseif language == "繁体中文" then
     script_name = "設置標記名稱"
-    text = "v=01: Region count 標記計數\nv=01-05 or v=05-01: Loop region count 循環標記計數\na=a: Letter count 字母計數\na=a-e or a=e-a: Loop letter count 循環字母計數\nr=10: Random string length 隨機字符串長度\n\nScript function description:\n脚本功能説明：\n\n1.Set name only\nRegion name 標記名稱\n\n2.Loop count\nLimit or reverse cycle count. Enter 1 to enable, 0 to disable\n限制或反轉循環計數。輸入1為啓用，0為不啓用\n"
-    text = text.."\nWill this list be displayed next time?\n下次還顯示此列表嗎？"
-    local box_ok = reaper.ShowMessageBox("Wildcards 通配符:\n\n"..text, script_name, 4)
+    text = "v=01: 標記計數\nv=01-05 or v=05-01: 循環標記計數\na=a: 字母計數\na=a-e or a=e-a: 循環字母計數\nr=10: 隨機字符串長度\n\n脚本功能説明：\n\n1.僅設置名稱\n標記名稱\n\n2.循環計數\n限制或反轉循環計數。輸入1為啓用，0為不啓用\n"
+    text = text.."\n下次還顯示此頁面嗎？"
+    heading = "通配符 :\n\n"
+  else
+    script_name = "Set Marker Name"
+    text = "v=01: Marker count\nv=01-05 or v=05-01: Loop marker count\na=a: Letter count\na=a-e or a=e-a: Loop letter count\nr=10: Random string length\n\nScript function description:\n\n1.Set name only\nMarker name\n\n2.Loop count\nLimit or reverse cycle count. Enter 1 to enable, 0 to disable\n"
+    text = text.."\nWill this list be displayed next time?"
+    heading = "Wildcards :\n\n"
+  end
 
-    if box_ok == 7 then
-      show_msg = "false"
-      reaper.SetExtState("SetMarkerName", "ShowMsg", show_msg, true)
-    end
+  local box_ok = reaper.ShowMessageBox(heading .. text, script_name, 4)
+
+  if box_ok == 7 then
+    show_msg = "false"
+    reaper.SetExtState("SET_MARKER_NAME", "ShowMsg", show_msg, true)
+  end
 end
 
-local pattern = reaper.GetExtState("SetMarkerName", "Name")
+local pattern = reaper.GetExtState("SET_MARKER_NAME", "Name")
 if (pattern == "") then pattern = "Marker_v=001" end
-local reverse = reaper.GetExtState("SetMarkerName", "Reverse")
+local reverse = reaper.GetExtState("SET_MARKER_NAME", "Reverse")
 if (reverse == "") then reverse = "1" end
 
-local retval, retvals_csv = reaper.GetUserInputs("Set Marker Name", 2, "Marker name 標記名稱,Loop count 循環計數,extrawidth=200", pattern ..','.. reverse)
-if not retval then return end
+if language == "简体中文" then
+  title = "设置标记名称"
+  uok, uinput = reaper.GetUserInputs(title, 2, "标记名称,循环计数,extrawidth=200", pattern ..','.. reverse)
+elseif language == "繁体中文" then
+  title = "設置標記名稱"
+  uok, uinput = reaper.GetUserInputs(title, 2, "標記名稱,循環計數,extrawidth=200", pattern ..','.. reverse)
+else
+  title = "Set Marker Name"
+  uok, uinput = reaper.GetUserInputs(title, 2, "Marker name,Loop count,extrawidth=200", pattern ..','.. reverse)
+end
 
-pattern, reverse = retvals_csv:match("(.*),(.*)")
-reaper.SetExtState("SetMarkerName", "Name", pattern, false)
-reaper.SetExtState("SetMarkerName", "Reverse", reverse, false)
+if not uok then return end
+
+pattern, reverse = uinput:match("(.*),(.*)")
+reaper.SetExtState("SET_MARKER_NAME", "Name", pattern, false)
+reaper.SetExtState("SET_MARKER_NAME", "Reverse", reverse, false)
 
 local sel_regions = get_sel_regions()
 
@@ -228,6 +320,6 @@ for i,region in ipairs(sel_regions) do
   create_region(region.left, region.right, region.name)
 end
 
-reaper.Undo_EndBlock('Set Marker Name', -1)
+reaper.Undo_EndBlock(title, -1)
 reaper.PreventUIRefresh(-1)
 reaper.UpdateArrange()
--- Regions/zaibuyidao_Set Region Name From Clipboard.lua
+++ Regions/zaibuyidao_Set Region Name From Clipboard.lua
@@ -1,22 +1,86 @@
---[[
- * ReaScript Name: Set Region Name From Clipboard
- * Version: 1.1
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository: GitHub > zaibuyidao > ReaScripts
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * REAPER: 6.0
- * Donation: http://www.paypal.me/zaibuyidao
---]]
+-- @description Set Region Name From Clipboard
+-- @version 1.1.1
+-- @author zaibuyidao
+-- @changelog Initial release
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
---[[
- * Changelog:
- * v1.0 (2021-6-14)
-  + Initial release
---]]
+function print(...)
+  local args = {...}
+  local str = ""
+  for i = 1, #args do
+    str = str .. tostring(args[i]) .. "\t"
+  end
+  reaper.ShowConsoleMsg(str .. "\n")
+end
 
-function Msg(param) 
-  reaper.ShowConsoleMsg(tostring(param) .. "\n") 
+if not reaper.SNM_GetIntConfigVar then
+  local retval = reaper.ShowMessageBox("This script requires the SWS Extension.\n該脚本需要 SWS 擴展。\n\nDo you want to download it now? \n你想現在就下載它嗎？", "Warning 警告", 1)
+  if retval == 1 then
+    if not OS then local OS = reaper.GetOS() end
+    if OS=="OSX32" or OS=="OSX64" then
+      os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+    else
+      os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+    end
+  end
+  return
+end
+
+if not reaper.APIExists("JS_Localize") then
+  reaper.MB("Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\n請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n\nThen restart REAPER and run the script again, thank you!\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n", "You must install JS_ReaScriptAPI 你必須安裝JS_ReaScriptAPI", 0)
+  local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+  if ok then
+    reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+  else
+    reaper.MB(err, "錯誤", 0)
+  end
+  return reaper.defer(function() end)
+end
+
+function getSystemLanguage()
+  local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+  local os = reaper.GetOS()
+  local lang
+
+  if os == "Win32" or os == "Win64" then -- Windows
+    if locale == 936 then -- Simplified Chinese
+      lang = "简体中文"
+    elseif locale == 950 then -- Traditional Chinese
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "OSX32" or os == "OSX64" then -- macOS
+    local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+    if lang == "zh-CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh-TW" then -- 繁体中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "Linux" then -- Linux
+    local handle = io.popen("echo $LANG")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+    if lang == "zh_CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh_TW" then -- 繁體中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  end
+
+  return lang
 end
 
 function max(a,b)
@@ -82,19 +146,32 @@ table.insert(regions, cur)
 reaper.PreventUIRefresh(1)
 reaper.Undo_BeginBlock()
 
+local language = getSystemLanguage()
+
 local clipboard = reaper.CF_GetClipboard('')
 local rgntext={}
 cnt = 0
 
-for text in clipboard:gmatch('[^\r]+') do -- 可選項 \r\n
+for text in clipboard:gmatch('[^\r\n]+') do -- 包含空行：clipboard:gmatch('[^\r]+')
   cnt = cnt + 1
   rgntext[cnt] = text
 end
 
 if #regions ~= #rgntext then
-  script_name = "從剪切板設置區域名稱"
-  text = 'Selected region 選定區域：' .. #regions .. '\nRegion name 區域名稱：' .. #rgntext
-  text = text..'\n\n仍然要繼續嗎？'
+  if language == "简体中文" then
+    script_name = "从剪切板设置区域名称"
+    text = '选定区域：' .. #regions .. '\n区域名称：' .. #rgntext
+    text = text..'\n\n仍然要继续吗？'
+  elseif language == "繁体中文" then
+    script_name = "從剪切板設置區域名稱"
+    text = '選定區域：' .. #regions .. '\n區域名稱：' .. #rgntext
+    text = text..'\n\n仍然要繼續嗎？'
+  else
+    script_name = "Set Region Name From Clipboard"
+    text = 'Selected region: ' .. #regions .. '\nRegion name: ' .. #rgntext
+    text = text..'\n\nStill want to continue?'
+  end
+
   local box_ok = reaper.ShowMessageBox(text, script_name, 4)
   
   if box_ok == 7 then
@@ -106,6 +183,6 @@ for i, region in ipairs(regions) do
   create_region(region.left, region.right, rgntext[i])
 end
 
-reaper.Undo_EndBlock("Set Region Name From Clipboard", -1)
+reaper.Undo_EndBlock(script_name, -1)
 reaper.PreventUIRefresh(-1)
 reaper.UpdateArrange()
