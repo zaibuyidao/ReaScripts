1d2eecfa6c58b1bb58be1930cb5a7afa65ccaafa
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2023-03-15 10:52:19 +0800

    Update zaibuyidao_Set Track Name.lua

--- Tracks Properties/zaibuyidao_Set Track Name.lua
+++ Tracks Properties/zaibuyidao_Set Track Name.lua
@@ -1,20 +1,86 @@
---[[
- * ReaScript Name: Set Track Name
- * Version: 1.0.3
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * Donation: http://www.paypal.me/zaibuyidao
---]]
+-- @description Set Track Name
+-- @version 1.0.4
+-- @author zaibuyidao
+-- @changelog Initial release
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
---[[
- * Changelog:
- * v1.0 (2021-7-17)
-  + Initial release
---]]
+function print(...)
+  local args = {...}
+  local str = ""
+  for i = 1, #args do
+    str = str .. tostring(args[i]) .. "\t"
+  end
+  reaper.ShowConsoleMsg(str .. "\n")
+end
 
-local function Msg(str)
-  reaper.ShowConsoleMsg(tostring(str).."\n")
+if not reaper.SNM_GetIntConfigVar then
+  local retval = reaper.ShowMessageBox("This script requires the SWS Extension.\n該脚本需要 SWS 擴展。\n\nDo you want to download it now? \n你想現在就下載它嗎？", "Warning 警告", 1)
+  if retval == 1 then
+    if not OS then local OS = reaper.GetOS() end
+    if OS=="OSX32" or OS=="OSX64" then
+      os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+    else
+      os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+    end
+  end
+  return
+end
+
+if not reaper.APIExists("JS_Localize") then
+  reaper.MB("Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\n請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n\nThen restart REAPER and run the script again, thank you!\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n", "You must install JS_ReaScriptAPI 你必須安裝JS_ReaScriptAPI", 0)
+  local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+  if ok then
+    reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+  else
+    reaper.MB(err, "錯誤", 0)
+  end
+  return reaper.defer(function() end)
+end
+
+function getSystemLanguage()
+  local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+  local os = reaper.GetOS()
+  local lang
+
+  if os == "Win32" or os == "Win64" then -- Windows
+    if locale == 936 then -- Simplified Chinese
+      lang = "简体中文"
+    elseif locale == 950 then -- Traditional Chinese
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "OSX32" or os == "OSX64" then -- macOS
+    local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+    if lang == "zh-CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh-TW" then -- 繁体中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "Linux" then -- Linux
+    local handle = io.popen("echo $LANG")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+    if lang == "zh_CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh_TW" then -- 繁體中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  end
+
+  return lang
 end
 
 function chsize(char)
@@ -88,17 +154,34 @@ function utf8_sub3(str,startChar)
   return str:sub(startIndex)
 end
 
-local show_msg = reaper.GetExtState("SetTrackName", "ShowMsg")
+local language = getSystemLanguage()
+
+local show_msg = reaper.GetExtState("SET_TRACK_NAME", "ShowMsg")
 if (show_msg == "") then show_msg = "true" end
 
 if show_msg == "true" then
-  script_name = "設置軌道名稱" text = "$foldername: 文件夾名稱\n$tracknum: 軌道編號\n$GUID: Track guid\nv=01: Track count 軌道計數\nv=01-05 or v=05-01: Loop track count 循環軌道計數\na=a: Letter count 字母順序\na=a-e or a=e-a: Loop letter count 循環字母計數\nr=10: Random string length 隨機字符串長度\n\nScript function description:\n脚本功能説明：\n\n1.Set name only\nTrack name 軌道名稱\n\n2.Loop count\nLimit or reverse cycle count. Enter 1 to enable, 0 to disable\n限制或反轉循環計數。輸入1為啓用，0為不啓用\n"
-  text = text.."\nWill this list be displayed next time?\n下次還顯示此列表嗎？"
-  local box_ok = reaper.ShowMessageBox("Wildcards 通配符 :\n\n"..text, script_name, 4)
+  if language == "简体中文" then
+    script_name = "设置轨道名称"
+    text = "$foldername: 文件夹名称\n$tracknum: 轨道编号\n$GUID: 轨道 GUID\nv=01: Track count 轨道计数\nv=01-05 or v=05-01: 循环轨道计数\na=a: 字母计数\na=a-e or a=e-a: Loop letter count 循环字母计数\nr=10: 随机字符串长度\n\n脚本功能说明：\n\n1.仅设置名称\n轨道名称\n\n2.循环计数\n限制或反转循环计数。输入1为启用，0为不启用\n"
+    text = text.."\n下次还显示此页面吗？"
+    heading = "通配符 :\n\n"
+  elseif language == "繁体中文" then
+    script_name = "設置軌道名稱"
+    text = "$foldername: 文件夾名稱\n$tracknum: 軌道編號\n$GUID: 軌道 GUID\nv=01: Track count 軌道計數\nv=01-05 or v=05-01: 循環軌道計數\na=a: 字母計數\na=a-e or a=e-a: Loop letter count 循環字母計數\nr=10: 隨機字符串長度\n\n脚本功能説明：\n\n1.僅設置名稱\n軌道名稱\n\n2.循環計數\n限制或反轉循環計數。輸入1為啓用，0為不啓用\n"
+    text = text.."\n下次還顯示此頁面嗎？"
+    heading = "通配符 :\n\n"
+  else
+    script_name = "Set Track Name"
+    text = "$foldername: Folder name\n$tracknum: Track number\n$GUID: Track guid\nv=01: Track count\nv=01-05 or v=05-01: Loop track count\na=a: Letter count\na=a-e or a=e-a: Loop letter count\nr=10: Random string length\n\nScript function description:\n\n1.Set name only\nTrack name\n\n2.Loop count\nLimit or reverse cycle count. Enter 1 to enable, 0 to disable\n"
+    text = text.."\nWill this list be displayed next time?"
+    heading = "Wildcards :\n\n"
+  end
 
+  local box_ok = reaper.ShowMessageBox(heading .. text, script_name, 4)
+  
   if box_ok == 7 then
-      show_msg = "false"
-      reaper.SetExtState("SetTrackName", "ShowMsg", show_msg, true)
+    show_msg = "false"
+    reaper.SetExtState("SET_TRACK_NAME", "ShowMsg", show_msg, true)
   end
 end
 
@@ -108,17 +191,28 @@ if count_sel_tracks == 0 then return end
 reaper.PreventUIRefresh(1)
 reaper.Undo_BeginBlock()
 
-local pattern = reaper.GetExtState("SetTrackName", "Name")
+local pattern = reaper.GetExtState("SET_TRACK_NAME", "Name")
 if (pattern == "") then pattern = "Track_v=001" end
-local reverse = reaper.GetExtState("SetTrackName", "Reverse")
+local reverse = reaper.GetExtState("SET_TRACK_NAME", "Reverse")
 if (reverse == "") then reverse = "1" end
 
-local retval, retvals_csv = reaper.GetUserInputs("Set Track Name", 2, "Track name 軌道名稱,Loop count 循環計數,extrawidth=200", pattern ..','.. reverse)
-if not retval then return end
-pattern, reverse = retvals_csv:match("(.*),(.*)")
+if language == "简体中文" then
+  title = "设置轨道名称"
+  uok, uinput = reaper.GetUserInputs(title, 2, "轨道名称,循环计数,extrawidth=200", pattern ..','.. reverse)
+elseif language == "繁体中文" then
+  title = "設置軌道名稱"
+  uok, uinput = reaper.GetUserInputs(title, 2, "軌道名稱,循環計數,extrawidth=200", pattern ..','.. reverse)
+else
+  title = "Set Track Name"
+  uok, uinput = reaper.GetUserInputs(title, 2, "Track name,Loop count,extrawidth=200", pattern ..','.. reverse)
+end
 
-reaper.SetExtState("SetTrackName", "Name", pattern, false)
-reaper.SetExtState("SetTrackName", "Reverse", reverse, false)
+if not uok then return end
+
+pattern, reverse = uinput:match("(.*),(.*)")
+
+reaper.SetExtState("SET_TRACK_NAME", "Name", pattern, false)
+reaper.SetExtState("SET_TRACK_NAME", "Reverse", reverse, false)
 
 function build_name(build_pattern, i)
   build_pattern = build_pattern:gsub('%$tracknum', track_num)
@@ -209,6 +303,6 @@ for i = 0, count_sel_tracks - 1 do
   reaper.GetSetMediaTrackInfo_String(track, 'P_NAME', track_name, true)
 end
 
-reaper.Undo_EndBlock('Set Track Name', -1)
+reaper.Undo_EndBlock(title, -1)
 reaper.PreventUIRefresh(-1)
 reaper.UpdateArrange()
