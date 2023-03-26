4eded9952594ff781fa5a73ab69351c68e098dd5
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2023-03-26 22:46:09 +0800

    Update SFX Tag Search Bundle.lua

--- Various/SFX Tag Search Bundle.lua
+++ Various/SFX Tag Search Bundle.lua
@@ -1,6 +1,6 @@
 -- @description SFX Tag Search Bundle
 -- @author zaibuyidao
--- @version 1.2.5
+-- @version 1.2.6
 -- @changelog Initial release
 -- @reference
 --   https://forum.cockos.com/showthread.php?t=229807
