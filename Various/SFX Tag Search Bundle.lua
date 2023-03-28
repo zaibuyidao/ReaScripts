92822a62c1a9cd67f96857af20e3b24190727562
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2023-03-28 23:23:09 +0800

    Update SFX Tag Search Bundle.lua

--- Various/SFX Tag Search Bundle.lua
+++ Various/SFX Tag Search Bundle.lua
@@ -1,6 +1,6 @@
 -- @description SFX Tag Search Bundle
 -- @author zaibuyidao
--- @version 1.3.8
+-- @version 1.3.9
 -- @changelog SFX tag search custom adds Chinese sorting feature
 -- @reference
 --   https://forum.cockos.com/showthread.php?t=229807
