5aeff64c9a8082cced8bea2f27608435b5cf6096
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2023-04-07 11:54:52 +0800

    Update UCS Tag Search Rename Bundle.lua

--- Various/UCS Tag Search Rename Bundle.lua
+++ Various/UCS Tag Search Rename Bundle.lua
@@ -1,7 +1,7 @@
 -- @description UCS Tag Search Rename Bundle
 -- @author zaibuyidao
--- @version 1.4.4
--- @changelog [UCS Tag Search] Added CatShort replacement feature to the search function.
+-- @version 1.4.5
+-- @changelog Added CatShort replacement feature to the search function.
 -- @links
 --   https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
 --   https://github.com/zaibuyidao/ReaScripts
