deb8379cc1c32dd33824babac9dec38601c01301
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2023-04-30 22:56:12 +0800

    Update

--- Tracks Properties/zaibuyidao_Pick Track (Dynamic Menu).lua
+++ Tracks Properties/zaibuyidao_Pick Track (Dynamic Menu).lua
@@ -1,32 +1,85 @@
---[[
- * ReaScript Name: Pick Track (Dynamic Menu)
- * Version: 1.1.2
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository: GitHub > zaibuyidao > ReaScripts
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * REAPER: 6.0
- * provides: [main=main,midi_editor,midi_eventlisteditor] .
---]]
+-- @description Pick Track (Dynamic Menu)
+-- @version 1.1.3
+-- @author zaibuyidao
+-- @changelog Add multilingual support
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @provides: [main=main,midi_editor,midi_eventlisteditor] .
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
---[[
- * Changelog:
- * v1.0 (2021-1-20)
-  + Initial release
---]]
+function print(param)
+  reaper.ShowConsoleMsg(tostring(param) .. "\n")
+end
 
-function Msg(param) reaper.ShowConsoleMsg(tostring(param) .. "\n") end
+if not reaper.SNM_GetIntConfigVar then
+  local retval = reaper.ShowMessageBox("This script requires the SWS Extension.\n該脚本需要 SWS 擴展。\n\nDo you want to download it now? \n你想現在就下載它嗎？", "Warning 警告", 1)
+  if retval == 1 then
+    if not OS then local OS = reaper.GetOS() end
+    if OS=="OSX32" or OS=="OSX64" then
+      os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+    else
+      os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+    end
+  end
+  return
+end
 
-if not reaper.APIExists("JS_Window_Find") then
-  reaper.MB("請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'. 然後重新啟動 REAPER 並再次運行腳本. 謝謝!", "你必須安裝 JS_ReaScriptAPI", 0)
+if not reaper.APIExists("JS_Localize") then
+  reaper.MB("Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\n請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n\nThen restart REAPER and run the script again, thank you!\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n", "You must install JS_ReaScriptAPI 你必須安裝JS_ReaScriptAPI", 0)
   local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
   if ok then
     reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
   else
-    reaper.MB(err, "出了些問題...", 0)
+    reaper.MB(err, "錯誤", 0)
   end
   return reaper.defer(function() end)
 end
+
+function getSystemLanguage()
+  local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+  local os = reaper.GetOS()
+  local lang
+
+  if os == "Win32" or os == "Win64" then -- Windows
+    if locale == 936 then -- Simplified Chinese
+      lang = "简体中文"
+    elseif locale == 950 then -- Traditional Chinese
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "OSX32" or os == "OSX64" then -- macOS
+    local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+    if lang == "zh-CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh-TW" then -- 繁体中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "Linux" then -- Linux
+    local handle = io.popen("echo $LANG")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+    if lang == "zh_CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh_TW" then -- 繁體中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  end
+
+  return lang
+end
+
+local language = getSystemLanguage()
 
 local count_track = reaper.CountTracks(0)
 local menu = "" -- #Pick Track|#Track List|| -- 標題
@@ -41,11 +94,22 @@ for i = 1, count_track do
   else
     flag = false
   end
+
+  if language == "简体中文" then
+    menu_track = "轨道 "
+    menu_folder = "文件夹 "
+  elseif language == "繁体中文" then
+    menu_track = "軌道 "
+    menu_folder = "文件夾 "
+  else
+    menu_track = "Track "
+    menu_folder = "Folder "
+  end
   
   if folder <= 0 then
-    menu = menu .. (flag and "!" or "") .. "Track " .. i .. ": " .. track_name .. "|"
+    menu = menu .. (flag and "!" or "") .. menu_track .. i .. ": " .. track_name .. "|"
   else
-    menu = menu .. (flag and "!" or "") .. "Folder " .. i .. ": " .. track_name .. "|"
+    menu = menu .. (flag and "!" or "") .. menu_folder .. i .. ": " .. track_name .. "|"
   end
 end
 
--- Tracks Properties/zaibuyidao_Pick Track - Only Select First Item.lua
+++ Tracks Properties/zaibuyidao_Pick Track - Only Select First Item.lua
@@ -1,24 +1,86 @@
---[[
- * ReaScript Name: Pick Track - Only Select First Item
- * Version: 1.0.1
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository: GitHub > zaibuyidao > ReaScripts
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * REAPER: 6.0
- * provides: [main=main,midi_editor,midi_eventlisteditor] .
---]]
+-- @description Pick Track - Only Select First Item
+-- @version 1.0.2
+-- @author zaibuyidao
+-- @changelog Add multilingual support
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @provides: [main=main,midi_editor,midi_eventlisteditor] .
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
---[[
- * Changelog:
- * v1.0 (2021-1-20)
-  + Initial release
---]]
+function print(param)
+  reaper.ShowConsoleMsg(tostring(param) .. "\n")
+end
 
-function Msg(param) reaper.ShowConsoleMsg(tostring(param) .. "\n") end
+if not reaper.SNM_GetIntConfigVar then
+  local retval = reaper.ShowMessageBox("This script requires the SWS Extension.\n該脚本需要 SWS 擴展。\n\nDo you want to download it now? \n你想現在就下載它嗎？", "Warning 警告", 1)
+  if retval == 1 then
+    if not OS then local OS = reaper.GetOS() end
+    if OS=="OSX32" or OS=="OSX64" then
+      os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+    else
+      os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+    end
+  end
+  return
+end
 
-reaper.Undo_BeginBlock() -- 撤銷塊開始
+if not reaper.APIExists("JS_Localize") then
+  reaper.MB("Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\n請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n\nThen restart REAPER and run the script again, thank you!\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n", "You must install JS_ReaScriptAPI 你必須安裝JS_ReaScriptAPI", 0)
+  local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+  if ok then
+    reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+  else
+    reaper.MB(err, "錯誤", 0)
+  end
+  return reaper.defer(function() end)
+end
+
+function getSystemLanguage()
+  local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+  local os = reaper.GetOS()
+  local lang
+
+  if os == "Win32" or os == "Win64" then -- Windows
+    if locale == 936 then -- Simplified Chinese
+      lang = "简体中文"
+    elseif locale == 950 then -- Traditional Chinese
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "OSX32" or os == "OSX64" then -- macOS
+    local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+    if lang == "zh-CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh-TW" then -- 繁体中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "Linux" then -- Linux
+    local handle = io.popen("echo $LANG")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+    if lang == "zh_CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh_TW" then -- 繁體中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  end
+
+  return lang
+end
+
 reaper.PreventUIRefresh(1)
+reaper.Undo_BeginBlock()
 
 local count_track = reaper.CountTracks(0)
 local count_sel_track = reaper.CountSelectedTracks(0)
@@ -29,9 +91,21 @@ end
 
 track_num = math.floor(track_num)
 
-local user_ok, user_input_CSV = reaper.GetUserInputs("Pick Track, " .. "total " .. count_track .. " tracks.", 1, "Track number", track_num)
-sel_only_num = user_input_CSV:match("(.*)")
-if not tonumber(sel_only_num) then return reaper.SN_FocusMIDIEditor() end
+local language = getSystemLanguage()
+
+if language == "简体中文" then
+  title = "选择轨道 - 只选择第一个对象"
+  uok, uinput = reaper.GetUserInputs("选择轨道, " .. "共计 " .. count_track .. " 条轨道.", 1, "轨道编号", track_num)
+elseif language == "繁体中文" then
+  title = "選擇軌道 - 只選擇第一個對象"
+  uok, uinput = reaper.GetUserInputs("選擇軌道, " .. "共計 " .. count_track .. " 條軌道.", 1, "軌道編號", track_num)
+else
+  title = "Pick Track - Only Select First Item"
+  uok, uinput = reaper.GetUserInputs("Pick Track, " .. "total " .. count_track .. " tracks.", 1, "Track number", track_num)
+end
+
+sel_only_num = uinput:match("(.*)")
+if not uok or not tonumber(sel_only_num) then return reaper.SN_FocusMIDIEditor() end
 sel_only_num = tonumber(sel_only_num)
 
 function UnselectAllTracks()
@@ -69,7 +143,7 @@ for i = 0, count_track-1 do
   reaper.Main_OnCommand(40913,0) -- Track: Vertical scroll selected tracks into view
 end
 
+reaper.Undo_EndBlock(title, -1)
 reaper.PreventUIRefresh(-1)
 reaper.UpdateArrange()
-reaper.Undo_EndBlock("Pick Track - Only Select First Item", 0) -- 撤銷塊結束
 reaper.SN_FocusMIDIEditor()
