c1d0cb75c9e3f80d10604426192660ac252b9619
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2023-05-02 02:14:47 +0800

    Update zaibuyidao_Duplicate Musical (Fast).lua

--- MIDI Editor/zaibuyidao_Duplicate Musical (Fast).lua
+++ MIDI Editor/zaibuyidao_Duplicate Musical (Fast).lua
@@ -1,7 +1,7 @@
 -- @description Duplicate Musical (Fast)
--- @version 1.0.5
+-- @version 1.0.6
 -- @author zaibuyidao
--- @changelog Optimised articulation
+-- @changelog Add support for triplet events.
 -- @links
 --   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
 --   repo https://github.com/zaibuyidao/ReaScripts
@@ -54,20 +54,92 @@ function table.print(t)
     end
 end
 
-function Open_URL(url)
-    if not OS then local OS = reaper.GetOS() end
-    if OS=="OSX32" or OS=="OSX64" then
-        os.execute("open ".. url)
-    else
-        os.execute("start ".. url)
+function getSystemLanguage()
+    local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+    local os = reaper.GetOS()
+    local lang
+
+    if os == "Win32" or os == "Win64" then -- Windows
+        if locale == 936 then -- Simplified Chinese
+            lang = "简体中文"
+        elseif locale == 950 then -- Traditional Chinese
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
+    elseif os == "OSX32" or os == "OSX64" then -- macOS
+        local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+        local result = handle:read("*a")
+        handle:close()
+        lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+        if lang == "zh-CN" then -- 简体中文
+            lang = "简体中文"
+        elseif lang == "zh-TW" then -- 繁体中文
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
+    elseif os == "Linux" then -- Linux
+        local handle = io.popen("echo $LANG")
+        local result = handle:read("*a")
+        handle:close()
+        lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+        if lang == "zh_CN" then -- 简体中文
+            lang = "简体中文"
+        elseif lang == "zh_TW" then -- 繁體中文
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
     end
+
+    return lang
+end
+
+local language = getSystemLanguage()
+
+if language == "简体中文" then
+    swsmsg = "该脚本需要 SWS 扩展，你想现在就下载它吗？"
+    swserr = "警告"
+    jsmsg = "请右键单击並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然后重新启动 REAPER 並再次运行脚本，谢谢！\n"
+    jstitle = "你必须安裝 JS_ReaScriptAPI"
+    jserr = "错误"
+elseif language == "繁体中文" then
+    swsmsg = "該脚本需要 SWS 擴展，你想現在就下載它嗎？"
+    swserr = "警告"
+    jsmsg = "請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n"
+    jstitle = "你必須安裝 JS_ReaScriptAPI"
+    jserr = "錯誤"
+else
+    swsmsg = "This script requires the SWS Extension. Do you want to download it now?"
+    swserr = "Warning"
+    jsmsg = "Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\nThen restart REAPER and run the script again, thank you!\n"
+    jstitle = "You must install JS_ReaScriptAPI"
+    jserr = "Error"
 end
 
 if not reaper.SNM_GetIntConfigVar then
-    local retval = reaper.ShowMessageBox("這個脚本需要SWS擴展，你想現在就下載它嗎？", "Warning", 1)
+    local retval = reaper.ShowMessageBox(swsmsg, swserr, 1)
     if retval == 1 then
-        Open_URL("http://www.sws-extension.org/download/pre-release/")
+        if not OS then local OS = reaper.GetOS() end
+        if OS=="OSX32" or OS=="OSX64" then
+            os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+        else
+            os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+        end
     end
+    return
+end
+
+if not reaper.APIExists("JS_Localize") then
+    reaper.MB(jsmsg, jstitle, 0)
+    local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+    if ok then
+      reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+    else
+      reaper.MB(err, jserr, 0)
+    end
+    return reaper.defer(function() end)
 end
 
 function clone(object)
@@ -373,6 +445,8 @@ elseif head_cdenom == 8 then
     end
 end
 
+local ret_grid, swing, _ = reaper.MIDI_GetGrid(take)
+
 if not copy_start_qn_from_head then
     local cur_beat = head_cml
     while true do
@@ -383,7 +457,9 @@ if not copy_start_qn_from_head then
         end
         if cur_beat > 1 then
             cur_beat = cur_beat - 1
-        else 
+        elseif ret_grid ~= 0.5 and ret_grid ~= 0.75 then
+            cur_beat = cur_beat / 3
+        else
             cur_beat = cur_beat / 2
         end
     end
@@ -407,6 +483,7 @@ for take, _ in pairs(getAllTakes()) do
     --     reaper.ShowMessageBox("腳本造成事件位置位移，原始MIDI數據已恢復", "錯誤", 0)
     -- end
 end
-reaper.Undo_EndBlock("Duplicate Musical (Fast)", -1)
+reaper.Undo_EndBlock("", -1)
 reaper.PreventUIRefresh(-1)
 reaper.UpdateArrange()
+reaper.defer(function() end)
