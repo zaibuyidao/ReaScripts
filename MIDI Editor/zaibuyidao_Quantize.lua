9d1738b39ccb52d9810692c23d2c96429198fc2b
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2023-05-01 19:43:56 +0800

    Update zaibuyidao_Quantize.lua

--- MIDI Editor/zaibuyidao_Quantize.lua
+++ MIDI Editor/zaibuyidao_Quantize.lua
@@ -1,20 +1,105 @@
---[[
- * ReaScript Name: Quantize
- * Version: 1.2.1
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository: GitHub > zaibuyidao > ReaScripts
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * REAPER: 6.0
---]]
+-- @description Quantize
+-- @version 1.2.2
+-- @author zaibuyidao
+-- @changelog Initial release
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
---[[
- * Changelog:
- * v1.0 (2020-2-1)
-  + Initial release
---]]
+function print(param)
+    reaper.ShowConsoleMsg(tostring(param) .. "\n")
+end
 
-function Msg(param) reaper.ShowConsoleMsg(tostring(param) .. "\n") end
+function getSystemLanguage()
+    local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+    local os = reaper.GetOS()
+    local lang
+
+    if os == "Win32" or os == "Win64" then -- Windows
+        if locale == 936 then -- Simplified Chinese
+            lang = "简体中文"
+        elseif locale == 950 then -- Traditional Chinese
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
+    elseif os == "OSX32" or os == "OSX64" then -- macOS
+        local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+        local result = handle:read("*a")
+        handle:close()
+        lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+        if lang == "zh-CN" then -- 简体中文
+            lang = "简体中文"
+        elseif lang == "zh-TW" then -- 繁体中文
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
+    elseif os == "Linux" then -- Linux
+        local handle = io.popen("echo $LANG")
+        local result = handle:read("*a")
+        handle:close()
+        lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+        if lang == "zh_CN" then -- 简体中文
+            lang = "简体中文"
+        elseif lang == "zh_TW" then -- 繁體中文
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
+    end
+
+    return lang
+end
+
+local language = getSystemLanguage()
+
+if language == "简体中文" then
+    swsmsg = "该脚本需要 SWS 扩展，你想现在就下载它吗？"
+    swserr = "警告"
+    jsmsg = "请右键单击並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然后重新启动 REAPER 並再次运行脚本，谢谢！\n"
+    jstitle = "你必须安裝 JS_ReaScriptAPI"
+    jserr = "错误"
+elseif language == "繁体中文" then
+    swsmsg = "該脚本需要 SWS 擴展，你想現在就下載它嗎？"
+    swserr = "警告"
+    jsmsg = "請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n"
+    jstitle = "你必須安裝 JS_ReaScriptAPI"
+    jserr = "錯誤"
+else
+    swsmsg = "This script requires the SWS Extension. Do you want to download it now?"
+    swserr = "Warning"
+    jsmsg = "Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\nThen restart REAPER and run the script again, thank you!\n"
+    jstitle = "You must install JS_ReaScriptAPI"
+    jserr = "Error"
+end
+
+if not reaper.SNM_GetIntConfigVar then
+    local retval = reaper.ShowMessageBox(swsmsg, swserr, 1)
+    if retval == 1 then
+        if not OS then local OS = reaper.GetOS() end
+        if OS=="OSX32" or OS=="OSX64" then
+            os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+        else
+            os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+        end
+    end
+    return
+end
+
+if not reaper.APIExists("JS_Localize") then
+    reaper.MB(jsmsg, jstitle, 0)
+    local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+    if ok then
+      reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+    else
+      reaper.MB(err, jserr, 0)
+    end
+    return reaper.defer(function() end)
+end
+
 local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
 local tick = reaper.SNM_GetIntConfigVar("MidiTicksPerBeat", 480)
 local cur_grid, swing = reaper.MIDI_GetGrid(take)
@@ -47,11 +132,22 @@ end
 local grid = reaper.GetExtState("Quantize", "Grid")
 if (grid == "") then grid = "120" end
 local toggle = reaper.GetExtState("Quantize", "Toggle")
-if (toggle == "") then toggle = "0" end
+if (toggle == "") then toggle = "dft" end
 
-local user_ok, input_cav = reaper.GetUserInputs('Quantize', 2, 'Enter A Tick,0=Default 1=Start 2=End 3=Pos', grid ..','.. toggle)
-grid, toggle = input_cav:match("(.*),(.*)")
-if not user_ok or not tonumber(grid) or not tonumber(toggle) then return reaper.SN_FocusMIDIEditor() end
+if language == "简体中文" then
+    title = "量化"
+    captions_csv = "输入嘀嗒数,量化 (dft/start/end/pos)"
+elseif language == "繁体中文" then
+    title = "量化"
+    captions_csv = "輸入嘀嗒数,量化 (dft/start/end/pos)"
+else
+    title = "Quantize"
+    captions_csv = "Enter A Tick,Qnz (dft/start/end/pos)"
+end
+
+local uok, uinput = reaper.GetUserInputs(title, 2, captions_csv, grid ..','.. toggle)
+grid, toggle = uinput:match("(.*),(.*)")
+if not uok or not tonumber(grid) or not tostring(toggle) then return reaper.SN_FocusMIDIEditor() end
 
 reaper.SetExtState("Quantize", "Grid", grid, false)
 reaper.SetExtState("Quantize", "Toggle", toggle, false)
@@ -170,7 +266,7 @@ function TextSysEvents()
         end
     end
 end
-function Main()
+function main()
     reaper.Undo_BeginBlock()
     reaper.MIDI_DisableSort(take)
     
@@ -180,37 +276,28 @@ function Main()
         reaper.MIDIEditor_LastFocused_OnCommand(40681,0) -- Options: Correct overlapping notes while editing
         flag = true
     end
-    if toggle == "3" then
+    if toggle == "pos" then
         Position()
-    elseif toggle == "2" then
+    elseif toggle == "end" then
         NoteDurations()
-    elseif toggle == "1" then
+    elseif toggle == "start" then
         StartTimes()
-    else
+    elseif toggle == "dft" then
         StartTimes()
         NoteDurations()
         CCEvents()
         TextSysEvents()
     end
     reaper.MIDIEditor_OnCommand(reaper.MIDIEditor_GetActive(), 40659)
-
     reaper.MIDI_Sort(take)
 
     if flag then
         reaper.MIDIEditor_LastFocused_OnCommand(40681,0) -- Options: Correct overlapping notes while editing
     end
 
-    reaper.UpdateArrange()
-    reaper.Undo_EndBlock("Quantize", -1)
+    reaper.Undo_EndBlock(title, -1)
 end
 
-if not reaper.SN_FocusMIDIEditor then
-    local retval = reaper.ShowMessageBox("SWS extension is required by this script.\n此腳本需要 SWS 擴展。\nHowever, it doesn't seem to be present for this REAPER installation.\n然而，對於這個REAPER安裝來說，它似乎並不存在。\n\nDo you want to download it now ?\n你想現在就下載它嗎？", "Warning", 1)
-    if retval == 1 then
-      Open_URL("http://www.sws-extension.org/download/pre-release/")
-    end
-    return
-end
-
-Main()
+main()
+reaper.UpdateArrange()
 reaper.SN_FocusMIDIEditor()
