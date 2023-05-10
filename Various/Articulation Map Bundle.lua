a1f7d1e898e0a5a4eb15ab065241612e75baa3d5
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2023-05-10 13:54:31 +0800

    Update

---
+++ Various/Articulation Map Bundle.lua
@@ -0,0 +1,19 @@
+-- @description Articulation Map Bundle
+-- @author zaibuyidao
+-- @version 1.0
+-- @changelog
+--   Initial release
+-- @links
+--   https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   https://github.com/zaibuyidao/ReaScripts
+-- @metapackage
+-- @provides
+--   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map Note To PC.lua
+--   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map Patch Change (Without GUI).lua
+--   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map Patch Change.lua
+--   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map PC To Note.lua
+--   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map Toggle Note PC.lua
+-- @donation http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
+
+-- Licensed under the GNU GPL v3
