3c4d2c6b90ff14cb10c77a1caba492061a657223
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2023-08-24 14:11:07 +0800

    Update zaibuyidao_Solo Track Virtual Key Setting.lua

--- Tracks Properties/zaibuyidao_Solo Track Virtual Key Setting.lua
+++ Tracks Properties/zaibuyidao_Solo Track Virtual Key Setting.lua
@@ -1,5 +1,5 @@
 -- @description Solo Track Virtual Key Setting
--- @version 1.0
+-- @version 1.0.1
 -- @author zaibuyidao
 -- @changelog Initial release
 -- @links
@@ -81,77 +81,27 @@ function getSystemLanguage()
   return lang
 end
 
-key_map = { 
-  ['0'] = 0x30,
-  ['1'] = 0x31,
-  ['2'] = 0x32,
-  ['3'] = 0x33,
-  ['4'] = 0x34,
-  ['5'] = 0x35,
-  ['6'] = 0x36,
-  ['7'] = 0x37,
-  ['8'] = 0x38,
-  ['9'] = 0x39,
-  ['A'] = 0x41,
-  ['B'] = 0x42,
-  ['C'] = 0x43,
-  ['D'] = 0x44,
-  ['E'] = 0x45,
-  ['F'] = 0x46,
-  ['G'] = 0x47,
-  ['H'] = 0x48,
-  ['I'] = 0x49,
-  ['J'] = 0x4A,
-  ['K'] = 0x4B,
-  ['L'] = 0x4C,
-  ['M'] = 0x4D,
-  ['N'] = 0x4E,
-  ['O'] = 0x4F,
-  ['P'] = 0x50,
-  ['Q'] = 0x51,
-  ['R'] = 0x52,
-  ['S'] = 0x53,
-  ['T'] = 0x54,
-  ['U'] = 0x55,
-  ['V'] = 0x56,
-  ['W'] = 0x57,
-  ['X'] = 0x58,
-  ['Y'] = 0x59,
-  ['Z'] = 0x5A,
-  ['a'] = 0x41,
-  ['b'] = 0x42,
-  ['c'] = 0x43,
-  ['d'] = 0x44,
-  ['e'] = 0x45,
-  ['f'] = 0x46,
-  ['g'] = 0x47,
-  ['h'] = 0x48,
-  ['i'] = 0x49,
-  ['j'] = 0x4A,
-  ['k'] = 0x4B,
-  ['l'] = 0x4C,
-  ['m'] = 0x4D,
-  ['n'] = 0x4E,
-  ['o'] = 0x4F,
-  ['p'] = 0x50,
-  ['q'] = 0x51,
-  ['r'] = 0x52,
-  ['s'] = 0x53,
-  ['t'] = 0x54,
-  ['u'] = 0x55,
-  ['v'] = 0x56,
-  ['w'] = 0x57,
-  ['x'] = 0x58,
-  ['y'] = 0x59,
-  ['z'] = 0x5A,
-  [','] = 0xBC,
-  ['.'] = 0xBE,
-  ['<'] = 0xBC,
-  ['>'] = 0xBE
-}
+local function generateKeyMap()
+  local map = {}
+  for i = 0, 9 do
+    map[tostring(i)] = 0x30 + i
+  end
+  for i = 0, 25 do
+    local char = string.char(65 + i)  -- Uppercase A-Z
+    map[char] = 0x41 + i
+    char = string.char(97 + i)  -- Lowercase a-z
+    map[char] = 0x41 + i  -- Virtual Key Codes are the same for uppercase
+  end
+  map[','] = 0xBC
+  map['.'] = 0xBE
+  map['<'] = 0xBC
+  map['>'] = 0xBE
+  return map
+end
+
+key_map = generateKeyMap()
 
 local language = getSystemLanguage()
-
 if language == "简体中文" then
   title = "独奏轨道虚拟键设置"
   lable = "输入 (0-9, A-Z, 使用';;'代替','或.)"
@@ -169,7 +119,7 @@ else
   lable = "Enter (0-9, A-Z, use ';;' for ',' or .)"
   err_title = "This key can't be set. Please choose another."
   okk_title = "Settings applied. Please reactivate the Solo Track script."
-  okk_box = "Completed"
+  okk_box = "Completed!"
 end
 
 reaper.Undo_BeginBlock()
