5cac91463f2a1e3b7cda8c46b39f883fbd0cc93f
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2023-09-02 20:40:25 +0800

    Update zaibuyidao_Length (Fast).lua

--- MIDI Editor/zaibuyidao_Length (Fast).lua
+++ MIDI Editor/zaibuyidao_Length (Fast).lua
@@ -1,12 +1,12 @@
 -- @description Length (Fast)
--- @version 1.0.1
+-- @version 1.0.2
 -- @author zaibuyidao
--- @changelog Optimized code
+-- @changelog Resolve the error in MIDI event scaling.
 -- @links
 --   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
---   repository https://github.com/zaibuyidao/ReaScripts
+--   repo https://github.com/zaibuyidao/ReaScripts
 -- @donate http://www.paypal.me/zaibuyidao
--- @about Requires SWS Extensions
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
 function print(...)
     local params = {...}
@@ -27,21 +27,13 @@ function table.print(t)
             if (type(t) == "table") then
                 for pos, val in pairs(t) do
                     if (type(val) == "table") then
-                        print(indent .. "[" .. tostring(pos) .. "] => " ..
-                                  tostring(t) .. " {")
-                        sub_print_r(val, indent ..
-                                        string.rep(" ",
-                                                   string.len(tostring(pos)) + 8))
-                        print(indent ..
-                                  string.rep(" ", string.len(tostring(pos)) + 6) ..
-                                  "}")
+                        print(indent .. "[" .. tostring(pos) .. "] => " .. tostring(t) .. " {")
+                        sub_print_r(val, indent .. string.rep(" ", string.len(tostring(pos)) + 8))
+                        print(indent .. string.rep(" ", string.len(tostring(pos)) + 6) .. "}")
                     elseif (type(val) == "string") then
-                        print(
-                            indent .. "[" .. tostring(pos) .. '] => "' .. val ..
-                                '"')
+                        print(indent .. "[" .. tostring(pos) .. '] => "' .. val .. '"')
                     else
-                        print(indent .. "[" .. tostring(pos) .. "] => " ..
-                                  tostring(val))
+                        print(indent .. "[" .. tostring(pos) .. "] => " .. tostring(val))
                     end
                 end
             else
@@ -58,20 +50,89 @@ function table.print(t)
     end
 end
 
-function open_url(url)
-    if not OS then local OS = reaper.GetOS() end
-    if OS=="OSX32" or OS=="OSX64" then
-        os.execute("open ".. url)
-    else
-        os.execute("start ".. url)
+function getSystemLanguage()
+    local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+    local os = reaper.GetOS()
+    local lang
+  
+    if os == "Win32" or os == "Win64" then -- Windows
+      if locale == 936 then -- Simplified Chinese
+        lang = "简体中文"
+      elseif locale == 950 then -- Traditional Chinese
+        lang = "繁體中文"
+      else -- English
+        lang = "English"
+      end
+    elseif os == "OSX32" or os == "OSX64" then -- macOS
+      local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+      local result = handle:read("*a")
+      handle:close()
+      lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+      if lang == "zh-CN" then -- 简体中文
+        lang = "简体中文"
+      elseif lang == "zh-TW" then -- 繁体中文
+        lang = "繁體中文"
+      else -- English
+        lang = "English"
+      end
+    elseif os == "Linux" then -- Linux
+      local handle = io.popen("echo $LANG")
+      local result = handle:read("*a")
+      handle:close()
+      lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+      if lang == "zh_CN" then -- 简体中文
+        lang = "简体中文"
+      elseif lang == "zh_TW" then -- 繁體中文
+        lang = "繁體中文"
+      else -- English
+        lang = "English"
+      end
     end
+  
+    return lang
+end
+
+local language = getSystemLanguage()
+
+if language == "简体中文" then
+    swsmsg = "该脚本需要 SWS 扩展，你想现在就下载它吗？"
+    swserr = "警告"
+    jsmsg = "请右键单击並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然后重新启动 REAPER 並再次运行脚本，谢谢！\n"
+    jstitle = "你必须安裝 JS_ReaScriptAPI"
+elseif language == "繁体中文" then
+    swsmsg = "該脚本需要 SWS 擴展，你想現在就下載它嗎？"
+    swserr = "警告"
+    jsmsg = "請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n"
+    jstitle = "你必須安裝 JS_ReaScriptAPI"
+else
+    swsmsg = "This script requires the SWS Extension. Do you want to download it now?"
+    swserr = "Warning"
+    jsmsg = "Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\nThen restart REAPER and run the script again, thank you!\n"
+    jstitle = "You must install JS_ReaScriptAPI"
 end
 
 if not reaper.SN_FocusMIDIEditor then
-    local retval = reaper.ShowMessageBox("這個脚本需要SWS擴展，你想現在就下載它嗎？", "Warning", 1)
+    local retval = reaper.ShowMessageBox(swsmsg, swserr, 1)
     if retval == 1 then
-        open_url("http://www.sws-extension.org/download/pre-release/")
+        if not OS then local OS = reaper.GetOS() end
+        if OS=="OSX32" or OS=="OSX64" then
+            os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+        else
+            os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+        end
     end
+    return
+end
+
+if not reaper.APIExists("JS_Localize") then
+    reaper.MB(jsmsg, jstitle, 0)
+    local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+    if ok then
+      reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+    else
+      reaper.MB(err, jserr, 0)
+    end
+    return reaper.defer(function() end)
 end
 
 function clone(object)
@@ -123,13 +184,10 @@ function getAllEvents(take, onEach)
     }
     local setters = {
         pitch = function(event, value)
-            event.msg = string.pack("BBB", event.msg:byte(1),
-                                    value or event.msg:byte(2),
-                                    event.msg:byte(3))
+            event.msg = string.pack("BBB", event.msg:byte(1), value or event.msg:byte(2), event.msg:byte(3))
         end,
         velocity = function(event, value)
-            event.msg = string.pack("BBB", event.msg:byte(1), event.msg:byte(2),
-                                    value or event.msg:byte(3))
+            event.msg = string.pack("BBB", event.msg:byte(1), event.msg:byte(2), value or event.msg:byte(3))
         end,
         selected = function(event, value)
             if value then
@@ -200,8 +258,7 @@ function getAllTakes()
 end
 
 -- 返回最末尾事件qn位置
-function scaleEvents(take, all_events, note_events, events, qn_range, percent,
-                     scale_start, scale_dur)
+function scaleEvents(take, all_events, note_events, events, qn_range, percent, scale_start, scale_dur)
     local max_qn = -math.huge
 
     local function calc_pos(pos)
@@ -241,19 +298,19 @@ function scaleEvents(take, all_events, note_events, events, qn_range, percent,
     for _, note_event in ipairs(note_events) do
         local note_len = note_event.right.pos - note_event.left.pos
         if scale_start and scale_dur then
-            note_event.left.pos = calc_pos(note_event.left.pos)
-            note_event.right.pos = calc_pos(note_event.right.pos)
+            note_event.left.pos = math.floor(calc_pos(note_event.left.pos))
+            note_event.right.pos = math.floor(calc_pos(note_event.right.pos))
             if note_event.articulation then
                 note_event.articulation.pos = note_event.left.pos
             end
         elseif scale_start then
-            note_event.left.pos = calc_pos(note_event.left.pos)
-            note_event.right.pos = note_event.left.pos + note_len
+            note_event.left.pos = math.floor(calc_pos(note_event.left.pos))
+            note_event.right.pos = math.floor(note_event.left.pos + note_len)
             if note_event.articulation then
                 note_event.articulation.pos = note_event.left.pos
             end
         elseif scale_dur then
-            note_event.right.pos = note_event.left.pos + (percent * note_len)
+            note_event.right.pos = math.floor(note_event.left.pos + (percent * note_len))
         end
         update_max_qn(note_event.right.pos)
     end
@@ -261,17 +318,28 @@ function scaleEvents(take, all_events, note_events, events, qn_range, percent,
     return process_item_extend()
 end
 
-local percent = reaper.GetExtState("Length", "Percent")
+if language == "简体中文" then
+    title = "长度"
+    lable = "百分比,0=起始+持续 1=起始 2=持续"
+elseif language == "繁体中文" then
+    title = "長度"
+    lable = "百分比,0=起始+持續 1=起始 2=持續"
+else
+    title = "Length"
+    lable = "Percent,0=Start+Dur 1=Start 2=Durations"
+end
+
+local percent = reaper.GetExtState("LENGTH", "Percent")
 if (percent == "") then percent = "200" end
-local toggle = reaper.GetExtState("Length", "Toggle")
+local toggle = reaper.GetExtState("LENGTH", "Toggle")
 if (toggle == "") then toggle = "0" end
 
-local uok, uinput = reaper.GetUserInputs('Length', 2, 'Percent,0=Start+Dur 1=Start 2=Durations', percent .. ',' .. toggle)
-if not uok or not tonumber(percent) or not tonumber(toggle) then return reaper.SN_FocusMIDIEditor() end
-percent, toggle = uinput:match("(%d*),(%d*)")
+local retval, retvals_csv = reaper.GetUserInputs(title, 2, lable, percent .. ',' .. toggle)
+if not retval or not tonumber(percent) or not tonumber(toggle) then return reaper.SN_FocusMIDIEditor() end
+percent, toggle = retvals_csv:match("(%d*),(%d*)")
 
-reaper.SetExtState("Length", "Percent", percent, false)
-reaper.SetExtState("Length", "Toggle", toggle, false)
+reaper.SetExtState("LENGTH", "Percent", percent, false)
+reaper.SetExtState("LENGTH", "Toggle", toggle, false)
 
 percent = tonumber(percent) / 100
 
@@ -371,7 +439,7 @@ for take, events in pairs(all_events) do
     reaper.MIDI_Sort(take)
 end
 
-reaper.Undo_EndBlock("Length (Fast)", -1)
+reaper.Undo_EndBlock(title, -1)
 reaper.PreventUIRefresh(-1)
 reaper.UpdateArrange()
 reaper.SN_FocusMIDIEditor()
