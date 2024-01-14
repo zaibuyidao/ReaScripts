2094ef94020e6358fa52c3f54946f34b35d2fdc6
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2024-01-14 20:45:39 +0800

    Update

--- MIDI Editor/zaibuyidao_Random CC Position (Within Time Selection).lua
+++ MIDI Editor/zaibuyidao_Random CC Position (Within Time Selection).lua
@@ -1,51 +1,180 @@
---[[
- * ReaScript Name: Random CC Position (Within Time Selection)
- * Instructions: Open a MIDI take in MIDI Editor. Set Time Selection, Run.
- * Version: 2.2
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository: GitHub > zaibuyidao > ReaScripts
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * REAPER: 6.0
- * Donation: http://www.paypal.me/zaibuyidao
---]]
+-- @description Random CC Position (Within Time Selection)
+-- @version 3.0
+-- @author zaibuyidao
+-- @changelog Initial release
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
---[[
- * Changelog:
- * v2.0 (2019-1-5)
-  + Version update
- * v1.0 (2019-12-12)
-  + Initial release
---]]
+-- USER AREA
+-- Settings that the user can customize.
 
-function Main()
-  local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
-  if take == nil then return end
-  time_start, time_end = reaper.GetSet_LoopTimeRange2( 0, false, false, 0, 0, 0)
-  local loop_start = math.floor(0.5 + reaper.MIDI_GetPPQPosFromProjTime(take, time_start))
-  local loop_end = math.floor(0.5 + reaper.MIDI_GetPPQPosFromProjTime(take, time_end))
-  local loop_len = loop_end - loop_start - 1
-  local cnt, index = 0, {}
-  local val = reaper.MIDI_EnumSelCC(take, -1)
-  reaper.MIDI_DisableSort(take)
-  while val ~= - 1 do
-    cnt = cnt + 1
-    index[cnt] = val
-    val = reaper.MIDI_EnumSelCC(take, val)
+local gridEnabled = true -- Set to true to enable grid-based randomization
+
+-- End of USER AREA
+
+function print(...)
+  for _, v in ipairs({...}) do
+    reaper.ShowConsoleMsg(tostring(v) .. " ")
   end
-  if #index > 1 then
-    for i = 1, #index do
-      local retval, sel, muted, ppqpos, chanmsg, chan, msg2, msg3 = reaper.MIDI_GetCC(take, index[i])
-      reaper.MIDI_SetCC(take, index[i], sel, muted, loop_start+math.random(loop_len), chanmsg, chan, msg2, msg3, false)
-    end
-  end
-  reaper.UpdateArrange()
-  reaper.MIDI_Sort(take)
+  reaper.ShowConsoleMsg("\n")
 end
 
-script_title = "Random CC Position"
+function getSystemLanguage()
+  local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+  local os = reaper.GetOS()
+  local lang
+
+  if os == "Win32" or os == "Win64" then -- Windows
+      if locale == 936 then -- Simplified Chinese
+          lang = "简体中文"
+      elseif locale == 950 then -- Traditional Chinese
+          lang = "繁體中文"
+      else -- English
+          lang = "English"
+      end
+  elseif os == "OSX32" or os == "OSX64" then -- macOS
+      local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+      local result = handle:read("*a")
+      handle:close()
+      lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+      if lang == "zh-CN" then -- 简体中文
+          lang = "简体中文"
+      elseif lang == "zh-TW" then -- 繁体中文
+          lang = "繁體中文"
+      else -- English
+          lang = "English"
+      end
+  elseif os == "Linux" then -- Linux
+      local handle = io.popen("echo $LANG")
+      local result = handle:read("*a")
+      handle:close()
+      lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+      if lang == "zh_CN" then -- 简体中文
+          lang = "简体中文"
+      elseif lang == "zh_TW" then -- 繁體中文
+          lang = "繁體中文"
+      else -- English
+          lang = "English"
+      end
+  end
+
+  return lang
+end
+
+local language = getSystemLanguage()
+
+if language == "简体中文" then
+  title = "随机CC位置"
+elseif language == "繁体中文" then
+  title = "隨機CC位置"
+else
+  title = "Random CC Position"
+end
+
+function isCCOverlap(take, ppqpos, index, currentIndex)
+  for j = 1, #index do
+    if j ~= currentIndex then
+      local _, _, _, other_ppqpos = reaper.MIDI_GetCC(take, index[j])
+      if ppqpos == other_ppqpos then
+        return true
+      end
+    end
+  end
+  return false
+end
+
+function getGrid(take)
+  local midi_tick = reaper.SNM_GetIntConfigVar("MidiTicksPerBeat", 480)
+  local grid_qn = reaper.MIDI_GetGrid(take)
+  return math.floor(midi_tick * grid_qn)
+end
+
+function Main(gridEnabled)
+  local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
+  if take == nil then return end
+
+  local grid = gridEnabled and getGrid(take) or 1
+  local time_start, time_end = reaper.GetSet_LoopTimeRange2(0, false, false, 0, 0, 0)
+  local loop_start, loop_end
+
+  local index = {}
+  local min_ppq, max_ppq
+  local val = reaper.MIDI_EnumSelCC(take, -1)
+  while val ~= -1 do
+    local _, _, _, ppqpos = reaper.MIDI_GetCC(take, val)
+    min_ppq = (min_ppq == nil) and ppqpos or math.min(min_ppq, ppqpos)
+    max_ppq = (max_ppq == nil) and ppqpos or math.max(max_ppq, ppqpos)
+    table.insert(index, val)
+    val = reaper.MIDI_EnumSelCC(take, val)
+  end
+
+  if #index == 0 then return end  -- 如果没有选中的 CC 事件，直接返回
+
+  if time_start == time_end then
+    -- 没有时间选区，根据选中的CC事件创建时间选区
+    time_start = reaper.MIDI_GetProjTimeFromPPQPos(take, min_ppq)
+    time_end = reaper.MIDI_GetProjTimeFromPPQPos(take, max_ppq)
+    reaper.GetSet_LoopTimeRange2(0, true, false, time_start, time_end, false)
+  end
+
+  loop_start = math.floor(0.5 + reaper.MIDI_GetPPQPosFromProjTime(take, time_start))
+  loop_end = math.floor(0.5 + reaper.MIDI_GetPPQPosFromProjTime(take, time_end))
+  local loop_len = loop_end - loop_start
+
+  -- 计算基于网格的时间选区的开始和结束位置
+  if gridEnabled then
+    -- 当时间选区的开始或结束正好在网格上时，保持它们不变
+    if loop_start % grid ~= 0 then
+      loop_start = loop_start + grid - (loop_start % grid)
+    end
+    if loop_end % grid ~= 0 then
+      loop_end = loop_end - (loop_end % grid)
+    end
+    if loop_start >= loop_end then return end -- 确保时间选区在网格范围内
+  end
+
+  reaper.MIDI_DisableSort(take)
+  for i, idx in ipairs(index) do
+    local retval, sel, muted, ppqpos, chanmsg, chan, msg2, msg3 = reaper.MIDI_GetCC(take, idx)
+    local rand_pos
+    local retry_count = 0
+    local max_retries = 50
+
+    repeat
+      if gridEnabled then
+        -- 确保随机位置包括时间选区的起始位置
+        local grid_count = math.floor((loop_end - loop_start) / grid)
+        local rand_grid = math.random(0, grid_count) * grid
+        rand_pos = loop_start + rand_grid
+
+        -- 防止随机位置超出时间选区
+        if rand_pos >= loop_end then
+          rand_pos = loop_end - grid
+        end
+      else
+        -- 非网格随机时确保随机位置不超出时间选区
+        rand_pos = math.random(loop_start, loop_end - 1)
+      end
+
+      retry_count = retry_count + 1
+      if retry_count > max_retries then
+        break
+      end
+    until not isCCOverlap(take, rand_pos, index, i)
+
+    if retry_count <= max_retries then
+      reaper.MIDI_SetCC(take, idx, sel, muted, rand_pos, chanmsg, chan, msg2, msg3, false)
+    end
+  end
+  
+  reaper.MIDI_Sort(take)
+  reaper.UpdateArrange()
+end
+
 reaper.Undo_BeginBlock()
 reaper.MIDIEditor_LastFocused_OnCommand(40747, 0) -- Edit: Select all CC events in time selection (in last clicked CC lane)
-Main()
-reaper.Undo_EndBlock(script_title, 0)
-reaper.SN_FocusMIDIEditor()
+Main(gridEnabled)
+reaper.Undo_EndBlock(title, -1)
--- MIDI Editor/zaibuyidao_Random Note Position By Grid (Within Time Selection).lua
+++ MIDI Editor/zaibuyidao_Random Note Position By Grid (Within Time Selection).lua
@@ -1,12 +1,12 @@
 -- @description Random Note Position By Grid (Within Time Selection)
--- @version 1.0.1
+-- @version 2.0
 -- @author zaibuyidao
 -- @changelog Initial release
 -- @links
 --   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
 --   repo https://github.com/zaibuyidao/ReaScripts
 -- @donate http://www.paypal.me/zaibuyidao
--- @about Requires SWS Extensions
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
 -- USER AREA
 -- Settings that the user can customize.
@@ -15,35 +15,155 @@ midi_grid = 1 -- 1 is the default MIDI grid. Random intervals are based on the M
 
 -- End of USER AREA
 
+function print(...)
+    for _, v in ipairs({...}) do
+        reaper.ShowConsoleMsg(tostring(v) .. " ")
+    end
+    reaper.ShowConsoleMsg("\n")
+end
+
+function getSystemLanguage()
+    local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+    local os = reaper.GetOS()
+    local lang
+  
+    if os == "Win32" or os == "Win64" then -- Windows
+        if locale == 936 then -- Simplified Chinese
+            lang = "简体中文"
+        elseif locale == 950 then -- Traditional Chinese
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
+    elseif os == "OSX32" or os == "OSX64" then -- macOS
+        local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+        local result = handle:read("*a")
+        handle:close()
+        lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+        if lang == "zh-CN" then -- 简体中文
+            lang = "简体中文"
+        elseif lang == "zh-TW" then -- 繁体中文
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
+    elseif os == "Linux" then -- Linux
+        local handle = io.popen("echo $LANG")
+        local result = handle:read("*a")
+        handle:close()
+        lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+        if lang == "zh_CN" then -- 简体中文
+            lang = "简体中文"
+        elseif lang == "zh_TW" then -- 繁體中文
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
+    end
+  
+    return lang
+end
+
+local language = getSystemLanguage()
+  
+if language == "简体中文" then
+    title = "按网格随机音符位置"
+elseif language == "繁体中文" then
+    title = "按網格隨機音符位置"
+else
+    title = "Random Note Position By Grid"
+end
+
+function isOverlap(take, rand_pos, notelen, notecnt, currentIndex)
+    for j = 0, notecnt - 1 do
+        if j ~= currentIndex then
+            _, _, _, startppqpos, endppqpos = reaper.MIDI_GetNote(take, j)
+            -- 检查随机位置是否与任何现有音符重叠
+            if (rand_pos < endppqpos and rand_pos + notelen > startppqpos) then
+                return true
+            end
+        end
+    end
+    return false
+end
+
 function main()
     local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
     local midi_tick = reaper.SNM_GetIntConfigVar("MidiTicksPerBeat", 480)
     local grid_qn = reaper.MIDI_GetGrid(take)
     local grid = math.floor(midi_tick*grid_qn)*midi_grid
-    local time_start, time_end = reaper.GetSet_LoopTimeRange2(0, false, false, 0, 0, 0)
-    local loop_start = math.floor(0.5+reaper.MIDI_GetPPQPosFromProjTime(take, time_start))
-    local loop_end = math.floor(0.5+reaper.MIDI_GetPPQPosFromProjTime(take, time_end))
-    local loop_len = math.floor(loop_end-loop_start)
-    local times = math.floor(loop_len/grid) -- Get all the intervals in the loop length
-    local new_loop_start = loop_start-loop_start%grid -- Get the start grid position
+
     _, notecnt, _, _ = reaper.MIDI_CountEvts(take)
+    if notecnt == 0 then return end
+
+    local time_start, time_end = reaper.GetSet_LoopTimeRange2(0, false, false, 0, 0, 0)
+    local loop_start, loop_end, loop_len
+
+    if time_start == time_end then
+        -- 没有时间选区，寻找选中音符的范围
+        local min_start, max_end
+        for i = 0, notecnt - 1 do
+            local _, selected, _, startppqpos, endppqpos = reaper.MIDI_GetNote(take, i)
+            if selected then
+                if not min_start or startppqpos < min_start then
+                    min_start = startppqpos
+                end
+                if not max_end or endppqpos > max_end then
+                    max_end = endppqpos
+                end
+            end
+        end
+        if min_start and max_end then
+            time_start = reaper.MIDI_GetProjTimeFromPPQPos(take, min_start)
+            time_end = reaper.MIDI_GetProjTimeFromPPQPos(take, max_end)
+            reaper.GetSet_LoopTimeRange2(0, true, false, time_start, time_end, false)
+        else
+            return -- 没有选中的音符
+        end
+    end
+
+    loop_start = math.floor(0.5+reaper.MIDI_GetPPQPosFromProjTime(take, time_start))
+    loop_end = math.floor(0.5+reaper.MIDI_GetPPQPosFromProjTime(take, time_end))
+    loop_len = math.floor(loop_end - loop_start)
+    local times = math.floor(loop_len/grid)
+    local new_loop_start = loop_start - loop_start % grid
 
     reaper.MIDI_DisableSort(take)
     sel = reaper.MIDI_EnumSelNotes(take, -1)
     if sel ~= -1 then sel_note = true end
-    if loop_len == 0  then return reaper.SN_FocusMIDIEditor() end
     local flag
     if reaper.GetToggleCommandStateEx(32060, 40681) == 1 then
         reaper.MIDIEditor_LastFocused_OnCommand(40681,0) -- Options: Correct overlapping notes while editing
         flag = true
     end
+
     for i = 1, notecnt do
         _, selected, muted, startppqpos, endppqpos, chan, pitch, vel = reaper.MIDI_GetNote(take, i - 1)
         notelen = endppqpos - startppqpos
         if selected then
-            local rand_grid = math.random(times)*grid
-            local rand_pos = new_loop_start+rand_grid-grid
-            reaper.MIDI_SetNote(take, i - 1, nil, nil, rand_pos, rand_pos+notelen, nil, nil, nil, false)
+            local retry_count = 0
+            local max_retries = 50
+            local rand_pos
+
+            repeat
+                local rand_grid = math.random(times) * grid
+                rand_pos = new_loop_start + rand_grid - grid
+
+                if rand_pos + notelen > loop_end then
+                    rand_pos = loop_end - notelen
+                end
+
+                retry_count = retry_count + 1
+                if retry_count > max_retries then
+                    -- reaper.ShowConsoleMsg("Max retries reached for note " .. i .. "\n")
+                    break
+                end
+            until not isOverlap(take, rand_pos, notelen, notecnt, i - 1)
+
+            if retry_count <= max_retries then
+                -- reaper.ShowConsoleMsg("Moving note " .. i .. " to position " .. rand_pos .. "\n")
+                reaper.MIDI_SetNote(take, i - 1, nil, nil, rand_pos, rand_pos + notelen, nil, nil, nil, false)
+            end
         end
     end
     reaper.MIDI_Sort(take)
@@ -55,5 +175,5 @@ end
 
 reaper.Undo_BeginBlock()
 main()
-reaper.Undo_EndBlock("Random Note Position By Grid", -1)
+reaper.Undo_EndBlock(title, -1)
 reaper.UpdateArrange()
