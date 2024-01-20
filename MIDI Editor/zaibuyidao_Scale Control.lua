65e1e6376cd14b2d0c667e8a4cdba1039df2aa29
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2024-01-20 15:27:40 +0800

    Update

--- MIDI Editor/zaibuyidao_Scale Control.lua
+++ MIDI Editor/zaibuyidao_Scale Control.lua
@@ -1,23 +1,79 @@
---[[
- * ReaScript Name: Scale Control
- * Instructions: Open a MIDI take in MIDI Editor. Select CC Events. Run.
- * Version: 1.0
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository: GitHub > zaibuyidao > ReaScripts
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * REAPER: 6.0
- * Donation: http://www.paypal.me/zaibuyidao
---]]
+-- @description Scale Control
+-- @version 1.0.1
+-- @author zaibuyidao
+-- @changelog
+--   + Add Multi-Language Support
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
---[[
- * Changelog:
- * v1.0 (2020-8-26)
-  + Initial release
---]]
+function print(...)
+  for _, v in ipairs({...}) do
+    reaper.ShowConsoleMsg(tostring(v) .. " ")
+  end
+  reaper.ShowConsoleMsg("\n")
+end
+
+function getSystemLanguage()
+  local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+  local os = reaper.GetOS()
+  local lang
+
+  if os == "Win32" or os == "Win64" then -- Windows
+    if locale == 936 then -- Simplified Chinese
+      lang = "简体中文"
+    elseif locale == 950 then -- Traditional Chinese
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "OSX32" or os == "OSX64" then -- macOS
+    local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+    if lang == "zh-CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh-TW" then -- 繁体中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "Linux" then -- Linux
+    local handle = io.popen("echo $LANG")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+    if lang == "zh_CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh_TW" then -- 繁體中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  end
+
+  return lang
+end
+
+local language = getSystemLanguage()
+
+if not reaper.SN_FocusMIDIEditor then
+  local retval = reaper.ShowMessageBox(swsmsg, swserr, 1)
+  if retval == 1 then
+    if not OS then local OS = reaper.GetOS() end
+    if OS=="OSX32" or OS=="OSX64" then
+      os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+    else
+      os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+    end
+  end
+  return
+end
 
 function main()
-  local script_title = "Scale Control"
   local take=reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
   if take == nil then return end
   local cnt, index = 0, {}
@@ -27,25 +83,41 @@ function main()
     index[cnt] = val
     val = reaper.MIDI_EnumSelCC(take, val)
   end
+
   reaper.Undo_BeginBlock()
   reaper.MIDI_DisableSort(take)
   if #index > 0 then
-    local val_start = reaper.GetExtState("ScaleControl", "Start")
-    local val_end = reaper.GetExtState("ScaleControl", "End")
-    local toggle = reaper.GetExtState("ScaleControl", "Toggle")
+    local title, captions_csv = "", ""
+    if language == "简体中文" then
+      title = "控制器缩放"
+      captions_csv = "开始,结束,0=绝对值 1=百分比"
+    elseif language == "繁体中文" then
+      title = "控制器縮放"
+      captions_csv = "開始,結束,0=絕對值 1=百分比"
+    else
+      title = "Scale Control"
+      captions_csv = "Begin,End,0=Default 1=Percentages"
+    end
+
+    local val_start = reaper.GetExtState("SCALE_CONTROL", "Start")
+    local val_end = reaper.GetExtState("SCALE_CONTROL", "End")
+    local toggle = reaper.GetExtState("SCALE_CONTROL", "Toggle")
     if (val_start == "") then val_start = "100" end
     if (val_end == "") then val_end = "100" end
     if (toggle == "") then toggle = "0" end
-    local userOK, userInputsCSV = reaper.GetUserInputs("Scale Control", 3, "Begin,End,0=Default 1=Percentages", val_start..','..val_end..','.. toggle)
+
+    local userOK, userInputsCSV = reaper.GetUserInputs(title, 3, captions_csv, val_start..','..val_end..','.. toggle)
     if not userOK then return reaper.SN_FocusMIDIEditor() end
     val_start, val_end, toggle = userInputsCSV:match("(%d*),(%d*),(%d*)")
     if not val_start:match('[%d%.]+') or not val_end:match('[%d%.]+') or not toggle:match('[%d%.]+') then return reaper.SN_FocusMIDIEditor() end
-    reaper.SetExtState("ScaleControl", "Start", val_start, false)
-    reaper.SetExtState("ScaleControl", "End", val_end, false)
-    reaper.SetExtState("ScaleControl", "Toggle", toggle, false)
+    reaper.SetExtState("SCALE_CONTROL", "Start", val_start, false)
+    reaper.SetExtState("SCALE_CONTROL", "End", val_end, false)
+    reaper.SetExtState("SCALE_CONTROL", "Toggle", toggle, false)
+
     local _, _, _, begin_ppqpos, _, _, _, _ = reaper.MIDI_GetCC(take, index[1])
     local _, _, _, end_ppqpos, _, _, _, _ = reaper.MIDI_GetCC(take, index[#index])
     local ppq_offset = (val_end - val_start) / (end_ppqpos - begin_ppqpos)
+
     for i = 1, #index do
       local _, _, _, startppqpos, _, _, _, value = reaper.MIDI_GetCC(take, index[i])
       if toggle == "1" then
@@ -68,19 +140,9 @@ function main()
     end
   end
   reaper.MIDI_Sort(take)
-  reaper.Undo_EndBlock(script_title, 0)
+  reaper.Undo_EndBlock(title, -1)
 end
-function CheckForNewVersion(new_version)
-    local app_version = reaper.GetAppVersion()
-    app_version = tonumber(app_version:match('[%d%.]+'))
-    if new_version > app_version then
-      reaper.MB('Update REAPER to newer version '..'('..new_version..' or newer)', '', 0)
-      return
-     else
-      return true
-    end
-end
-local CFNV = CheckForNewVersion(6.03)
-if CFNV then main() end
+
+main()
 reaper.UpdateArrange()
 reaper.SN_FocusMIDIEditor()
