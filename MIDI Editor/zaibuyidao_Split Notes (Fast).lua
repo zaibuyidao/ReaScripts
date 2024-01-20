1261310ce3c19b37e269d59392d3fbe26ea5a5b0
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2024-01-21 01:06:14 +0800

    Update zaibuyidao_Split Notes (Fast).lua

--- MIDI Editor/zaibuyidao_Split Notes (Fast).lua
+++ MIDI Editor/zaibuyidao_Split Notes (Fast).lua
@@ -1,5 +1,5 @@
 -- @description Split Notes (Fast)
--- @version 1.0.5
+-- @version 1.0.6
 -- @author zaibuyidao
 -- @changelog
 --   + Add Multi-Language Support
@@ -26,37 +26,37 @@ function getSystemLanguage()
   local lang
 
   if os == "Win32" or os == "Win64" then -- Windows
-      if locale == 936 then -- Simplified Chinese
-          lang = "简体中文"
-      elseif locale == 950 then -- Traditional Chinese
-          lang = "繁體中文"
-      else -- English
-          lang = "English"
-      end
+    if locale == 936 then -- Simplified Chinese
+      lang = "简体中文"
+    elseif locale == 950 then -- Traditional Chinese
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
   elseif os == "OSX32" or os == "OSX64" then -- macOS
-      local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
-      local result = handle:read("*a")
-      handle:close()
-      lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
-      if lang == "zh-CN" then -- 简体中文
-          lang = "简体中文"
-      elseif lang == "zh-TW" then -- 繁体中文
-          lang = "繁體中文"
-      else -- English
-          lang = "English"
-      end
+    local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+    if lang == "zh-CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh-TW" then -- 繁体中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
   elseif os == "Linux" then -- Linux
-      local handle = io.popen("echo $LANG")
-      local result = handle:read("*a")
-      handle:close()
-      lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
-      if lang == "zh_CN" then -- 简体中文
-          lang = "简体中文"
-      elseif lang == "zh_TW" then -- 繁體中文
-          lang = "繁體中文"
-      else -- English
-          lang = "English"
-      end
+    local handle = io.popen("echo $LANG")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+    if lang == "zh_CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh_TW" then -- 繁體中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
   end
 
   return lang
@@ -64,33 +64,61 @@ end
 
 local language = getSystemLanguage()
 
+if language == "简体中文" then
+  swsmsg = "该脚本需要 SWS 扩展，你想现在就下载它吗？"
+  swserr = "警告"
+  jsmsg = "请右键单击並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然后重新启动 REAPER 並再次运行脚本，谢谢！\n"
+  jstitle = "你必须安裝 JS_ReaScriptAPI"
+elseif language == "繁体中文" then
+  swsmsg = "該脚本需要 SWS 擴展，你想現在就下載它嗎？"
+  swserr = "警告"
+  jsmsg = "請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n"
+  jstitle = "你必須安裝 JS_ReaScriptAPI"
+else
+  swsmsg = "This script requires the SWS Extension. Do you want to download it now?"
+  swserr = "Warning"
+  jsmsg = "Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\nThen restart REAPER and run the script again, thank you!\n"
+  jstitle = "You must install JS_ReaScriptAPI"
+end
+
 if not reaper.SN_FocusMIDIEditor then
   local retval = reaper.ShowMessageBox(swsmsg, swserr, 1)
   if retval == 1 then
-      if not OS then local OS = reaper.GetOS() end
-      if OS=="OSX32" or OS=="OSX64" then
-          os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
-      else
-          os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
-      end
+    if not OS then local OS = reaper.GetOS() end
+    if OS=="OSX32" or OS=="OSX64" then
+      os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+    else
+      os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+    end
   end
   return
+end
+
+if not reaper.APIExists("JS_Window_Find") then
+  reaper.MB(jsmsg, jstitle, 0)
+  local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+  if ok then
+    reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+  else
+    reaper.MB(err, jserr, 0)
+  end
+  return reaper.defer(function() end)
 end
 
 local function clone(object)
   local lookup_table = {}
   local function _copy(object)
-      if type(object) ~= "table" then
-          return object
-      elseif lookup_table[object] then
-          return lookup_table[object]
-      end
-      local new_table = {}
-      lookup_table[object] = new_table
-      for key, value in pairs(object) do
-          new_table[_copy(key)] = _copy(value)
-      end
-      return setmetatable(new_table, getmetatable(object))
+    if type(object) ~= "table" then
+      return object
+    elseif lookup_table[object] then
+      return lookup_table[object]
+    end
+    local new_table = {}
+    lookup_table[object] = new_table
+    for key, value in pairs(object) do
+        new_table[_copy(key)] = _copy(value)
+    end
+    return setmetatable(new_table, getmetatable(object))
   end
   return _copy(object)
 end
@@ -121,13 +149,13 @@ local EventMeta = {
 function setAllEvents(take, events)
     local lastPos = 0
     for _, event in pairs(events) do
-        event.offset = event.pos - lastPos
-        lastPos = event.pos
+      event.offset = event.pos - lastPos
+      lastPos = event.pos
     end
 
     local tab = {}
     for _, event in pairs(events) do
-        table.insert(tab, string.pack("i4Bs4", event.offset, event.flags, event.msg))
+      table.insert(tab, string.pack("i4Bs4", event.offset, event.flags, event.msg))
     end
     reaper.MIDI_SetAllEvts(take, table.concat(tab))
 end
@@ -138,17 +166,17 @@ function getAllEvents(take, onEach)
     local stringPos = 1
     local lastPos = 0
     while stringPos <= MIDIstring:len() do
-        local offset, flags, msg
-        offset, flags, msg, stringPos = string.unpack("i4Bs4", MIDIstring, stringPos)
-        local event = setmetatable({
-            offset = offset,
-            pos = lastPos + offset,
-            flags = flags,
-            msg = msg
-        }, EventMeta)
-        table.insert(events, event)
-        onEach(event)
-        lastPos = lastPos + offset
+      local offset, flags, msg
+      offset, flags, msg, stringPos = string.unpack("i4Bs4", MIDIstring, stringPos)
+      local event = setmetatable({
+        offset = offset,
+        pos = lastPos + offset,
+        flags = flags,
+        msg = msg
+      }, EventMeta)
+      table.insert(events, event)
+      onEach(event)
+      lastPos = lastPos + offset
     end
     return events
 end
@@ -158,13 +186,13 @@ function getAllTakes()
   if reaper.MIDIEditor_EnumTakes then
     local editor = reaper.MIDIEditor_GetActive()
     for i = 0, math.huge do
-        take = reaper.MIDIEditor_EnumTakes(editor, i, false)
-        if take and reaper.ValidatePtr2(0, take, "MediaItem_Take*") then 
-          tTake[take] = true
-          tTake[take] = {item = reaper.GetMediaItemTake_Item(take)}
-        else
-            break
-        end
+      take = reaper.MIDIEditor_EnumTakes(editor, i, false)
+      if take and reaper.ValidatePtr2(0, take, "MediaItem_Take*") then 
+        tTake[take] = true
+        tTake[take] = {item = reaper.GetMediaItemTake_Item(take)}
+      else
+        break
+      end
     end
   else
     for i = 0, reaper.CountMediaItems(0)-1 do
@@ -192,38 +220,38 @@ function main(div, take)
     local articulationEventAtPitch = {}
 
     local events = getAllEvents(take, function(event)
-        if event.type == EVENT_NOTE_START then
-            noteLastEventAtPitch[event.pitch] = event
-        elseif event.type == EVENT_NOTE_END then
-            local showmsg = ""
-            if language == "简体中文" then
-                showmsg = "音符有重叠无法解析"
-            elseif language == "繁体中文" then
-                showmsg = "音符有重叠無法解析"
-            else
-                showmsg = "Notes are overlapping and cannot be resolved."
-            end
-            local head = noteLastEventAtPitch[event.pitch]
-            if head == nil then error(showmsg) end
-            local tail = event
-            if event.selected and div <= tail.pos - head.pos then
-                table.insert(notes, {
-                    head = head,
-                    tail = tail,
-                    articulation = articulationEventAtPitch[event.pitch],
-                    pitch = event.pitch
-                })
-            end
-            noteLastEventAtPitch[event.pitch] = nil
-            articulationEventAtPitch[event.pitch] = nil
-        elseif event.type == EVENT_ARTICULATION then
-            if event.msg:byte(1) == 0xFF and not (event.msg:byte(2) == 0x0F) then
-                -- text event
-            elseif event.msg:find("articulation") then
-                local chan, pitch = event.msg:match("NOTE (%d+) (%d+) ")
-                articulationEventAtPitch[tonumber(pitch)] = event
-            end
+      if event.type == EVENT_NOTE_START then
+        noteLastEventAtPitch[event.pitch] = event
+      elseif event.type == EVENT_NOTE_END then
+        local showmsg = ""
+        if language == "简体中文" then
+          showmsg = "音符有重叠无法解析"
+        elseif language == "繁体中文" then
+          showmsg = "音符有重叠無法解析"
+        else
+          showmsg = "Notes are overlapping and cannot be resolved."
         end
+        local head = noteLastEventAtPitch[event.pitch]
+        if head == nil then error(showmsg) end
+        local tail = event
+        if event.selected and div <= tail.pos - head.pos then
+            table.insert(notes, {
+              head = head,
+              tail = tail,
+              articulation = articulationEventAtPitch[event.pitch],
+              pitch = event.pitch
+            })
+        end
+        noteLastEventAtPitch[event.pitch] = nil
+        articulationEventAtPitch[event.pitch] = nil
+      elseif event.type == EVENT_ARTICULATION then
+        if event.msg:byte(1) == 0xFF and not (event.msg:byte(2) == 0x0F) then
+          -- text event
+        elseif event.msg:find("articulation") then
+          local chan, pitch = event.msg:match("NOTE (%d+) (%d+) ")
+          articulationEventAtPitch[tonumber(pitch)] = event
+        end
+      end
     end)
 
     local skipEvents = {}
