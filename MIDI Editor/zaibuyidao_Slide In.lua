80f91e3c6fcb7f4c135b7739d0594616a5b194ea
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2024-01-21 00:33:53 +0800

    Update

--- MIDI Editor/zaibuyidao_Slide In.lua
+++ MIDI Editor/zaibuyidao_Slide In.lua
@@ -1,69 +1,104 @@
 -- @description Slide In
--- @version 1.4.3
+-- @version 1.4.4
 -- @author zaibuyidao
--- @changelog Optimised pitch bend
+-- @changelog
+--   + Add Multi-Language Support
 -- @links
 --   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
 --   repo https://github.com/zaibuyidao/ReaScripts
 -- @donate http://www.paypal.me/zaibuyidao
--- @about Requires SWS Extensions
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
 function print(...)
-  local params = {...}
-  for i = 1, #params do
-    if i ~= 1 then reaper.ShowConsoleMsg(" ") end
-    reaper.ShowConsoleMsg(tostring(params[i]))
+  for _, v in ipairs({...}) do
+    reaper.ShowConsoleMsg(tostring(v) .. " ")
   end
   reaper.ShowConsoleMsg("\n")
 end
 
-function table.print(t)
-  local print_r_cache = {}
-  local function sub_print_r(t, indent)
-    if (print_r_cache[tostring(t)]) then
-      print(indent .. "*" .. tostring(t))
-    else
-      print_r_cache[tostring(t)] = true
-      if (type(t) == "table") then
-        for pos, val in pairs(t) do
-          if (type(val) == "table") then
-            print(indent .. "[" .. tostring(pos) .. "] => " .. tostring(t) .. " {")
-            sub_print_r(val, indent .. string.rep(" ", string.len(tostring(pos)) + 8))
-            print(indent .. string.rep(" ", string.len(tostring(pos)) + 6) .. "}")
-          elseif (type(val) == "string") then
-            print(indent .. "[" .. tostring(pos) .. '] => "' .. val .. '"')
-          else
-            print(indent .. "[" .. tostring(pos) .. "] => " .. tostring(val))
-          end
-        end
-      else
-        print(indent .. tostring(t))
-      end
+function getSystemLanguage()
+  local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+  local os = reaper.GetOS()
+  local lang
+
+  if os == "Win32" or os == "Win64" then -- Windows
+    if locale == 936 then -- Simplified Chinese
+      lang = "简体中文"
+    elseif locale == 950 then -- Traditional Chinese
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "OSX32" or os == "OSX64" then -- macOS
+    local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+    if lang == "zh-CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh-TW" then -- 繁体中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "Linux" then -- Linux
+    local handle = io.popen("echo $LANG")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+    if lang == "zh_CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh_TW" then -- 繁體中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
     end
   end
-  if (type(t) == "table") then
-    print(tostring(t) .. " {")
-    sub_print_r(t, "  ")
-    print("}")
-  else
-    sub_print_r(t, "  ")
-  end
+
+  return lang
 end
 
-function open_url(url)
-  if not OS then local OS = reaper.GetOS() end
-  if OS=="OSX32" or OS=="OSX64" then
-    os.execute("open ".. url)
-  else
-    os.execute("start ".. url)
-  end
+local language = getSystemLanguage()
+
+if language == "简体中文" then
+  swsmsg = "该脚本需要 SWS 扩展，你想现在就下载它吗？"
+  swserr = "警告"
+  jsmsg = "请右键单击並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然后重新启动 REAPER 並再次运行脚本，谢谢！\n"
+  jstitle = "你必须安裝 JS_ReaScriptAPI"
+elseif language == "繁体中文" then
+  swsmsg = "該脚本需要 SWS 擴展，你想現在就下載它嗎？"
+  swserr = "警告"
+  jsmsg = "請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n"
+  jstitle = "你必須安裝 JS_ReaScriptAPI"
+else
+  swsmsg = "This script requires the SWS Extension. Do you want to download it now?"
+  swserr = "Warning"
+  jsmsg = "Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\nThen restart REAPER and run the script again, thank you!\n"
+  jstitle = "You must install JS_ReaScriptAPI"
 end
 
 if not reaper.SN_FocusMIDIEditor then
-  local retval = reaper.ShowMessageBox("這個脚本需要SWS擴展，你想現在就下載它嗎？", "Warning", 1)
+  local retval = reaper.ShowMessageBox(swsmsg, swserr, 1)
   if retval == 1 then
-    open_url("http://www.sws-extension.org/download/pre-release/")
+    if not OS then local OS = reaper.GetOS() end
+    if OS=="OSX32" or OS=="OSX64" then
+      os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+    else
+      os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+    end
   end
+  return
+end
+
+if not reaper.APIExists("JS_Window_Find") then
+  reaper.MB(jsmsg, jstitle, 0)
+  local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+  if ok then
+    reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+  else
+    reaper.MB(err, jserr, 0)
+  end
+  return reaper.defer(function() end)
 end
 
 local editor = reaper.MIDIEditor_GetActive()
@@ -73,16 +108,27 @@ local loop_start = math.floor(0.5 + reaper.MIDI_GetPPQPosFromProjTime(take, time
 local loop_end = math.floor(0.5 + reaper.MIDI_GetPPQPosFromProjTime(take, time_end))
 if loop_start == loop_end then return reaper.SN_FocusMIDIEditor() end
 
-local pitch = reaper.GetExtState("SlideIn", "Pitch")
+if language == "简体中文" then
+  title = "弯音滑入"
+  captions_csv = "弯音间隔,弯音范围,贝塞尔 (-100,100),0=SMO 1=LIN 2=FRE 3=REV"
+elseif language == "繁体中文" then
+  title = "彎音滑入"
+  captions_csv = "彎音間隔,彎音範圍,貝塞爾 (-100,100),0=SMO 1=LIN 2=FRE 3=REV"
+else
+  title = "Slide In"
+  captions_csv = "Pitch interval,Pitch Range,Bezier (-100,100),0=SMO 1=LIN 2=FRE 3=REV"
+end
+
+local pitch = reaper.GetExtState("SLIDE_IN", "Pitch")
 if (pitch == "") then pitch = "-7" end
-local range = reaper.GetExtState("SlideIn", "Range")
+local range = reaper.GetExtState("SLIDE_IN", "Range")
 if (range == "") then range = "12" end
-local bezier = reaper.GetExtState("SlideIn", "Bezier")
+local bezier = reaper.GetExtState("SLIDE_IN", "Bezier")
 if (bezier == "") then bezier = "20" end
-local toggle = reaper.GetExtState("SlideIn", "Toggle")
+local toggle = reaper.GetExtState("SLIDE_IN", "Toggle")
 if (toggle == "") then toggle = "2" end
 
-uok, uinput = reaper.GetUserInputs("Slide In", 4, "Pitch interval 彎音間隔,Pitch Range 彎音範圍,Bezier 貝塞爾 (-100,100),0=SMO 1=LIN 2=FRE 3=REV", pitch ..','.. range ..','.. bezier ..','.. toggle)
+uok, uinput = reaper.GetUserInputs(title, 4, captions_csv, pitch ..','.. range ..','.. bezier ..','.. toggle)
 if not uok then return reaper.SN_FocusMIDIEditor() end
 pitch, range, bezier, toggle = uinput:match("(.*),(.*),(.*),(.*)")
 
@@ -92,10 +138,10 @@ end
 
 pitch, range, bezier, toggle = tonumber(pitch), tonumber(range),tonumber(bezier), tonumber(toggle)
 
-reaper.SetExtState("SlideIn", "Pitch", pitch, false)
-reaper.SetExtState("SlideIn", "Range", range, false)
-reaper.SetExtState("SlideIn", "Bezier", bezier, false)
-reaper.SetExtState("SlideIn", "Toggle", toggle, false)
+reaper.SetExtState("SLIDE_IN", "Pitch", pitch, false)
+reaper.SetExtState("SLIDE_IN", "Range", range, false)
+reaper.SetExtState("SLIDE_IN", "Bezier", bezier, false)
+reaper.SetExtState("SLIDE_IN", "Toggle", toggle, false)
 
 function getSegments(n)
   local x = 8192
@@ -195,6 +241,6 @@ elseif toggle == 3 then
     reaper.MIDI_InsertCC(take, false, false, loop_end + (loop_end-loop_start) * ((i+i)/-(math.abs(pitch)+i)), 224, 0, LSB, MSB)
   end
 end
-reaper.Undo_EndBlock("Slide In", -1)
+reaper.Undo_EndBlock(title, -1)
 reaper.UpdateArrange()
 reaper.SN_FocusMIDIEditor()
