d1b57c2c0adb07cf95e748aa56d921400f09806c
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2024-01-21 23:34:46 +0800

    New Script: Add VSTi Track And Set Receives

---
+++ Utility/zaibuyidao_Add VSTi Track And Set Receives (Settings).lua
@@ -0,0 +1,135 @@
+-- @description Add VSTi Track And Set Receives (Settings)
+-- @version 1.0
+-- @author zaibuyidao
+-- @changelog
+--   + New Script
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
+
+function print(...)
+    for _, v in ipairs({...}) do
+        reaper.ShowConsoleMsg(tostring(v) .. " ")
+    end
+    reaper.ShowConsoleMsg("\n")
+end
+
+function getSystemLanguage()
+    local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+    local os = reaper.GetOS()
+    local lang
+  
+    if os == "Win32" or os == "Win64" then -- Windows
+        if locale == 936 then -- Simplified Chinese
+            lang = "简体中文"
+        elseif locale == 950 then -- Traditional Chinese
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
+    elseif os == "OSX32" or os == "OSX64" then -- macOS
+        local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+        local result = handle:read("*a")
+        handle:close()
+        lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+        if lang == "zh-CN" then -- 简体中文
+            lang = "简体中文"
+        elseif lang == "zh-TW" then -- 繁体中文
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
+    elseif os == "Linux" then -- Linux
+        local handle = io.popen("echo $LANG")
+        local result = handle:read("*a")
+        handle:close()
+        lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+        if lang == "zh_CN" then -- 简体中文
+            lang = "简体中文"
+        elseif lang == "zh_TW" then -- 繁體中文
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
+    end
+
+    return lang
+end
+
+local language = getSystemLanguage()
+
+if language == "简体中文" then
+    swsmsg = "该脚本需要 SWS 扩展，你想现在就下载它吗？"
+    swserr = "警告"
+    jsmsg = "请右键单击並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然后重新启动 REAPER 並再次运行脚本，谢谢！\n"
+    jstitle = "你必须安裝 JS_ReaScriptAPI"
+elseif language == "繁體中文" then
+    swsmsg = "該脚本需要 SWS 擴展，你想現在就下載它嗎？"
+    swserr = "警告"
+    jsmsg = "請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n"
+    jstitle = "你必須安裝 JS_ReaScriptAPI"
+else
+    swsmsg = "This script requires the SWS Extension. Do you want to download it now?"
+    swserr = "Warning"
+    jsmsg = "Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\nThen restart REAPER and run the script again, thank you!\n"
+    jstitle = "You must install JS_ReaScriptAPI"
+end
+
+if not reaper.SNM_GetIntConfigVar then
+    local retval = reaper.ShowMessageBox(swsmsg, swserr, 1)
+    if retval == 1 then
+        if not OS then local OS = reaper.GetOS() end
+        if OS=="OSX32" or OS=="OSX64" then
+            os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+        else
+            os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+        end
+    end
+    return
+end
+
+if not reaper.APIExists("JS_Window_Find") then
+    reaper.MB(jsmsg, jstitle, 0)
+    local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+    if ok then
+        reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+    else
+        reaper.MB(err, jserr, 0)
+    end
+    return reaper.defer(function() end)
+end
+
+reaper.Undo_BeginBlock() -- 撤销块开始
+
+local section = "AddVSTiTrackAndSetReceives"
+local key = "VSTiName"
+
+if language == "简体中文" then
+    title = "添加 VSTi 轨道并设置发送"
+    ip_title = "VSTi 设置"
+    ip_caption = "输入 VSTi 名称:,extrawidth=150"
+elseif language == "繁體中文" then
+    title = "添加 VSTi 軌道並設置發送"
+    ip_title = "VSTi 設置"
+    ip_caption = "輸入 VSTi 名稱:extrawidth=150"
+else
+    title = "Add VSTi Track And Set Receives (Settings)"
+    ip_title = "VSTi Settings"
+    ip_caption = "Enter VSTi name:extrawidth=150"
+end
+
+-- 尝试获取已保存的 VSTi 名称
+local savedVSTiName = reaper.GetExtState(section, key)
+if not savedVSTiName then savedVSTiName = "SOUND Canvas VA" end
+
+-- 获取用户输入的 VSTi 名称，显示已保存的名称作为默认值
+local retval, vstiName = reaper.GetUserInputs(ip_title, 1, ip_caption, savedVSTiName)
+
+if retval then
+    -- 保存用户输入的 VSTi 名称为扩展状态
+    reaper.SetExtState(section, key, vstiName, true)
+end
+
+reaper.Undo_EndBlock(title, -1) -- 撤销块结束
