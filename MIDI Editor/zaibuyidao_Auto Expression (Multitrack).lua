716ec9b8238840e0f81722f9dc2ab43ee4a8d571
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2024-01-21 17:49:35 +0800

    Update

--- MIDI Editor/zaibuyidao_Auto Expression (Multitrack).lua
+++ MIDI Editor/zaibuyidao_Auto Expression (Multitrack).lua
@@ -1,28 +1,104 @@
---[[
- * ReaScript Name: Auto Expression (Multitrack)
- * Version: 1.3.2
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository: GitHub > zaibuyidao > ReaScripts
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * REAPER: 6.0
---]]
+-- @description Auto Expression (Multitrack)
+-- @version 1.3.3
+-- @author zaibuyidao
+-- @changelog
+--   + Add Multi-Language Support
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
---[[
- * Changelog:
- * v1.0 (2020-8-27)
-  + Initial release
---]]
+function print(...)
+  for _, v in ipairs({...}) do
+    reaper.ShowConsoleMsg(tostring(v) .. " ")
+  end
+  reaper.ShowConsoleMsg("\n")
+end
 
--- USER AREA
--- Settings that the user can customize.
+function getSystemLanguage()
+  local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+  local os = reaper.GetOS()
+  local lang
 
-cc_number = 11
+  if os == "Win32" or os == "Win64" then -- Windows
+    if locale == 936 then -- Simplified Chinese
+      lang = "简体中文"
+    elseif locale == 950 then -- Traditional Chinese
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "OSX32" or os == "OSX64" then -- macOS
+    local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+    if lang == "zh-CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh-TW" then -- 繁体中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "Linux" then -- Linux
+    local handle = io.popen("echo $LANG")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+    if lang == "zh_CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh_TW" then -- 繁體中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  end
 
--- End of USER AREA
+  return lang
+end
 
-function Msg(param)
-  reaper.ShowConsoleMsg(tostring(param) .. "\n")
+local language = getSystemLanguage()
+
+if language == "简体中文" then
+  swsmsg = "该脚本需要 SWS 扩展，你想现在就下载它吗？"
+  swserr = "警告"
+  jsmsg = "请右键单击並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然后重新启动 REAPER 並再次运行脚本，谢谢！\n"
+  jstitle = "你必须安裝 JS_ReaScriptAPI"
+elseif language == "繁体中文" then
+  swsmsg = "該脚本需要 SWS 擴展，你想現在就下載它嗎？"
+  swserr = "警告"
+  jsmsg = "請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n"
+  jstitle = "你必須安裝 JS_ReaScriptAPI"
+else
+  swsmsg = "This script requires the SWS Extension. Do you want to download it now?"
+  swserr = "Warning"
+  jsmsg = "Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\nThen restart REAPER and run the script again, thank you!\n"
+  jstitle = "You must install JS_ReaScriptAPI"
+end
+
+if not reaper.SN_FocusMIDIEditor then
+  local retval = reaper.ShowMessageBox(swsmsg, swserr, 1)
+  if retval == 1 then
+    if not OS then local OS = reaper.GetOS() end
+    if OS=="OSX32" or OS=="OSX64" then
+      os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+    else
+      os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+    end
+  end
+  return
+end
+
+if not reaper.APIExists("JS_Window_Find") then
+  reaper.MB(jsmsg, jstitle, 0)
+  local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+  if ok then
+    reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+  else
+    reaper.MB(err, jserr, 0)
+  end
+  return reaper.defer(function() end)
 end
 
 function autoExp()
@@ -71,23 +147,34 @@ function autoExp()
   end
 end
 
-min_val = reaper.GetExtState("AutoExpression", "Begin")
-max_val = reaper.GetExtState("AutoExpression", "End")
+if language == "简体中文" then
+  title = "自动表情"
+  captions_csv = "CC编号:,最小值:,最大值:"
+elseif language == "繁体中文" then
+  title = "自動表情"
+  captions_csv = "CC編號:,最小值:,最大值:"
+else
+  title = "Auto expression"
+  captions_csv = "CC Number:,Min value:,Max value:"
+end
 
+cc_number = reaper.GetExtState("AUTO_EXPRESSION_MULTI", "CC_Number")
+min_val = reaper.GetExtState("AUTO_EXPRESSION_MULTI", "Begin")
+max_val = reaper.GetExtState("AUTO_EXPRESSION_MULTI", "End")
+if (cc_number == "") then cc_number = "11" end
 if (min_val == "") then min_val = "90" end
 if (max_val == "") then max_val = "127" end
 
-local user_ok, user_input_CSV = reaper.GetUserInputs("Auto expression (multitrack)", 2, "Min value,Max value", min_val ..','.. max_val)
-min_val, max_val = user_input_CSV:match("(.*),(.*)")
-
-if not user_ok or not tonumber(min_val) or not tonumber(max_val) then return reaper.SN_FocusMIDIEditor() end
-min_val, max_val = tonumber(min_val), tonumber(max_val)
-
-if min_val > 127 or min_val < 0 or max_val > 127 or max_val < 0  then return reaper.SN_FocusMIDIEditor() end
+local uok, uinput = reaper.GetUserInputs(title, 3, captions_csv, cc_number ..','.. min_val ..','.. max_val)
+cc_number, min_val, max_val = uinput:match("(.*),(.*),(.*)")
+if not uok or not tonumber(cc_number) or not tonumber(min_val) or not tonumber(max_val) then return reaper.SN_FocusMIDIEditor() end
+cc_number, min_val, max_val = tonumber(cc_number), tonumber(min_val), tonumber(max_val)
+if min_val > 127 or min_val < 0 or max_val > 127 or max_val < 0 or cc_number > 127 or cc_number < 0  then return reaper.SN_FocusMIDIEditor() end
 if min_val >= max_val then return reaper.SN_FocusMIDIEditor() end
 
-reaper.SetExtState("AutoExpression", "Begin", min_val, false)
-reaper.SetExtState("AutoExpression", "End", max_val, false)
+reaper.SetExtState("AUTO_EXPRESSION_MULTI", "CC_Number", cc_number, false)
+reaper.SetExtState("AUTO_EXPRESSION_MULTI", "Begin", min_val, false)
+reaper.SetExtState("AUTO_EXPRESSION_MULTI", "End", max_val, false)
 
 tick = reaper.SNM_GetIntConfigVar("MidiTicksPerBeat", 480)
 diff = max_val - min_val
@@ -124,7 +211,7 @@ else
 end
 
 -- reaper.MIDIEditor_LastFocused_OnCommand(reaper.NamedCommandLookup("_RS7d3c_38c941e712837e405c3c662e2a39e3d03ffd5364"), 0) -- 移除冗余CCs
-reaper.Undo_EndBlock("Auto Expression", -1) -- 撤销块结束
+reaper.Undo_EndBlock(title, -1) -- 撤销块结束
 reaper.PreventUIRefresh(-1) -- 恢复UI刷新
 reaper.UpdateArrange() -- 更新排列
 reaper.SN_FocusMIDIEditor() -- 聚焦MIDI编辑器
--- MIDI Editor/zaibuyidao_Auto Expression.lua
+++ MIDI Editor/zaibuyidao_Auto Expression.lua
@@ -1,28 +1,104 @@
---[[
- * ReaScript Name: Auto Expression
- * Version: 1.3.2
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository: GitHub > zaibuyidao > ReaScripts
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * REAPER: 6.0
---]]
+-- @description Auto Expression
+-- @version 1.3.3
+-- @author zaibuyidao
+-- @changelog
+--   + Add Multi-Language Support
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
---[[
- * Changelog:
- * v1.0 (2020-8-27)
-  + Initial release
---]]
+function print(...)
+  for _, v in ipairs({...}) do
+    reaper.ShowConsoleMsg(tostring(v) .. " ")
+  end
+  reaper.ShowConsoleMsg("\n")
+end
 
--- USER AREA
--- Settings that the user can customize.
+function getSystemLanguage()
+  local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+  local os = reaper.GetOS()
+  local lang
 
-cc_number = 11
+  if os == "Win32" or os == "Win64" then -- Windows
+    if locale == 936 then -- Simplified Chinese
+      lang = "简体中文"
+    elseif locale == 950 then -- Traditional Chinese
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "OSX32" or os == "OSX64" then -- macOS
+    local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+    if lang == "zh-CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh-TW" then -- 繁体中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "Linux" then -- Linux
+    local handle = io.popen("echo $LANG")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+    if lang == "zh_CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh_TW" then -- 繁體中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  end
 
--- End of USER AREA
+  return lang
+end
 
-function Msg(param)
-  reaper.ShowConsoleMsg(tostring(param) .. "\n")
+local language = getSystemLanguage()
+
+if language == "简体中文" then
+  swsmsg = "该脚本需要 SWS 扩展，你想现在就下载它吗？"
+  swserr = "警告"
+  jsmsg = "请右键单击並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然后重新启动 REAPER 並再次运行脚本，谢谢！\n"
+  jstitle = "你必须安裝 JS_ReaScriptAPI"
+elseif language == "繁体中文" then
+  swsmsg = "該脚本需要 SWS 擴展，你想現在就下載它嗎？"
+  swserr = "警告"
+  jsmsg = "請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n"
+  jstitle = "你必須安裝 JS_ReaScriptAPI"
+else
+  swsmsg = "This script requires the SWS Extension. Do you want to download it now?"
+  swserr = "Warning"
+  jsmsg = "Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\nThen restart REAPER and run the script again, thank you!\n"
+  jstitle = "You must install JS_ReaScriptAPI"
+end
+
+if not reaper.SN_FocusMIDIEditor then
+  local retval = reaper.ShowMessageBox(swsmsg, swserr, 1)
+  if retval == 1 then
+    if not OS then local OS = reaper.GetOS() end
+    if OS=="OSX32" or OS=="OSX64" then
+      os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+    else
+      os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+    end
+  end
+  return
+end
+
+if not reaper.APIExists("JS_Window_Find") then
+  reaper.MB(jsmsg, jstitle, 0)
+  local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+  if ok then
+    reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+  else
+    reaper.MB(err, jserr, 0)
+  end
+  return reaper.defer(function() end)
 end
 
 local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
@@ -38,20 +114,34 @@ while val ~= - 1 do
 end
 
 if #index > 0 then
-  local min_val = reaper.GetExtState("AutoExpression", "Begin")
+  if language == "简体中文" then
+    title = "自动表情"
+    captions_csv = "CC编号:,最小值:,最大值:"
+  elseif language == "繁体中文" then
+    title = "自動表情"
+    captions_csv = "CC編號:,最小值:,最大值:"
+  else
+    title = "Auto expression"
+    captions_csv = "CC Number:,Min value:,Max value:"
+  end
+  
+  local cc_number = reaper.GetExtState("AUTO_EXPRESSION", "CC_Number")
+  local min_val = reaper.GetExtState("AUTO_EXPRESSION", "Begin")
+  local max_val = reaper.GetExtState("AUTO_EXPRESSION", "End")
+  if (cc_number == "") then cc_number = "11" end
   if (min_val == "") then min_val = "90" end
-  local max_val = reaper.GetExtState("AutoExpression", "End")
   if (max_val == "") then max_val = "127" end
 
-  local user_ok, user_input_CSV = reaper.GetUserInputs("Auto Expression", 2, "Min Value,Max Value", min_val ..','.. max_val)
-  min_val, max_val = user_input_CSV:match("(.*),(.*)")
-  if not user_ok or not tonumber(min_val) or not tonumber(max_val) then return reaper.SN_FocusMIDIEditor() end
-  min_val, max_val = tonumber(min_val), tonumber(max_val)
-  if min_val > 127 or min_val < 0 or max_val > 127 or max_val < 0  then return reaper.SN_FocusMIDIEditor() end
+  local uok, uinput = reaper.GetUserInputs(title, 3, captions_csv, cc_number ..','.. min_val ..','.. max_val)
+  cc_number, min_val, max_val = uinput:match("(.*),(.*),(.*)")
+  if not uok or not tonumber(cc_number) or not tonumber(min_val) or not tonumber(max_val) then return reaper.SN_FocusMIDIEditor() end
+  cc_number, min_val, max_val = tonumber(cc_number), tonumber(min_val), tonumber(max_val)
+  if min_val > 127 or min_val < 0 or max_val > 127 or max_val < 0 or cc_number > 127 or cc_number < 0  then return reaper.SN_FocusMIDIEditor() end
   if min_val >= max_val then return reaper.SN_FocusMIDIEditor() end
 
-  reaper.SetExtState("AutoExpression", "Begin", min_val, false)
-  reaper.SetExtState("AutoExpression", "End", max_val, false)
+  reaper.SetExtState("AUTO_EXPRESSION", "CC_Number", cc_number, false)
+  reaper.SetExtState("AUTO_EXPRESSION", "Begin", min_val, false)
+  reaper.SetExtState("AUTO_EXPRESSION", "End", max_val, false)
 
   local diff = max_val - min_val
   local startppqpos = {} -- 音符开头位置
@@ -110,7 +200,7 @@ if #index > 0 then
     end
   end
   reaper.MIDI_Sort(take)
-  reaper.Undo_EndBlock("Auto Expression", 0) -- 撤销块结束
+  reaper.Undo_EndBlock(title, -1) -- 撤销块结束
   reaper.PreventUIRefresh(-1) -- 恢复UI刷新
 end
 
