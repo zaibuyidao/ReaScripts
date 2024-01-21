1674d118eb9365dbe16129cd46355b081f64f8b4
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2024-01-21 17:28:59 +0800

    Update

--- MIDI Editor/zaibuyidao_Transform CC (For Selected CC).lua
+++ MIDI Editor/zaibuyidao_Transform CC (For Selected CC).lua
@@ -1,48 +1,149 @@
---[[
- * ReaScript Name: Transform CC (For Selected CC)
- * Version: 1.1
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository: GitHub > zaibuyidao > ReaScripts
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * REAPER: 6.0
- * Donation: http://www.paypal.me/zaibuyidao
---]]
+-- @description Transform CC (For Selected CC)
+-- @version 1.1.1
+-- @author zaibuyidao
+-- @changelog
+--   + Add Multi-Language Support
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
---[[
- * Changelog:
- * v1.0 (2020-6-24)
-  + Initial release
---]]
-
-function Main()
-  local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
-  if take == nil then return end
-  local _, notecnt, ccevtcnt, textsyxevtcnt = reaper.MIDI_CountEvts(take)
-  local cc_new = reaper.GetExtState("TransformCC", "CC_New")
-  if (cc_new == "") then cc_new = "11" end
-  local user_ok, user_input_CSV = reaper.GetUserInputs('Transform CC', 1, 'CC number', cc_new)
-  cc_new = user_input_CSV:match("(.*)")
-  if not user_ok or not tonumber(cc_new) then return reaper.SN_FocusMIDIEditor() end
-  cc_new = tonumber(cc_new)
-  if cc_new > 127 or cc_new < 0 then
-    return
-    reaper.MB("Please enter a value from 0 through 127", "Error", 0),
-    reaper.SN_FocusMIDIEditor()
+function print(...)
+  for _, v in ipairs({...}) do
+    reaper.ShowConsoleMsg(tostring(v) .. " ")
   end
-  reaper.SetExtState("TransformCC", "CC_New", cc_new, false)
-  reaper.MIDI_DisableSort(take)
-  for i = 1, ccevtcnt do
-    local _, selected, muted, ppqpos, chanmsg, chan, msg2, msg3 = reaper.MIDI_GetCC(take, i - 1)
-    if selected == true then
-      reaper.MIDI_SetCC(take, i - 1, nil, nil, nil, nil, nil, cc_new, nil, false)
+  reaper.ShowConsoleMsg("\n")
+end
+
+function getSystemLanguage()
+  local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+  local os = reaper.GetOS()
+  local lang
+
+  if os == "Win32" or os == "Win64" then -- Windows
+    if locale == 936 then -- Simplified Chinese
+      lang = "简体中文"
+    elseif locale == 950 then -- Traditional Chinese
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "OSX32" or os == "OSX64" then -- macOS
+    local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+    if lang == "zh-CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh-TW" then -- 繁体中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
+    end
+  elseif os == "Linux" then -- Linux
+    local handle = io.popen("echo $LANG")
+    local result = handle:read("*a")
+    handle:close()
+    lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+    if lang == "zh_CN" then -- 简体中文
+      lang = "简体中文"
+    elseif lang == "zh_TW" then -- 繁體中文
+      lang = "繁體中文"
+    else -- English
+      lang = "English"
     end
   end
-  reaper.UpdateArrange()
-  reaper.MIDI_Sort(take)
+
+  return lang
 end
-local script_title = "Transform CC (For Selected CC)"
+
+local language = getSystemLanguage()
+
+if language == "简体中文" then
+  swsmsg = "该脚本需要 SWS 扩展，你想现在就下载它吗？"
+  swserr = "警告"
+  jsmsg = "请右键单击並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然后重新启动 REAPER 並再次运行脚本，谢谢！\n"
+  jstitle = "你必须安裝 JS_ReaScriptAPI"
+elseif language == "繁体中文" then
+  swsmsg = "該脚本需要 SWS 擴展，你想現在就下載它嗎？"
+  swserr = "警告"
+  jsmsg = "請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n"
+  jstitle = "你必須安裝 JS_ReaScriptAPI"
+else
+  swsmsg = "This script requires the SWS Extension. Do you want to download it now?"
+  swserr = "Warning"
+  jsmsg = "Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\nThen restart REAPER and run the script again, thank you!\n"
+  jstitle = "You must install JS_ReaScriptAPI"
+end
+
+if not reaper.SN_FocusMIDIEditor then
+  local retval = reaper.ShowMessageBox(swsmsg, swserr, 1)
+  if retval == 1 then
+    if not OS then local OS = reaper.GetOS() end
+    if OS=="OSX32" or OS=="OSX64" then
+      os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+    else
+      os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+    end
+  end
+  return
+end
+
+if not reaper.APIExists("JS_Window_Find") then
+  reaper.MB(jsmsg, jstitle, 0)
+  local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+  if ok then
+    reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+  else
+    reaper.MB(err, jserr, 0)
+  end
+  return reaper.defer(function() end)
+end
+
+local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
+if take == nil then return end
+local _, notecnt, ccevtcnt, textsyxevtcnt = reaper.MIDI_CountEvts(take)
+
+if language == "简体中文" then
+  title = "转换CC"
+  captions_csv = "CC编号:"
+  msgval = "请输入一个介于 0 到 127 之间的值"
+  msgerr = "错误"
+elseif language == "繁体中文" then
+  title = "轉換CC"
+  captions_csv = "CC編號"
+  msgval = "請輸入一個介於 0 到 127 之間的值"
+  msgerr = "錯誤"
+else
+  title = "Transform CC"
+  captions_csv = "CC number:"
+  msgval = "Please enter a value from 0 through 127."
+  msgerr = "Error"
+end
+
+local cc_new = reaper.GetExtState("TRANSFORM_CC", "CC_New")
+if (cc_new == "") then cc_new = "11" end
+local uok, uinput = reaper.GetUserInputs(title, 1, captions_csv, cc_new)
+cc_new = uinput:match("(.*)")
+if not uok or not tonumber(cc_new) then return reaper.SN_FocusMIDIEditor() end
+cc_new = tonumber(cc_new)
+if cc_new > 127 or cc_new < 0 then
+  return
+  reaper.MB(msgval, msgerr, 0),
+  reaper.SN_FocusMIDIEditor()
+end
+reaper.SetExtState("TRANSFORM_CC", "CC_New", cc_new, false)
+
 reaper.Undo_BeginBlock()
-Main()
-reaper.Undo_EndBlock(script_title, 0)
+reaper.MIDI_DisableSort(take)
+for i = 1, ccevtcnt do
+  local _, selected, muted, ppqpos, chanmsg, chan, msg2, msg3 = reaper.MIDI_GetCC(take, i - 1)
+  if selected == true then
+    reaper.MIDI_SetCC(take, i - 1, nil, nil, nil, nil, nil, cc_new, nil, false)
+  end
+end
+reaper.MIDI_Sort(take)
+reaper.Undo_EndBlock(title, -1)
+reaper.UpdateArrange()
 reaper.SN_FocusMIDIEditor()
