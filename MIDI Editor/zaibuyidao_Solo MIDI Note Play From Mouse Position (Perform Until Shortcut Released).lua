8f5d0d36d2ae6eb0302a797c43040c420c3ae91e
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2024-02-17 19:18:22 +0800

    Update

--- MIDI Editor/zaibuyidao_Solo MIDI Note Play From Mouse Position (Perform Until Shortcut Released).lua
+++ MIDI Editor/zaibuyidao_Solo MIDI Note Play From Mouse Position (Perform Until Shortcut Released).lua
@@ -1,5 +1,5 @@
 -- @description Solo MIDI Note Play From Mouse Position (Perform Until Shortcut Released)
--- @version 1.1.0
+-- @version 1.1.1
 -- @author zaibuyidao
 -- @changelog
 --   # Optimized playback speed when soloing MIDI notes.
@@ -137,51 +137,6 @@ local function generateKeyMap()
   return map
 end
 
-key_map = generateKeyMap()
-
-local key = reaper.GetExtState("SOLO_MIDI_NOTE_SHORTCUT_SETTING", "VirtualKey")
-VirtualKeyCode = key_map[key]
-shift_key = 0x10
-ctrl_key = 0x11
-alt_key = 0x12
-
-if (not key or not key_map[key]) then
-  key = "9"
-  local retval, retvals_csv = reaper.GetUserInputs(title, 1, lable, key)
-
-  if retval == nil or retval == false then 
-    return
-  end
-
-  -- If the user entered ";;", interpret it as ","
-  if retvals_csv == ";;" then
-    retvals_csv = ","
-  end
-
-  if (not key_map[retvals_csv]) then
-    reaper.MB(err_title, "Error", 0)
-    return
-  end
-
-  key = retvals_csv
-  VirtualKeyCode = key_map[key]
-  reaper.SetExtState("SOLO_MIDI_NOTE_SHORTCUT_SETTING", "VirtualKey", key, true)
-
-  if language == "简体中文" then
-    okk_title = "虚拟键 ".. key .." 设置完毕。接下来，你需要将按键 ".. key .." 设置为无动作，以避免触发系统警报声。\n点击【确定】将会弹出操作列表的快捷键设置，请将快捷键设置为按键 ".. key .." 。\n\n最后，请重新运行 Solo MIDI Note 脚本，並使用快捷键 ".. key .." 进行独奏。"
-    okk_box = "继续下一步"
-  elseif language == "繁體中文" then
-    okk_title = "虛擬鍵 ".. key .." 設置完畢。接下來，你需要將按鍵 ".. key .." 設置為無動作，以避免觸發系統警報聲。\n點擊【確定】將會彈出操作列表的快捷鍵設置，請將快捷鍵設置為按鍵 ".. key .." 。\n\n最後，請重新運行 Solo MIDI Note 腳本，並使用快捷鍵 ".. key .." 進行獨奏。"
-    okk_box = "繼續下一步"
-  else
-    okk_title = "The virtual key " .. key .. " has been set up. Next, you need to configure the key " .. key .. " to 'No Action' to prevent triggering system alert sounds.\nClicking [OK] will open the action list's shortcut settings. Please set the shortcut to key " .. key .. ".\n\nLastly, please rerun the Solo MIDI Note script and use the shortcut " .. key .. " to solo."
-    okk_box = "Proceed to the next step."
-  end
-
-  reaper.MB(okk_title, okk_box, 0) -- 继续下一步
-  reaper.DoActionShortcutDialog(0, 0, 65535, -1) -- No-op (no action)
-end
-
 local function stash_save_take_events(take)
   local _, MIDI = reaper.MIDI_GetAllEvts(take, "")
   local encodedStr = reaper.NF_Base64_Encode(MIDI, true) -- 使用REAPER的函数进行Base64编码
@@ -248,6 +203,30 @@ function get_all_takes()
   return tTake
 end
 
+function CheckShortcutSetting()
+  local shortcutSetting = reaper.GetResourcePath() .. '/Scripts/zaibuyidao Scripts/MIDI Editor/zaibuyidao_Solo MIDI Note Shortcut Setting.lua'
+
+  if reaper.file_exists(shortcutSetting) then
+    dofile(shortcutSetting)
+  else
+    reaper.MB(shortcutSetting:gsub('%\\', '/')..' not found. Please ensure the script is correctly placed.', '', 0)
+    if reaper.APIExists('ReaPack_BrowsePackages') then
+      reaper.ReaPack_BrowsePackages('zaibuyidao Solo MIDI Note Shortcut Setting')
+    else
+      reaper.MB('ReaPack extension not found', '', 0)
+    end
+  end
+end
+
+local key = reaper.GetExtState("SOLO_MIDI_NOTE_SHORTCUT_SETTING", "VirtualKey")
+if key == "" then
+  CheckShortcutSetting()
+  reaper.defer(function() end) -- 终止执行
+  key = reaper.GetExtState("SOLO_MIDI_NOTE_SHORTCUT_SETTING", "VirtualKey")
+end
+
+key_map = generateKeyMap()
+VirtualKeyCode = key_map[key]
 play_flag = false
 
 function main()
