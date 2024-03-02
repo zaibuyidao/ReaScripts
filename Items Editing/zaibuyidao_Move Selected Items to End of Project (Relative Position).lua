19d7c315366276e6e50d45a326720d0ecd6f90fe
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2024-03-02 17:46:57 +0800

    New Script

---
+++ Items Editing/zaibuyidao_Move Selected Items to End of Project (Relative Position).lua
@@ -0,0 +1,159 @@
+-- @description Move Selected Items to End of Project (Relative Position)
+-- @version 1.0
+-- @author zaibuyidao
+-- @changelog
+--   + New Script
+-- @links
+--   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   repo https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
+
+function print(...)
+    for _, v in ipairs({...}) do
+        reaper.ShowConsoleMsg(tostring(v) .. " ")
+    end
+    reaper.ShowConsoleMsg("\n")
+end
+
+function getSystemLanguage()
+    local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
+    local os = reaper.GetOS()
+    local lang
+  
+    if os == "Win32" or os == "Win64" then -- Windows
+        if locale == 936 then -- Simplified Chinese
+            lang = "简体中文"
+        elseif locale == 950 then -- Traditional Chinese
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
+    elseif os == "OSX32" or os == "OSX64" then -- macOS
+        local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
+        local result = handle:read("*a")
+        handle:close()
+        lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
+        if lang == "zh-CN" then -- 简体中文
+            lang = "简体中文"
+        elseif lang == "zh-TW" then -- 繁体中文
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
+    elseif os == "Linux" then -- Linux
+        local handle = io.popen("echo $LANG")
+        local result = handle:read("*a")
+        handle:close()
+        lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
+        if lang == "zh_CN" then -- 简体中文
+            lang = "简体中文"
+        elseif lang == "zh_TW" then -- 繁體中文
+            lang = "繁體中文"
+        else -- English
+            lang = "English"
+        end
+    end
+
+    return lang
+end
+
+local language = getSystemLanguage()
+
+if language == "简体中文" then
+    swsmsg = "该脚本需要 SWS 扩展，你想现在就下载它吗？"
+    swserr = "警告"
+    jsmsg = "请右键单击並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然后重新启动 REAPER 並再次运行脚本，谢谢！\n"
+    jstitle = "你必须安裝 JS_ReaScriptAPI"
+elseif language == "繁體中文" then
+    swsmsg = "該脚本需要 SWS 擴展，你想現在就下載它嗎？"
+    swserr = "警告"
+    jsmsg = "請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n"
+    jstitle = "你必須安裝 JS_ReaScriptAPI"
+else
+    swsmsg = "This script requires the SWS Extension. Do you want to download it now?"
+    swserr = "Warning"
+    jsmsg = "Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\nThen restart REAPER and run the script again, thank you!\n"
+    jstitle = "You must install JS_ReaScriptAPI"
+end
+
+if not reaper.SNM_GetIntConfigVar then
+    local retval = reaper.ShowMessageBox(swsmsg, swserr, 1)
+    if retval == 1 then
+        if not OS then local OS = reaper.GetOS() end
+        if OS=="OSX32" or OS=="OSX64" then
+            os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
+        else
+            os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
+        end
+    end
+    return
+end
+
+if not reaper.APIExists("JS_Window_Find") then
+    reaper.MB(jsmsg, jstitle, 0)
+    local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
+    if ok then
+        reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
+    else
+        reaper.MB(err, jserr, 0)
+    end
+    return reaper.defer(function() end)
+end
+
+function findProjectEndItemAndEarliestSelectedItem()
+    local itemCount = reaper.CountMediaItems(0)
+    local maxEndPos = 0
+    local earliestSelectedPos = math.huge
+    local projectEndItem = nil
+    
+    for i = 0, itemCount-1 do
+        local item = reaper.GetMediaItem(0, i)
+        local itemEndPos = reaper.GetMediaItemInfo_Value(item, "D_POSITION") + reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+        if itemEndPos > maxEndPos then
+            maxEndPos = itemEndPos
+            projectEndItem = item
+        end
+    end
+    
+    local selItemCount = reaper.CountSelectedMediaItems(0)
+    for i = 0, selItemCount-1 do
+        local item = reaper.GetSelectedMediaItem(0, i)
+        local itemPos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+        if itemPos < earliestSelectedPos then
+            earliestSelectedPos = itemPos
+        end
+    end
+    
+    return projectEndItem, maxEndPos, earliestSelectedPos
+end
+
+function moveItemsToEnd()
+    local projectEndItem, projectEndPos, earliestSelectedPos = findProjectEndItemAndEarliestSelectedItem()
+    if not projectEndItem or earliestSelectedPos == math.huge then return end
+    
+    local targetStartPos = projectEndPos + 1.0 -- 第一个选定对象的起始位置
+    local offset = targetStartPos - earliestSelectedPos
+    
+    local itemCount = reaper.CountSelectedMediaItems(0)
+    for i = 0, itemCount-1 do
+        local item = reaper.GetSelectedMediaItem(0, i)
+        local track = reaper.GetMediaItem_Track(item)
+        local itemPos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+        local newPosition = itemPos + offset
+        reaper.SetMediaItemInfo_Value(item, "D_POSITION", newPosition)
+        reaper.MoveMediaItemToTrack(item, track)
+    end
+    
+    reaper.UpdateArrange()
+end
+
+reaper.Undo_BeginBlock()
+moveItemsToEnd()
+if language == "简体中文" then
+    reaper.Undo_EndBlock("将选中对象移动到工程末尾(保持相对位置)", -1)
+elseif language == "繁體中文" then
+    reaper.Undo_EndBlock("將選取對象移動到工程末尾(保持相對位置)", -1)
+else
+    reaper.Undo_EndBlock("Move Selected Items to End of Project (Relative Position)", -1)
+end
