3705f5e7e67a9d3c9e466f55813702f21439c75f
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2024-03-21 20:18:33 +0800

    Update a_test.lua

--- Various/a_test.lua
+++ Various/a_test.lua
@@ -1,5 +1,5 @@
 -- @description a_test
--- @version 1.0.21
+-- @version 1.0.22
 -- @metapackage
 -- @provides
---    test/Effects/a_test.jsfx > . > ../../Effects/a_test.jsfx
+--    test/Effects/a_test.jsfx > | Effects/a_test.jsfx
