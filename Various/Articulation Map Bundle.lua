0794f97ab6269bf969221ea10b4ba6de2ea943df
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2024-03-22 20:58:54 +0800

    New Script

---
+++ Various/Articulation Map Bundle.lua
@@ -0,0 +1,21 @@
+-- @description Articulation Map Bundle
+-- @author zaibuyidao
+-- @version 1.0
+-- @changelog
+--   + New Script
+-- @links
+--   https://forum.cockos.com/showthread.php?t=289373
+--   https://github.com/zaibuyidao/ReaScripts
+-- @metapackage
+-- @provides
+--   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map - No GUI.lua
+--   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map - Note To PC.lua
+--   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map - PC To Note.lua
+--   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map - Toggle Note PC.lua
+--   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map.lua
+--   [nomain] Articulation Map/lib/*.lua
+--   Articulation Map/articulation_map_factory.reabank
+-- @donation http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
+
+-- Licensed under the GNU GPL v3
