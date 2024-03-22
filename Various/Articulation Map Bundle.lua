e7e29dfd1c20b166286b83fcfe21db1df4083860
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2024-03-23 02:45:34 +0800

    Update

--- Various/Articulation Map Bundle.lua
+++ Various/Articulation Map Bundle.lua
@@ -1,13 +1,15 @@
 -- @description Articulation Map Bundle
 -- @author zaibuyidao
--- @version 1.0.2
+-- @version 1.0.3
 -- @changelog
 --   + New Script
 -- @links
---   https://forum.cockos.com/showthread.php?t=289373
---   https://github.com/zaibuyidao/ReaScripts
+--   Forum Thread https://forum.cockos.com/showthread.php?t=289373
+--   GitHub repository https://github.com/zaibuyidao/ReaScripts
 -- @metapackage
 -- @provides
+--  [jsfx] Articulation Map/jsfx/Articulation Map.jsfx
+--  [jsfx] Articulation Map/jsfx/Pre-trigger CC Event.jsfx
 --   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map - No GUI.lua
 --   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map - Note To PC.lua
 --   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map - PC To Note.lua
@@ -15,8 +17,6 @@
 --   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map.lua
 --   [nomain] Articulation Map/lib/*.lua
 --   Articulation Map/articulation_map_factory.reabank
---  [jsfx] Articulation Map/jsfx/Articulation Map.jsfx
---  [jsfx] Articulation Map/jsfx/Pre-trigger CC Event.jsfx
 -- @donation http://www.paypal.me/zaibuyidao
 -- @about Requires JS_ReaScriptAPI & SWS Extension
 
