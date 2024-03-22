035e8ec9e673a68fd9f11e7ea8cd29d9a696d8c4
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2024-03-22 17:51:43 +0800

    up

--- Various/test bundle.lua
+++ Various/test bundle.lua
@@ -1,8 +1,7 @@
 -- @noindex
 -- @description test bundle
--- @version 1.0.54
+-- @version 1.0.55
 -- @metapackage
 
 -- @provides
 --  [effect] amjs/test_test.jsfx
---  [effect] amjs/test_test_test.jsfx
