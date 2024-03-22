88c715d48f720961138de32dc6835f73a11e9a21
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2024-03-22 15:19:41 +0800

    up

--- Various/a_test.lua
+++
@@ -1,5 +0,0 @@
--- @description a_test
--- @version 1.0.25
--- @metapackage
--- @provides
---  [effect] test/a_test.jsfx
---
+++ Various/test bundle.lua
@@ -0,0 +1,5 @@
+-- @description test bundle
+-- @version 1.0.26
+-- @metapackage
+-- @provides
+--  [effect] test/test.jsfx
