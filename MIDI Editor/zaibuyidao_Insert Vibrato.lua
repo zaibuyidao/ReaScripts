a70460ef84ca9f9f3abd1212fa564b665a22a906
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2024-06-20 16:15:50 +0800

    Update zaibuyidao_Insert Vibrato.lua

--- MIDI Editor/zaibuyidao_Insert Vibrato.lua
+++ MIDI Editor/zaibuyidao_Insert Vibrato.lua
@@ -1,105 +1,42 @@
 -- @description Insert Vibrato
--- @version 2.0.6
+-- @version 2.0.7
 -- @author zaibuyidao
 -- @changelog
---   + Add Multi-Language Support
+--   New Script
 -- @links
---   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
---   repo https://github.com/zaibuyidao/ReaScripts
+--   https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   https://github.com/zaibuyidao/ReaScripts
 -- @donate http://www.paypal.me/zaibuyidao
--- @about Requires JS_ReaScriptAPI & SWS Extension
+-- @about
+--   Pitch Bend Script Series, filter "zaibuyidao pitch bend" in ReaPack or Actions to access all scripts.
+--   Requires JS_ReaScriptAPI & SWS Extension
 
-function print(...)
-  for _, v in ipairs({...}) do
-    reaper.ShowConsoleMsg(tostring(v) .. " ")
-  end
-  reaper.ShowConsoleMsg("\n")
-end
-
-function getSystemLanguage()
-  local locale = tonumber(string.match(os.setlocale(), "(%d+)$"))
-  local os = reaper.GetOS()
-  local lang
-
-  if os == "Win32" or os == "Win64" then -- Windows
-    if locale == 936 then -- Simplified Chinese
-      lang = "简体中文"
-    elseif locale == 950 then -- Traditional Chinese
-      lang = "繁體中文"
-    else -- English
-      lang = "English"
-    end
-  elseif os == "OSX32" or os == "OSX64" then -- macOS
-    local handle = io.popen("/usr/bin/defaults read -g AppleLocale")
-    local result = handle:read("*a")
-    handle:close()
-    lang = result:gsub("_", "-"):match("[a-z]+%-[A-Z]+")
-    if lang == "zh-CN" then -- 简体中文
-      lang = "简体中文"
-    elseif lang == "zh-TW" then -- 繁体中文
-      lang = "繁體中文"
-    else -- English
-      lang = "English"
-    end
-  elseif os == "Linux" then -- Linux
-    local handle = io.popen("echo $LANG")
-    local result = handle:read("*a")
-    handle:close()
-    lang = result:gsub("%\n", ""):match("[a-z]+%-[A-Z]+")
-    if lang == "zh_CN" then -- 简体中文
-      lang = "简体中文"
-    elseif lang == "zh_TW" then -- 繁體中文
-      lang = "繁體中文"
-    else -- English
-      lang = "English"
-    end
-  end
-
-  return lang
-end
-
-local language = getSystemLanguage()
-
-if language == "简体中文" then
-  swsmsg = "该脚本需要 SWS 扩展，你想现在就下载它吗？"
-  swserr = "警告"
-  jsmsg = "请右键单击並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然后重新启动 REAPER 並再次运行脚本，谢谢！\n"
-  jstitle = "你必须安裝 JS_ReaScriptAPI"
-elseif language == "繁体中文" then
-  swsmsg = "該脚本需要 SWS 擴展，你想現在就下載它嗎？"
-  swserr = "警告"
-  jsmsg = "請右鍵單擊並安裝 'js_ReaScriptAPI: API functions for ReaScripts'。\n然後重新啟動 REAPER 並再次運行腳本，謝謝！\n"
-  jstitle = "你必須安裝 JS_ReaScriptAPI"
+local ZBYDFuncPath = reaper.GetResourcePath() .. '/Scripts/zaibuyidao Scripts/Utility/zaibuyidao_Functions.lua'
+if reaper.file_exists(ZBYDFuncPath) then
+  dofile(ZBYDFuncPath)
+  if not checkSWSExtension() or not checkJSAPIExtension() then return end
 else
-  swsmsg = "This script requires the SWS Extension. Do you want to download it now?"
-  swserr = "Warning"
-  jsmsg = "Please right-click and install 'js_ReaScriptAPI: API functions for ReaScripts'.\nThen restart REAPER and run the script again, thank you!\n"
-  jstitle = "You must install JS_ReaScriptAPI"
-end
+  local errorMsg = "Error - Missing Script (错误 - 缺失脚本)\n\n" ..
+  "[English]\nThe required 'zaibuyidao Functions' script file was not found. Please ensure the file is correctly placed at:\n" ..
+  ZBYDFuncPath:gsub('%\\', '/') .. "\n\nIf the file is missing, you can install it via ReaPack by searching for 'zaibuyidao Functions' in the ReaPack package browser.\n\n" ..
+  "[中文]\n必需的 'zaibuyidao Functions' 脚本文件未找到。请确保文件正确放置在以下位置：\n" ..
+  ZBYDFuncPath:gsub('%\\', '/') .. "\n\n如果文件缺失，您可以通过 ReaPack 包浏览器搜索并安装 'zaibuyidao Functions'。\n"
 
-if not reaper.SN_FocusMIDIEditor then
-  local retval = reaper.ShowMessageBox(swsmsg, swserr, 1)
-  if retval == 1 then
-    if not OS then local OS = reaper.GetOS() end
-    if OS=="OSX32" or OS=="OSX64" then
-      os.execute("open " .. "http://www.sws-extension.org/download/pre-release/")
-    else
-      os.execute("start " .. "http://www.sws-extension.org/download/pre-release/")
-    end
+  reaper.MB(errorMsg, "Missing Script Error/脚本文件缺失错误", 0)
+
+  if reaper.APIExists('ReaPack_BrowsePackages') then
+    reaper.ReaPack_BrowsePackages('zaibuyidao Functions')
+  else
+    local reapackErrorMsg = "Error - ReaPack Not Found (错误 - 未找到 ReaPack)\n\n" ..
+    "[English]\nThe ReaPack extension is not found. Please install ReaPack to manage and install REAPER scripts and extensions easily. Visit https://reapack.com for installation instructions.\n\n" ..
+    "[中文]\n未找到 ReaPack 扩展。请安装 ReaPack 来便捷地管理和安装 REAPER 脚本及扩展。访问 https://reapack.com 获取安装指南。\n"
+
+    reaper.MB(reapackErrorMsg, "ReaPack Not Found/未找到 ReaPack", 0)
   end
   return
 end
 
-if not reaper.APIExists("JS_Window_Find") then
-  reaper.MB(jsmsg, jstitle, 0)
-  local ok, err = reaper.ReaPack_AddSetRepository("ReaTeam Extensions", "https://github.com/ReaTeam/Extensions/raw/master/index.xml", true, 1)
-  if ok then
-    reaper.ReaPack_BrowsePackages("js_ReaScriptAPI")
-  else
-    reaper.MB(err, jserr, 0)
-  end
-  return reaper.defer(function() end)
-end
+local language = getSystemLanguage()
 
 local _SN_FocusMIDIEditor = reaper.SN_FocusMIDIEditor
 reaper.SN_FocusMIDIEditor = function(...) if _SN_FocusMIDIEditor then _SN_FocusMIDIEditor(...) end end
@@ -194,18 +131,18 @@ if (length == "") then length = "240" end
 local num = reaper.GetExtState("INSERT_VIBRATO", "Num")
 if (num == "") then num = "12" end
 local shape = reaper.GetExtState("INSERT_VIBRATO", "Shape")
-if (shape == "") then shape = "0" end
+if (shape == "") then shape = "1" end
 
 local captions_csv = ""
 if language == "简体中文" then
   title = "插入揉弦"
-  captions_csv = "起始点,最高点,重复,长度,点数,0=正弦波 1=三角波"
-elseif language == "繁体中文" then
+  captions_csv = "起始点:,最高点:,重复:,长度:,点数:,模式(1=正弦波 2=三角波):"
+elseif language == "繁體中文" then
   title = "插入揉弦"
-  captions_csv = "起始點,最高點,重複,長度,點數,0=正弦波 1=三角波"
+  captions_csv = "起始點:,最高點:,重複:,長度:,點數:,模式(1=正弦波 2=三角波):"
 else
   title = "Insert Vibrato"
-  captions_csv = "Starting value,Highest value,Repetitions,Length,Points,0=Sine 1=Triangle"
+  captions_csv = "Starting value:,Highest value:,Repetitions:,Length:,Points:,Mode (1=Sine 2=Triangle):"
 end
 
 local uok, uinput = reaper.GetUserInputs(title, 6, captions_csv, bottom ..','.. top ..','.. times .. "," .. length .. "," .. num .. "," .. shape)
@@ -213,7 +150,7 @@ if not uok then return reaper.SN_FocusMIDIEditor() end
 bottom, top, times, length, num, shape = uinput:match("(.*),(.*),(.*),(.*),(.*),(.*)")
 if not tonumber(bottom) or not tonumber(top) or not tonumber(times) or not tonumber(length) or not tonumber(num) or not tonumber(shape) then return reaper.SN_FocusMIDIEditor() end
 bottom, top, times, length, num, shape = tonumber(bottom), tonumber(top), tonumber(times), tonumber(length), tonumber(num), tonumber(shape)
-if times < 1 or shape > 1 then return reaper.SN_FocusMIDIEditor() end
+if times < 1 or shape > 2 then return reaper.SN_FocusMIDIEditor() end
 
 reaper.SetExtState("INSERT_VIBRATO", "Bottom", bottom, false)
 reaper.SetExtState("INSERT_VIBRATO", "Top", top, false)
@@ -237,13 +174,13 @@ end
 
 chan = 0 -- 通道默认为0
 
-if shape == 0 then
+if shape == 1 then
   curve = get_curve1(bottom, top, num)
-elseif shape == 1 then
-  curve = get_curve2(bottom, top, num)
 elseif shape == 2 then
-  curve = get_curve3(bottom, top, num)
+  curve = get_curve2(bottom, top, num)
 elseif shape == 3 then
+  curve = get_curve3(bottom, top, num)
+elseif shape == 4 then
   curve = get_curve4(bottom, top, num)
 end
 
@@ -266,14 +203,14 @@ if (curve[#curve] ~= bottom) then
   reaper.MIDI_InsertCC(take, false, false, cur_tick, 224, chan, value & 0x7f, value >> 7 & 0x7f)
 end
 
-if shape == 0 or shape == 1 then
+if shape == 1 or shape == 2 then
   j = reaper.MIDI_EnumSelCC(take, -1) -- 选中CC设置形状
   while j ~= -1 do
     reaper.MIDI_SetCCShape(take, j, 1, 0, false)
     reaper.MIDI_SetCC(take, j, false, false, nil, nil, nil, nil, nil, false)
     j = reaper.MIDI_EnumSelCC(take, j)
   end
-elseif shape == 2 or shape == 3 then
+elseif shape == 3 or shape == 4 then
   j = reaper.MIDI_EnumSelCC(take, -1) -- 选中CC设置形状
   while j ~= -1 do
     reaper.MIDI_SetCCShape(take, j, 0, 0, false)
