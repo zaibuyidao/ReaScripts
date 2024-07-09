d1b1faec1434b15a30bc14fdf627e042bcbe4670
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2024-07-09 22:41:26 +0800

    Update zaibuyidao_Random Note Mute.lua

--- MIDI Editor/zaibuyidao_Random Note Mute.lua
+++ MIDI Editor/zaibuyidao_Random Note Mute.lua
@@ -1,52 +1,41 @@
 -- @description Random Note Mute
--- @version 1.0
+-- @version 1.0.1
 -- @author zaibuyidao
--- @changelog Initial release
+-- @changelog
+--   New Script
 -- @links
---   webpage https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
---   repo https://github.com/zaibuyidao/ReaScripts
+--   https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   https://github.com/zaibuyidao/ReaScripts
 -- @donate http://www.paypal.me/zaibuyidao
--- @about Requires SWS Extensions
+-- @about Random Note Script Series, filter "zaibuyidao random note" in ReaPack or Actions to access all scripts.
 
 function main()
-    take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
+    local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
     if not take or not reaper.TakeIsMIDI(take) then return end
-    _, notecnt, _, _ = reaper.MIDI_CountEvts(take)
+    
+    local _, notecnt = reaper.MIDI_CountEvts(take)
+    local has_selected_notes = reaper.MIDI_EnumSelNotes(take, -1) ~= -1
 
-    i = reaper.MIDI_EnumSelNotes(take, -1)
-    if i ~= -1 then sel_note = true end
-    while i ~= -1 do
-        local note = {}
-        note[i] = {}
-        note[i].ret,
-        note[i].sel,
-        note[i].muted,
-        note[i].startppqpos,
-        note[i].endppqpos,
-        note[i].chan,
-        note[i].pitch,
-        note[i].vel = reaper.MIDI_GetNote(take, i)
-        if note[i].sel then
-            bool = math.random(2)
-            if bool == 1 then
-                reaper.MIDI_SetNote(take, i, nil, true, nil, nil, nil, nil, nil, true)
-            else
-                reaper.MIDI_SetNote(take, i, nil, false, nil, nil, nil, nil, nil, true)
+    reaper.MIDI_DisableSort(take)
+    
+    if has_selected_notes then
+        local i = reaper.MIDI_EnumSelNotes(take, -1)
+        while i ~= -1 do
+            local _, sel = reaper.MIDI_GetNote(take, i)
+            if sel then
+                local mute_state = math.random(2) == 1
+                reaper.MIDI_SetNote(take, i, nil, mute_state, nil, nil, nil, nil, nil, true)
             end
+            i = reaper.MIDI_EnumSelNotes(take, i)
         end
-        i = reaper.MIDI_EnumSelNotes(take, i)
-    end
-    for i = 1, notecnt do
-        _, selected, _, startppqpos, endppqpos, _, _, _ = reaper.MIDI_GetNote(take, i - 1)
-        if not sel_note then
-            bool = math.random(2)
-            if bool == 1 then
-                reaper.MIDI_SetNote(take, i - 1, nil, true, nil, nil, nil, nil, nil, true)
-            else
-                reaper.MIDI_SetNote(take, i - 1, nil, false, nil, nil, nil, nil, nil, true)
-            end
+    else
+        -- 如果没有选中的音符，则处理所有音符
+        for i = 0, notecnt - 1 do
+            local mute_state = math.random(2) == 1
+            reaper.MIDI_SetNote(take, i, nil, mute_state, nil, nil, nil, nil, nil, true)
         end
     end
+    
     reaper.MIDI_Sort(take)
 end
 
