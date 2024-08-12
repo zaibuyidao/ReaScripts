0c928e5ff9dcd46dbc5c557841db59c4cd32d0ca
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2024-08-12 12:00:38 +0800

    Update Articulation Map

--- Various/Articulation Map Bundle.lua
+++ Various/Articulation Map Bundle.lua
@@ -1,9 +1,8 @@
 -- @description Articulation Map Bundle
 -- @author zaibuyidao
--- @version 1.0.47
+-- @version 1.0.48
 -- @changelog
---   # Improved the array management logic by dynamically expanding the array size, preventing data loss caused by array overflow.
---   # Optimized the file reading and configuration processing logic, enhancing the stability and performance of the pre_trigger_events jsfx.
+--   + Added support for dynamic Bank MSB/LSB mapping through a configuration file, enabling flexible MIDI bank remapping.
 -- @links
 --   Forum Thread https://forum.cockos.com/showthread.php?t=289373
 --   GitHub repository https://github.com/zaibuyidao/ReaScripts
@@ -12,6 +11,7 @@
 --  [jsfx] Articulation Map/jsfx/articulation_map.jsfx > Articulation Map/articulation_map.jsfx
 --  [jsfx] Articulation Map/jsfx/pre_trigger_events.jsfx > Articulation Map/pre_trigger_events.jsfx
 --  [data] Articulation Map/jsfx/zaibuyidao_articulation_map/simul-arts.txt > zaibuyidao_articulation_map/simul-arts.txt
+--  [data] Articulation Map/jsfx/zaibuyidao_articulation_map/bank-map.txt > zaibuyidao_articulation_map/bank-map.txt
 --   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map - No GUI.lua
 --   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map - CC to PC.lua
 --   [main=midi_editor] Articulation Map/zaibuyidao_Articulation Map - PC to CC.lua
