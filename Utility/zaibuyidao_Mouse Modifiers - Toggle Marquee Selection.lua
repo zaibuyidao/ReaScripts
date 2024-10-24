ca4eb39401b372ae99af24018551080f46c36ab6
ito.qiu <46724195+zaibuyidao@users.noreply.github.com>
2024-10-24 14:09:17 +0800

    Update Mouse Modifiers

--- Utility/zaibuyidao_Mouse Modifiers - Toggle Marquee Selection.lua
+++ Utility/zaibuyidao_Mouse Modifiers - Toggle Marquee Selection.lua
@@ -1,5 +1,5 @@
 -- @description Mouse Modifiers - Toggle Marquee Selection
--- @version 1.0
+-- @version 1.0.1
 -- @author zaibuyidao
 -- @changelog
 --   New Script
@@ -36,9 +36,11 @@ end
 
 local marquee_selection = reaper.NamedCommandLookup('_RSe26d413e28a889ec8384d643aeb592b204e5e7e9')
 local time_selection = reaper.NamedCommandLookup('_RSa7b6a0054a56c6f5822b24ee2b398cd4488b449c')
+local razor_editing = reaper.NamedCommandLookup('_RS310aee702b4cc146de8699ea241a41e0c3d46b3d')
 local is_new_value, filename, sectionID, cmdID,mode, resolution, val = reaper.get_action_context()
 reaper.SetToggleCommandState(sectionID, marquee_selection, 1)
 reaper.SetToggleCommandState(sectionID, time_selection, 0)
+reaper.SetToggleCommandState(sectionID, razor_editing, 0)
 reaper.RefreshToolbar2(sectionID, cmdID)
 
 reaper.SetMouseModifier('MM_CTX_ITEM',0,'13 m') -- Move item ignoring time selection
