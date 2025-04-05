d29023c1d691fe28e9f5722d15d98c3cd9a6fada
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2025-04-05 20:13:53 +0800

    New Script

--- MIDI Editor/zaibuyidao_Insert Expression CC Events.lua
+++ MIDI Editor/zaibuyidao_Insert Expression CC Events.lua
@@ -1,31 +1,48 @@
---[[
- * ReaScript Name: Insert Expression (Set CC Shape)
- * Instructions: Open a MIDI take in MIDI Editor. Select Notes. Run.
- * Version: 1.0
- * Author: zaibuyidao
- * Author URI: https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
- * Repository: GitHub > zaibuyidao > ReaScripts
- * Repository URI: https://github.com/zaibuyidao/ReaScripts
- * Reference: https://forum.cockos.com/showthread.php?t=225108
- * REAPER: 6.0
- * Donation: http://www.paypal.me/zaibuyidao
---]]
+-- @description Insert Expression CC Events
+-- @version 1.0
+-- @author zaibuyidao
+-- @changelog
+--   + New Script
+-- @links
+--   https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
+--   https://github.com/zaibuyidao/ReaScripts
+-- @donate http://www.paypal.me/zaibuyidao
+-- @about Requires JS_ReaScriptAPI & SWS Extension
 
---[[
- * Changelog:
- * v1.0 (2020-1-1)
-  + Initial release
---]]
+local ZBYDFuncPath = reaper.GetResourcePath() .. '/Scripts/zaibuyidao Scripts/Utility/zaibuyidao_Functions.lua'
+if reaper.file_exists(ZBYDFuncPath) then
+  dofile(ZBYDFuncPath)
+  if not checkSWSExtension() or not checkJSAPIExtension() then return end
+else
+  local errorMsg = "Error - Missing Script (错误 - 缺失脚本)\n\n" ..
+  "[English]\nThe required 'zaibuyidao Functions' script file was not found. Please ensure the file is correctly placed at:\n" ..
+  ZBYDFuncPath:gsub('%\\', '/') .. "\n\nIf the file is missing, you can install it via ReaPack by searching for 'zaibuyidao Functions' in the ReaPack package browser.\n\n" ..
+  "[中文]\n必需的 'zaibuyidao Functions' 脚本文件未找到。请确保文件正确放置在以下位置：\n" ..
+  ZBYDFuncPath:gsub('%\\', '/') .. "\n\n如果文件缺失，您可以通过 ReaPack 包浏览器搜索并安装 'zaibuyidao Functions'。\n"
+
+  reaper.MB(errorMsg, "Missing Script Error/脚本文件缺失错误", 0)
+
+  if reaper.APIExists('ReaPack_BrowsePackages') then
+    reaper.ReaPack_BrowsePackages('zaibuyidao Functions')
+  else
+    local reapackErrorMsg = "Error - ReaPack Not Found (错误 - 未找到 ReaPack)\n\n" ..
+    "[English]\nThe ReaPack extension is not found. Please install ReaPack to manage and install REAPER scripts and extensions easily. Visit https://reapack.com for installation instructions.\n\n" ..
+    "[中文]\n未找到 ReaPack 扩展。请安装 ReaPack 来便捷地管理和安装 REAPER 脚本及扩展。访问 https://reapack.com 获取安装指南。\n"
+
+    reaper.MB(reapackErrorMsg, "ReaPack Not Found/未找到 ReaPack", 0)
+  end
+  return
+end
 
 take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
 if take == nil then return end
 tick = reaper.SNM_GetIntConfigVar("MidiTicksPerBeat", 480)
 local _, notecnt, _, _ = reaper.MIDI_CountEvts(take)
 
-local cc_num = reaper.GetExtState("InsertExpressionShape", "CC")
-local val_01 = reaper.GetExtState("InsertExpressionShape", "Val1")
-local val_02 = reaper.GetExtState("InsertExpressionShape", "Val2")
-local val_03 = reaper.GetExtState("InsertExpressionShape", "Val3")
+local cc_num = reaper.GetExtState("InsertExpressionCCEvents", "CC")
+local val_01 = reaper.GetExtState("InsertExpressionCCEvents", "Val1")
+local val_02 = reaper.GetExtState("InsertExpressionCCEvents", "Val2")
+local val_03 = reaper.GetExtState("InsertExpressionCCEvents", "Val3")
 if (cc_num == "") then cc_num = "11" end
 if (val_01 == "") then val_01 = "90" end
 if (val_02 == "") then val_02 = "127" end
@@ -37,10 +54,10 @@ cc_num, val_01, val_02, val_03 = user_input_csv:match("(.*),(.*),(.*),(.*)")
 if not tonumber(cc_num) or not tonumber(val_01) or not tonumber(val_02) or not tonumber(val_03) then return reaper.SN_FocusMIDIEditor() end
 cc_num, val_01, val_02, val_03 = tonumber(cc_num), tonumber(val_01), tonumber(val_02), tonumber(val_03)
 
-reaper.SetExtState("InsertExpressionShape", "CC", cc_num, false)
-reaper.SetExtState("InsertExpressionShape", "Val1", val_01, false)
-reaper.SetExtState("InsertExpressionShape", "Val2", val_02, false)
-reaper.SetExtState("InsertExpressionShape", "Val3", val_03, false)
+reaper.SetExtState("InsertExpressionCCEvents", "CC", cc_num, false)
+reaper.SetExtState("InsertExpressionCCEvents", "Val1", val_01, false)
+reaper.SetExtState("InsertExpressionCCEvents", "Val2", val_02, false)
+reaper.SetExtState("InsertExpressionCCEvents", "Val3", val_03, false)
 
 function INST1() -- 音符开头插入
   for i = 0,  notecnt - 1 do
@@ -126,7 +143,7 @@ function INST4() -- 音符结尾插入
   reaper.MIDIEditor_LastFocused_OnCommand(40671, 0) -- Unselect all CC events
 end
 
-script_title = "Insert Expression (Set CC Shape)"
+script_title = "Insert Expression CC Events"
 reaper.Undo_BeginBlock()
 INST1()
 INST2()
