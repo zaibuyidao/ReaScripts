7eeaa4f7044dba8d49182a99a2cd33ae57893b32
zaibuyidao <46724195+zaibuyidao@users.noreply.github.com>
2025-05-26 22:56:07 +0800

    Update zaibuyidao_Functions.lua

--- Utility/zaibuyidao_Functions.lua
+++ Utility/zaibuyidao_Functions.lua
@@ -1,9 +1,10 @@
 -- @description Functions
--- @version 1.0.6
+-- @version 1.0.7
 -- @author zaibuyidao
 -- @links
 --   https://www.soundengine.cn/user/%E5%86%8D%E8%A3%9C%E4%B8%80%E5%88%80
 --   https://github.com/zaibuyidao/ReaScripts
+-- @provides [nomain] .
 -- @about Core Function Library
 -- @changelog New Script
 
